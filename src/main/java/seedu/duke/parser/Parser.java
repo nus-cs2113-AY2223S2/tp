f1d27df761a8b65d8c0302021e774cf312df2b0a
adenteo <adenteo@gmail.com>
2023-03-04 18:42:34 +0800

    Update Parser.java

--- src/main/java/seedu/duke/parser/Parser.java
+++ src/main/java/seedu/duke/parser/Parser.java
@@ -51,6 +51,7 @@ public class Parser {
             break;
         case COMMAND_BYE:
             parseByeCommand();
+            break;
         default:
             throw new InvalidCommandException(MESSAGE_INVALID_COMMAND);
         }
