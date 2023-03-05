96fe374d923b0c488cca6912e5042efd43278a6f
EangJS <eugeneang654871@gmail.com>
2023-03-06 03:07:36 +0800

    Set exercise data as resource

--- src/main/java/seedu/duke/exercisegenerator/ParseData.java
+++ src/main/java/seedu/duke/exercisegenerator/ParseData.java
@@ -1,27 +1,29 @@
 package seedu.duke.exercisegenerator;
 
+
 import com.google.gson.Gson;
 import seedu.duke.exersisedata.ExerciseData;
 import seedu.duke.exersisedata.ExerciseDataList;
 
 
-import java.util.ArrayList;
-import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.ArrayList;
 
 public class ParseData{
-    private static final String FILE_PATH = "ExerciseDataset.json";
+
     public ArrayList<ExerciseData> getExercises(){
-        Gson gson = new Gson();
-        try {
-            Reader reader =new FileReader(FILE_PATH); //reads the database
-            ExerciseDataList exerciseDataList = gson.fromJson(reader, ExerciseDataList.class);
-            ArrayList<ExerciseData> exerciseData = exerciseDataList.exercises; //gets array of exercises
+        try (Reader reader = new InputStreamReader(this.getClass()
+                .getResourceAsStream("/data.json"))) {
+            ExerciseDataList result = new Gson().fromJson(reader, ExerciseDataList.class);
+            ArrayList<ExerciseData> exerciseData = result.exercises;
             return exerciseData;
 
-        }catch (Exception e){
-            System.out.println(e);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
         return null;
     }
 }
--- src/main/java/seedu/duke/ui/Greet.java
+++ src/main/java/seedu/duke/ui/Greet.java
@@ -1,6 +1,5 @@
 package seedu.duke.ui;
 
-import java.util.Scanner;
 
 public class Greet {
 
@@ -12,9 +11,9 @@ public class Greet {
 
     public static void greet(){
         System.out.println("Hello from\n" + LOGO);
-        System.out.println("What is your name?");
-        Scanner in = new Scanner(System.in);
-        System.out.println("Hello " + in.nextLine());
+        //System.out.println("What is your name?");
+        //Scanner in = new Scanner(System.in);
+        //System.out.println("Hello " + in.nextLine());
         System.out.println("type: generate x (where x is a positive integer)");
     }
 }
