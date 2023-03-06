4bbe0cc483fc2156bf8a5e9960ad16bbb2259247
HiIAmTzeKean <ngtzekean600@gmail.com>
2023-03-06 13:38:14 +0800

    exception package

---
+++ src/main/java/seedu/dukeofbooks/data/exception/IllegalDateException.java
@@ -0,0 +1,10 @@
+public package seedu.dukeofbooks.data.exception;
+
+public class IllegalDateException {
+    /**
+     * @param message should contain relevant information on the failed constraint(s)
+     */
+    public IllegalDateException(String message) {
+        super(message);
+    }
+}
