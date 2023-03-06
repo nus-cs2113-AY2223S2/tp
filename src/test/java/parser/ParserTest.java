76e7e227a61c462c48bd348026c3e84bb84c94ee
Zijin <e0772699@u.nus.edu>
2023-03-06 21:43:29 +0800

    Add unit testing codes for current functions

---
+++ src/test/java/parser/ParserTest.java
@@ -0,0 +1,42 @@
+
+package parser;
+
+import commands.AddCommand;
+import commands.Command;
+import objects.exceptions.EmptyFlashcardAnswerException;
+import objects.exceptions.EmptyFlashcardQuestionException;
+import objects.exceptions.InvalidAddFlashcardInputException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+class ParserTest {
+    @Test
+    public void parseAddCommand_correctlyFormattedInput_successful() {
+        Parser parser = new Parser();
+        Command parsedCommand = parser.parseCommand("add /q Question /a Answer");
+        Command expectedCommand = new AddCommand("Question", "Answer");
+        assertEquals(expectedCommand.toString(), parsedCommand.toString());
+    }
+
+    @Test
+    public void parserAddCommand_incorrectFormattedInput_invalidInputException() {
+        Parser parser = new Parser();
+        assertThrows(InvalidAddFlashcardInputException.class,
+                () -> parser.reformatAddCommandInput("add Question /a Answer"));
+        assertThrows(InvalidAddFlashcardInputException.class,
+                () -> parser.reformatAddCommandInput("add /q Question /w Answer"));
+    }
+
+    @Test
+    public void parserAddCommand_partOfInputMissing_emptyInputPartException() {
+        Parser parser = new Parser();
+        assertThrows(EmptyFlashcardAnswerException.class,
+                () -> parser.reformatAddCommandInput("add /q Question /a "));
+        assertThrows(EmptyFlashcardQuestionException.class,
+                () -> parser.reformatAddCommandInput("add /q  /a Answer"));
+    }
+
+}
