621c3688b5c0460c64ad6135d6881cca1ab8945e
Zijin <e0772699@u.nus.edu>
2023-03-06 15:54:57 +0800

    Add Flashcard.java to construct a flashcard object

---
+++ src/main/java/Flashcard.java
@@ -0,0 +1,10 @@
+public class Flashcard {
+    private String question;
+    private String answer;
+
+    public Flashcard(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+
+}
