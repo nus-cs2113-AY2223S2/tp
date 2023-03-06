d7e12bb0fec3fde9c56729ecfe2aab614df85b61
HiIAmTzeKean <ngtzekean600@gmail.com>
2023-03-06 13:37:33 +0800

    book package

---
+++ src/main/java/seedu/dukeofbooks/data/book/Book.java
@@ -0,0 +1,13 @@
+package seedu.data.book;
+
+package seedu.dukeofbooks.data.book;
+import seedu.dukeofbooks.data.person.Person;
+
+public class Book {
+    private Isbn isbn;
+    private Title title;
+    private Topic topic;
+    private Person author;
+
+    
+}
---
+++ src/main/java/seedu/dukeofbooks/data/book/Title.java
@@ -0,0 +1,6 @@
+package seedu.dukeofbooks.data.book
+import seedu.dukeofbooks.common.Name;
+
+public class Title extends Name {
+    
+}
