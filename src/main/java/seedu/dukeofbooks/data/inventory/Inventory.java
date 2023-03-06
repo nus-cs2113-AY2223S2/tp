4c3b49cf72f142f75d5e0bac6562259d2c6950ac
HiIAmTzeKean <ngtzekean600@gmail.com>
2023-03-06 13:37:42 +0800

    inventory package

---
+++ src/main/java/seedu/dukeofbooks/data/inventory/Inventory.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+
+package seedu.dukeofbooks.data.inventory;
+import seedu.dukeofbooks.data.book.Book;
+import seedu.dukeofbooks.data.inventory.InventoryDetails;
+
+public class Inventory {
+    private HashMap<Book, InventoryDetails> inventoryListing;
+}
