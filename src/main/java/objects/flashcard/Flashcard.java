4be87ab0daab374608bb25fc046645041235f3a9
Zijin <e0772699@u.nus.edu>
2023-03-06 21:42:56 +0800

    Update Flashcard.java to better follow OOP standard

---
+++ src/main/java/objects/flashcard/Flashcard.java
@@ -0,0 +1,20 @@
+package objects.flashcard;
+
+
+public class Flashcard {
+    private String questionText;
+    private String answerText;
+
+    public Flashcard(String questionText, String answerText) {
+        this.questionText = questionText;
+        this.answerText = answerText;
+    }
+
+    public String getQuestionText() {
+        return questionText;
+    }
+
+    public String getAnswerText() {
+        return answerText;
+    }
+}
