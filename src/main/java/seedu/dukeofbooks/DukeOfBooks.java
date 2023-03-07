55e28ca1d8b68dcde8a99485015fb1e528492a34
HiIAmTzeKean <ngtzekean600@gmail.com>
2023-03-07 17:38:56 +0800

    base main file

---
+++ src/main/java/seedu/dukeofbooks/DukeOfBooks.java
@@ -0,0 +1,13 @@
+package seedu.dukeofbooks;
+
+/**
+ * Main file to run
+ */
+public class DukeOfBooks {
+    public static void main(String[] args) {
+        start();
+    }
+    private static void start() {
+        return;
+    }
+}
