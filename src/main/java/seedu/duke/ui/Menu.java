8c16fe2f78e653c6c94d1197ca6ba6811ce50e2c
JeraldChen <jeraldchenjunhan@gmail.com>
2023-03-08 22:05:43 +0800

    Add hashmap to store login and registration details

--- src/main/java/seedu/duke/ui/Menu.java
+++ src/main/java/seedu/duke/ui/Menu.java
@@ -1,10 +1,11 @@
 package seedu.duke.ui;
 
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Menu {
+    static HashMap<String, String> nameToPassword = new HashMap<>();
     public static void showMenu() {
         System.out.println("Welcome to the Duke App!");
         System.out.println("What would you like to do?");
@@ -14,23 +15,38 @@ public class Menu {
     }
 
     public static void register() {
-        ArrayList<String> register = new ArrayList<>();
         System.out.println("Please enter your name: ");
-        int count = 0;
-        register.add(count, new Scanner(System.in).nextLine());
-        System.out.println("Please enter your email: ");
-        register.add(count + 1, new Scanner(System.in).nextLine());
-        System.out.println("Please enter your contact number: ");
-        register.add(count + 2, new Scanner(System.in).nextLine());
+        String name = new Scanner(System.in).nextLine();
+        System.out.println("Please enter your password: ");
+        String password = new Scanner(System.in).nextLine();
+        System.out.println("Please re-enter your password: ");
+        String password2 = new Scanner(System.in).nextLine();
+        if (password.equals(password2)) {
+            System.out.println("Registration successful!");
+        } else {
+            System.out.println("Passwords do not match!");
+        }
+        nameToPassword.put(name, password); //store name as key and password as value
     }
 
     public static void login() {
         System.out.println("Please enter your name: ");
         String name = new Scanner(System.in).nextLine();
+        System.out.println("Please enter your password: ");
+        String password = new Scanner(System.in).nextLine();
+        if (nameToPassword.containsKey(name)) {
+            if (nameToPassword.get(name).equals(password)) {
+                System.out.println("Login successful!");
+            } else {
+                System.out.println("Wrong password!");
+            }
+        } else {
+            System.out.println("User does not exist!");
+        }
     }
 
     public static void exit() {
-        System.out.println("Thank you for using Duke App!");
+        System.out.println("Thank you for using Dr Duke!");
         System.exit(0);
     }
 }
