c11c86f406bc1044f0b93d84eb27b9aeeda5d9dd
lil1n <lilinongsc@gmail.com>
2023-03-08 23:35:42 +0800

    add generateReport method

--- src/main/java/seedu/rainyDay/rainyDay.java
+++ src/main/java/seedu/rainyDay/rainyDay.java
@@ -1,5 +1,8 @@
 package seedu.rainyDay;
 
+import seedu.rainyDay.data.FinancialStatement;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class rainyDay {
@@ -9,5 +12,30 @@ public class rainyDay {
 
         Scanner in = new Scanner(System.in);
         System.out.println("Hello " + in.nextLine());
+    }
+    public static String generateReport(ArrayList<FinancialStatement> financialReport) {
+        if (financialReport.size() == 0) {
+            return "Your financial report is empty";
+        }
+        int totalInflow = 0;
+        int totalOutflow = 0;
+        String financialStatements = "";
+        for (int i = 0; i < financialReport.size(); i += 1) {
+            if (financialReport.get(i).getFlowDirection().equals("in")) {
+                totalInflow += financialReport.get(i).getValue();
+            } else {
+                totalOutflow += financialReport.get(i).getValue();
+            }
+            int index = i + 1;
+            String financialStatement = String.join("", String.valueOf(index), ". ",
+                    financialReport.get(i).getFullStatement(), System.lineSeparator());
+            financialStatements = String.join("", financialStatements, financialStatement);
+        }
+        String inflowInformation = "Inflow: " + totalInflow;
+        String outflowInformation = "Outflow: " + totalOutflow;
+        String remainingValueInformation = "Remaining value: " + (totalInflow - totalOutflow);
+        String report = String.join(System.lineSeparator(), financialStatements, inflowInformation, outflowInformation,
+                remainingValueInformation);
+        return report;
     }
 }
