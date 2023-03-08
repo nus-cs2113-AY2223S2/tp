2d3bea69c909b9a48c1257ca0e87074173cc02e5
SSzeWen <seetszewen@gmail.com>
2023-03-08 22:05:02 +0800

    Add initialising of Storage and saving of newly added task to Storage

--- src/main/java/seedu/duke/Storage.java
+++ src/main/java/seedu/duke/Storage.java
@@ -1,6 +1,50 @@
 package seedu.duke;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Storage {
     private final String SAVED_MODULES_FILE_PATH = "./data/saved_modules.txt";
-    private final String SAVED_UNIVERSITIES_FILE_PATH = "./data/saved_universities.txt";
+
+    public ArrayList<Module> modules;
+
+    public Storage() {
+        this.modules = new ArrayList<>();
+        try {
+            initialiseMods();
+        } catch (IOException e) {
+            System.out.println("Initialise Saved Modules Failure");
+        }
+    }
+
+    private void initialiseMods() throws IOException {
+        File savedModulesFile = new File(SAVED_MODULES_FILE_PATH);
+        if (!savedModulesFile.exists()) {
+            savedModulesFile.createNewFile();
+            return;
+        }
+        DataReader.readModData(SAVED_MODULES_FILE_PATH, modules);
+    }
+
+    public void addModuleToModuleList(Module moduleToAdd) {
+        modules.add(moduleToAdd);
+        try {
+            saveModuleToStorage(moduleToAdd.toString());
+        } catch (IOException e) {
+            System.out.println("Save Module Failed");
+        }
+    }
+
+    private void saveModuleToStorage(String saveModuleString) throws IOException {
+        FileWriter fw = new FileWriter(SAVED_MODULES_FILE_PATH, true);
+        fw.write(writeTaskPreparation(saveModuleString));
+        fw.close();
+    }
+
+    private static String writeTaskPreparation(String saveString) {
+        return saveString + System.lineSeparator();
+    }
+
 }
