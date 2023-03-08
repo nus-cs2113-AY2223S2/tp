212455252c036cf3354701b48bd5b16b12955a5b
LINSHANG\linsh <e0774890@u.nus.edu>
2023-03-08 22:15:33 +0800

    Update automatic testing for .bat

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -8,8 +8,6 @@ cd ..
 
 cd text-ui-test
 
-cp save_test.txt save.txt
-
 java  -jar $(find ../build/libs/ -mindepth 1 -print -quit) < input.txt > ACTUAL.TXT
 
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
@@ -17,21 +15,9 @@ dos2unix EXPECTED-UNIX.TXT ACTUAL.TXT
 diff EXPECTED-UNIX.TXT ACTUAL.TXT
 if [ $? -eq 0 ]
 then
-    echo "Output: PASSED"
-    # compare save file to expected save file
-    diff save.txt save_result.txt
-    if [ $? -eq 0 ]
-    then
-        echo "Save file: PASSED"
-        echo "Test result: PASSED"
-        exit 0
-    else
-        echo "Save file: FAILED"
-        echo "Test result: FAILED"
-        exit 1
-    fi
+    echo "Test passed!"
+    exit 0
 else
-    echo "Output: FAILED"
-    echo "Test result: FAILED"
+    echo "Test failed!"
     exit 1
 fi
