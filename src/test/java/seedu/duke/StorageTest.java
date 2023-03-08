063b86dc32ec7b70b0c454ce1f870f65398485fe
SSzeWen <seetszewen@gmail.com>
2023-03-09 00:35:49 +0800

    Removed test for one module in database

--- src/test/java/seedu/duke/StorageTest.java
+++ src/test/java/seedu/duke/StorageTest.java
@@ -6,8 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StorageTest {
-
-    //Run addModule and then run initialise database
+    
 
     @Test
     @Order(1)
@@ -27,11 +26,5 @@ public class StorageTest {
         assertEquals(storage.modules.size(), 0);
     }
 
-    @Test
-    @Order(3)
-    public void initialiseStorage_oneModuleSaved_success() {
-        Storage storage = new Storage();
-        assertEquals(storage.modules.get(0).toString(), "1,AE320,Aerodynamics II,3,ME4231,Aerodynamics,4");
-    }
 
 }
