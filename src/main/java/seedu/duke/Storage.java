b4e8f36f2e6f111afe7edd28915acaa94712cb65
SSzeWen <seetszewen@gmail.com>
2023-03-08 19:05:50 +0800

    Create Storage Class

--- src/main/java/seedu/duke/Storage.java
+++ src/main/java/seedu/duke/Storage.java
@@ -1,7 +1,6 @@
 package seedu.duke;
 
 public class Storage {
-
     private final String SAVED_MODULES_FILE_PATH = "./data/saved_modules.txt";
     private final String SAVED_UNIVERSITIES_FILE_PATH = "./data/saved_universities.txt";
 }
