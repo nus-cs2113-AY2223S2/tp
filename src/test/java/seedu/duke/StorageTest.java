804dc1f9744abdaf57fb50afc940f7c44f26661d
SSzeWen <seetszewen@gmail.com>
2023-03-08 23:55:24 +0800

    Revised code to adhere to Coding Standard for StorageTest

--- src/test/java/seedu/duke/StorageTest.java
+++ src/test/java/seedu/duke/StorageTest.java
@@ -14,10 +14,10 @@ public class StorageTest {
     @Order(1)
     public void addModule_correctFormat_success() {
         Storage storage = new Storage();
-        Module module = new Module(1,"AE320", "Aerodynamics II", 3,
+        Module module = new Module(1, "AE320", "Aerodynamics II", 3,
                 "ME4231", "Aerodynamics", 4);
         storage.addModuleToModuleList(module);
-        int lastIndex = storage.modules.size()-1;
+        int lastIndex = storage.modules.size() - 1;
         assertEquals(storage.modules.get(lastIndex).toString(), "1,AE320,Aerodynamics II,3,ME4231,Aerodynamics,4");
     }
 
@@ -25,14 +25,14 @@ public class StorageTest {
     @Order(2)
     public void initialiseStorage_NoModuleSaved_success() {
         Storage storage = new Storage();
-        assertEquals(storage.modules.size(),0);
+        assertEquals(storage.modules.size(), 0);
     }
 
     @Test
     @Order(3)
     public void initialiseStorage_oneModuleSaved_success() {
         Storage storage = new Storage();
-        assertEquals(storage.modules.get(0).toString(),"1,AE320,Aerodynamics II,3,ME4231,Aerodynamics,4");
+        assertEquals(storage.modules.get(0).toString(), "1,AE320,Aerodynamics II,3,ME4231,Aerodynamics,4");
     }
 
 }
