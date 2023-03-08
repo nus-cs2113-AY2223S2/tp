46caddf801791038dc43b3d395f79a5dff3921f6
HiIAmTzeKean <ngtzekean600@gmail.com>
2023-03-08 23:18:21 +0800

    feat: command package

---
+++ src/main/java/seedu/dukeofbooks/command/ExitCommand.java
@@ -0,0 +1,11 @@
+package seedu.dukeofbooks.command;
+
+public class ExitCommand extends Command{
+    public static final String COMMAND_WORD = "exit";
+
+    @Override
+    public CommandResult execute() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'execute'");
+    }
+}
