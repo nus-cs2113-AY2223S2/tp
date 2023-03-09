6241521421e126b917fc62e23ff53b22f507cb7c
pyongjie <pyjavatar@gmail.com>
2023-03-09 21:36:07 +0800

    Add variables and constructor for Pet

--- src/main/java/seedu/pettracker/data/Pet.java
+++ src/main/java/seedu/pettracker/data/Pet.java
@@ -1,4 +1,15 @@
 package seedu.pettracker.data;
 
 public class Pet {
+    protected String petName;
+    protected String petType;
+    protected String age;
+    protected String weight;
+
+    public Pet(String petName) {
+        this.petName = petName;
+        petType = "";
+        age = "";
+        weight = "";
+    }
 }
