f1adc2910b675aa4b546b34399b1cf827a948727
BenjaminPoh <e0726717@u.nus.edu>
2023-03-09 19:06:17 +0800

    Added UI Class to handle interactions with the user

---
+++ src/main/java/seedu/rainyDay/UI.java
@@ -0,0 +1,22 @@
+package seedu.rainyDay;
+
+import java.util.Scanner;
+
+public class UI {
+
+    private static String username;
+
+    public static void printLogo () {
+        System.out.println("Hello from rainyDay");
+        System.out.println("What is your name?");
+    }
+
+    public static void greetUser (String name) {
+        username = name;
+        System.out.println("Welcome " + username);
+    }
+
+    public static void sayFarewellToUser () {
+        System.out.println("Bye" + username);
+    }
+}
--- src/main/java/seedu/rainyDay/rainyDay.java
+++ src/main/java/seedu/rainyDay/rainyDay.java
@@ -7,11 +7,9 @@ import java.util.Scanner;
 
 public class rainyDay {
     public static void main(String[] args) {
-        System.out.println("Hello from rainyDay\n");
-        System.out.println("What is your name?");
-
         Scanner in = new Scanner(System.in);
-        System.out.println("Hello " + in.nextLine());
+        UI.printLogo();
+        UI.greetUser(in.nextLine());
     }
 
     public static String addFinancialStatement(String description, String flowDirection, int value,
