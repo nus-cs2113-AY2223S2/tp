f7c236de196b056375f4ea1577a6248a297a555f
thomasjlalba <e0773507@u.nus.edu>
2023-03-09 17:25:53 +0800

    Update Expense.java and ExpenseList.java to use spaces instead of tabs

--- src/main/java/seedu/duke/data/expense/Expense.java
+++ src/main/java/seedu/duke/data/expense/Expense.java
@@ -1,29 +1,29 @@
 package seedu.duke.data.expense;
 
 public class Expense {
-	private String description;
-	private String date; // to change to timestamp in the future
-	private float value;
-	
-	public Expense(String description, String date, float value) {
-		this.description = description;
-		this.date = date;
-		this.value = value;
-	}
-	
-	public String getDescription() {
-		return description;
-	}
-	
-	public String getDate() {
-		return date;
-	}
-	
-	public float getValue() {
-		return value;
-	}
-	
-	public String toString() {
-		return "Expense - " + getDescription() + " - Date - " + getDate() + " - value: - " + getValue();
-	}
+    private String description;
+    private String date; // to change to timestamp in the future
+    private float value;
+    
+    public Expense(String description, String date, float value) {
+        this.description = description;
+        this.date = date;
+        this.value = value;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public String getDate() {
+        return date;
+    }
+    
+    public float getValue() {
+        return value;
+    }
+    
+    public String toString() {
+        return "Expense - " + getDescription() + " - Date - " + getDate() + " - value: - " + getValue();
+    }
 }
--- src/main/java/seedu/duke/data/expense/ExpenseList.java
+++ src/main/java/seedu/duke/data/expense/ExpenseList.java
@@ -3,32 +3,32 @@ package seedu.duke.data.expense;
 import java.util.ArrayList;
 
 public class ExpenseList {
-	private ArrayList<Expense> expenseList;
-	private int expenseCount;
-	
-	
-	public ExpenseList() {
-		expenseList = new ArrayList<Expense>();
-		expenseCount = 0;
-	}
-	
-	public ArrayList<Expense> getExpenseList() {
-		return expenseList;
-	}
-	
-	public int getExpenseCount() {
-		return expenseCount;
-	}
-	
-	public void addExpense(Expense expense) {
-		expenseList.add(expense);
-		expenseCount++;
-	}
-	
-	public void printExpenseList() {
-		for (int i = 1; i <= expenseCount; i++) {
-			Expense expense = expenseList.get(i - 1);
-			System.out.println(i + ". " + expense.toString());
-		}
-	}
+    private ArrayList<Expense> expenseList;
+    private int expenseCount;
+    
+    
+    public ExpenseList() {
+        expenseList = new ArrayList<Expense>();
+        expenseCount = 0;
+    }
+    
+    public ArrayList<Expense> getExpenseList() {
+        return expenseList;
+    }
+    
+    public int getExpenseCount() {
+        return expenseCount;
+    }
+    
+    public void addExpense(Expense expense) {
+        expenseList.add(expense);
+        expenseCount++;
+    }
+    
+    public void printExpenseList() {
+        for (int i = 1; i <= expenseCount; i++) {
+            Expense expense = expenseList.get(i - 1);
+            System.out.println(i + ". " + expense.toString());
+        }
+    }
 }
