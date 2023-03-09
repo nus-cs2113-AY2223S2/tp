20fd49f5dffbf5db0d57a2fedc4d0ec817b8bd2b
avielcx <avielcx@gmail.com>
2023-03-09 20:10:05 +0800

    Exit test, ExpenseList count test

---
+++ src/test/java/seedu/duke/data/expense/ExpenseListTest.java
@@ -0,0 +1,17 @@
+package seedu.duke.data.expense;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExpenseListTest {
+
+    @Test
+    void getExpenseCount_one_expectOne() {
+
+        Expense expenseOne = new Expense("beach party","23 may 2023", 50);
+        ExpenseList list = new ExpenseList();
+        list.addExpense(expenseOne);
+        assertEquals(1,list.getExpenseCount());
+    }
+}
