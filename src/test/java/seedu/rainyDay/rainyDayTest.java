6d5910da430af6c8af5533559fc80df5af5da384
lil1n <lilinongsc@gmail.com>
2023-03-09 11:37:11 +0800

    add JUnit test for addFinancialStatement method

--- src/test/java/seedu/rainyDay/rainyDayTest.java
+++ src/test/java/seedu/rainyDay/rainyDayTest.java
@@ -1,12 +1,25 @@
 package seedu.rainyDay;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.rainyDay.rainyDay.addFinancialStatement;
 
 import org.junit.jupiter.api.Test;
+import seedu.rainyDay.data.FinancialStatement;
+
+import java.util.ArrayList;
 
 class rainyDayTest {
     @Test
     public void sampleTest() {
         assertTrue(true);
+    }
+
+    @Test
+    public void verifyAddStatement() {
+        ArrayList<FinancialStatement> financialReport = new ArrayList<FinancialStatement>();
+        String actualAddStatement = addFinancialStatement("Ipad", "out", 120, financialReport);
+        String expectedAddStatement = "Done, added: out for Ipad, $120";
+        assertEquals(expectedAddStatement, actualAddStatement);
     }
 }
