60ad8e7373859cd8a0fa080eb16de810450c3bf2
thomasjlalba <e0773507@u.nus.edu>
2023-03-09 16:46:51 +0800

    Update Duke.java to run empty command loop until 'bye' is written

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -3,19 +3,47 @@ package seedu.duke;
 import java.util.Scanner;
 
 public class Duke {
+    
+    public static final String DIVIDER = "____________________________________________________________\n";
+    
     /**
      * Main entry-point for the java.duke.Duke application.
      */
     public static void main(String[] args) {
+        greet();
+        runCommandLoopUntilExitCommand();
+        exit();
+    }
+    
+    private static void greet() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println("What is your name?");
-
+        System.out.println("What can I do for you today?");
+        System.out.println(DIVIDER);
+    }
+    
+    private static void runCommandLoopUntilExitCommand() {
         Scanner in = new Scanner(System.in);
-        System.out.println("Hello " + in.nextLine());
+        String line = in.nextLine();
+        
+        // continuously reads input from command line until command 'bye' is inputted
+        while (!line.equalsIgnoreCase("bye")) {
+            System.out.println(DIVIDER);
+            runCommand(line);
+            System.out.println(DIVIDER);
+            line = in.nextLine();
+        }
+    }
+    
+    private static void runCommand(String line) {
+        //
+    }
+    
+    private static void exit() {
+        System.out.println("Bye!");
     }
 }
