c2a2e81dfc57bda7e93506757bf5d42a4e524ff0
clement559 <clementsiut@gmail.com>
2023-03-09 21:36:43 +0800

    Half-attempted merge conflict

---
+++ src/main/java/seedu/duke/task/Task.java
@@ -0,0 +1,34 @@
+package seedu.duke.task;
+
+public class Task {
+    private String description;
+    private String deadline;
+    private boolean isDone;
+    public Task(String description, String deadline) {
+        this.description = description;
+        this.deadline = deadline;
+        this.isDone = false;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public String getDeadline() {
+        return this.deadline;
+    }
+
+    public boolean getIsDone() {
+        return this.isDone;
+    }
+    public void setDone(boolean isDone) {
+        this.isDone = isDone;
+    }
+    public void editDeadline (String deadline) {
+        this.deadline = deadline;
+    }
+
+    public String toString() {
+        return "[" + (isDone ? "X" : " ") + "] " + description + "due by: " + deadline;
+    }
+}
