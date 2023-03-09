65adaf3ab5d0c7c3d7a1d67298a53f16ff574900
BenjaminPoh <e0726717@u.nus.edu>
2023-03-09 22:44:49 +0800

    Implemented basic JTesting for UI class

---
+++ src/test/java/seedu/rainyDay/UITest.java
@@ -0,0 +1,38 @@
+package seedu.rainyDay;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+class UITest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+
+    @Test
+    void checkGreetingPrinted() {
+        setUpStreams();
+        String greeting = "Welcome pekopekopeko\r\n";
+        UI.greetUser("pekopekopeko");
+        assertEquals(greeting, outContent.toString());
+        restoreStreams();
+    }
+}
