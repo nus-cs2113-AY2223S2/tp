c6bafdbe076c79b441bccc964db2d10947a28863
kyrixn <keyrixn@gmail.com>
2023-03-09 21:07:58 +0800

    fixed bugs, still haven't finished revise time

--- src/main/java/seedu/duke/Event.java
+++ src/main/java/seedu/duke/Event.java
@@ -7,20 +7,26 @@ public class Event {
     private String description;
     private LocalDateTime startTime;
     private LocalDateTime endTime;
+    private boolean hasEndInfo;
+    private boolean hasStartTime;
     private boolean hasEndTime;
 
-    static SimpleDateFormat dateTimeFormatter = new SimpleDateFormat("MMM/dd/yyyy HH:mm");
+    static SimpleDateFormat dateTimeFormatter = new SimpleDateFormat("yyyy/MM/dd HH:mm");
 
-    public Event(String eventDescription, LocalDateTime start, LocalDateTime End) {
+    public Event(String eventDescription, LocalDateTime start, LocalDateTime End, boolean hasSt, boolean hasEd) {
         this.description = eventDescription;
         this.startTime = start;
         this.endTime = End;
-        this.hasEndTime = true;
+        this.hasEndInfo = true;
+        this.hasStartTime = hasSt;
+        this.hasEndTime = hasEd;
     }
 
-    public Event(String eventDescription, LocalDateTime start) {
+    public Event(String eventDescription, LocalDateTime start, boolean hasSt) {
         this.description = eventDescription;
         this.startTime = start;
+        this.hasEndTime = false;
+        this.hasStartTime = hasSt;
         this.hasEndTime = false;
     }
 
--- src/main/java/seedu/duke/EventList.java
+++ src/main/java/seedu/duke/EventList.java
@@ -1,14 +1,17 @@
 package seedu.duke;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class EventList {
     protected ArrayList<Event> taskList;
     protected int listSize;
-    private static DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+    private static DateTimeFormatter dfWithTime = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");
 
+    private static String DTINIT = "2000/01/01 01:01";
+    
     public EventList() {
         this.taskList = new ArrayList<Event>();
         this.listSize = 0;
@@ -33,25 +36,53 @@ public class EventList {
     }
 
     private LocalDateTime changeToDate(String time, String date) {
-        String combination = time + " " + date;
-        return LocalDateTime.parse(combination, df);
+        String combination = date + " " + time;
+        return LocalDateTime.parse(combination, dfWithTime);
     }
 
-    private LocalDateTime ChangeToDate(String date) {
-        return LocalDateTime.parse(date, df);
+    private LocalDateTime changeToDate(String date) {
+        return LocalDateTime.parse(date + " 00:00", dfWithTime);
     }
 
     public void addEvent(String description, String startTime, String startDay, String endTime,
             String endDay) {
-        Event newEvent =
-                new Event(description, changeToDate(startTime, startDay), changeToDate(endTime, endDay));
+
+        boolean hasStTime = true;
+        boolean hasEdTime = false;
+        LocalDateTime combinedStartTime = LocalDateTime.parse(DTINIT, dfWithTime);
+        LocalDateTime combinedEndTime = LocalDateTime.parse(DTINIT, dfWithTime);
+
+        if (startTime.equals("")) {
+            hasStTime = false;
+            combinedStartTime = changeToDate(startDay);
+        } else {
+            combinedStartTime = changeToDate(startTime, startDay);
+        }
+
+        if (endTime.equals("")) {
+            hasEdTime = false;
+            combinedEndTime = changeToDate(endDay);
+        } else{
+            combinedEndTime = changeToDate(startTime, startDay);
+        }
+
+        Event newEvent = new Event(description, combinedStartTime, combinedEndTime, hasStTime, hasEdTime);
         taskList.add(newEvent);
         listSize++;
     }
 
     public void addEvent(String description, String startTime, String startDay) {
-        Event newEvent =
-                new Event(description, changeToDate(startTime, startDay));
+        boolean hasStTime = true;
+        LocalDateTime combinedStartTime = LocalDateTime.parse(DTINIT, dfWithTime);
+
+        if (startTime.equals("")) {
+            hasStTime = false;
+            combinedStartTime = changeToDate(startDay);
+        } else {
+            combinedStartTime = changeToDate(startTime, startDay);
+        }
+
+        Event newEvent = new Event(description, combinedStartTime, hasStTime);
         taskList.add(newEvent);
         listSize++;
     }
