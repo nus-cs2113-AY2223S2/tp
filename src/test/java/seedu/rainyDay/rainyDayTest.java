dc22f371ab75bec1c8d55a5f4d0a621e02b2e2cb
ChongQiRong <e07272229@u.nus.edu>
2023-03-09 16:43:31 +0800

    Add JUnit Testing for writeToFile Function

--- src/test/java/seedu/rainyDay/rainyDayTest.java
+++ src/test/java/seedu/rainyDay/rainyDayTest.java
@@ -1,18 +1,23 @@
 package seedu.rainyDay;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.rainyDay.rainyDay.addFinancialStatement;
+import static seedu.rainyDay.rainyDay.generateReport;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import seedu.rainyDay.data.FinancialStatement;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.util.ArrayList;
 
 class rainyDayTest {
     @Test
     public void sampleTest() {
-        assertTrue(true);
+        Assertions.assertTrue(true);
     }
 
     @Test
@@ -23,5 +28,27 @@ class rainyDayTest {
         assertEquals(expectedAddStatement, actualAddStatement);
     }
 
+    @Test
+    void writeToFileTest_fileExists() {
+        ArrayList<FinancialStatement> financialReport = new ArrayList<>();
+        String filePath = "rainyDay.txt";
+        rainyDay.writeToFile(financialReport, filePath);
+        Assertions.assertTrue(new File(filePath).exists());
+    }
 
+    @Test
+    void writeToFileTest_contentMatch() throws IOException, ClassNotFoundException {
+        ArrayList<FinancialStatement> financialReport = new ArrayList<>();
+        String filePath = "rainyDay.txt";
+        financialReport.add(new FinancialStatement("noodles", "in", 5));
+        rainyDay.writeToFile(financialReport, filePath);
+
+        FileInputStream readData = new FileInputStream(filePath);
+        ObjectInputStream readStream = new ObjectInputStream(readData);
+        @SuppressWarnings("unchecked")
+        ArrayList<FinancialStatement> data = (ArrayList<FinancialStatement>) readStream.readObject();
+        readStream.close();
+
+        assertEquals(generateReport(financialReport), generateReport(data));
+    }
 }
