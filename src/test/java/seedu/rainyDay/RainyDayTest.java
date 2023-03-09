cce197ceb6b8da62435f008687d81bee1f12dea9
azriellee <azriel20005@gmail.com>
2023-03-10 02:24:10 +0800

    Update JUnit tests for parser and parser to throw exceptions for wrong user input

--- src/test/java/seedu/rainyDay/RainyDayTest.java
+++ src/test/java/seedu/rainyDay/RainyDayTest.java
@@ -3,6 +3,7 @@ package seedu.rainyDay;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static seedu.rainyDay.RainyDay.loadFromFile;
+import static seedu.rainyDay.RainyDay.parseUserInput;
 
 import seedu.rainyDay.data.FinancialReport;
 import seedu.rainyDay.data.FinancialStatement;
@@ -16,6 +17,48 @@ import java.io.ObjectInputStream;
 import java.util.ArrayList;
 
 class RainyDayTest {
+
+    @Test
+    public void parseAddInCommand() {
+        try {
+            ArrayList<FinancialStatement> statements = new ArrayList<>();
+            FinancialReport testReport = new FinancialReport(statements);
+            testReport.addStatement(new FinancialStatement("noodles", "in", 5));
+            RainyDay.parseUserInput("add -in noodles $5");
+            assertEquals(RainyDay.financialReport.getFullStatement(0), testReport.getFullStatement(0));
+        } catch (Exception e) {
+            System.out.println("Wrong input format! Please refer to help for correct user input!");
+        }
+    }
+
+    @Test
+    public void parseAddOutCommand() {
+        try {
+            ArrayList<FinancialStatement> statements = new ArrayList<>();
+            FinancialReport testReport = new FinancialReport(statements);
+            testReport.addStatement(new FinancialStatement("noodles", "out", 5));
+            RainyDay.parseUserInput("add -out noodles $5");
+            assertEquals(RainyDay.financialReport.getFullStatement(0), testReport.getFullStatement(0));
+        } catch (Exception e) {
+            System.out.println("Wrong input format! Please refer to help for correct user input!");
+        }
+    }
+
+    @Test
+    public void parseDeleteCommand() {
+        RainyDay.clearFinancialReport();
+        RainyDay.addFinancialStatement("Ipad", "out", 120);
+        RainyDay.addFinancialStatement("angpao", "in", 3000);
+        ArrayList<FinancialStatement> statements = new ArrayList<>();
+        FinancialReport testReport = new FinancialReport(statements);
+        testReport.addStatement(new FinancialStatement("Ipad", "out", 120));
+        try {
+            RainyDay.parseUserInput("delete 2");
+            assertEquals(RainyDay.financialReport.getFullStatement(0), testReport.getFullStatement(0));
+        } catch (Exception e) {
+            System.out.println("Wrong input format! Please refer to help for correct user input!");
+        }
+    }
 
     @Test
     public void addFinancialStatement() {
