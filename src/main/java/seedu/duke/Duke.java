35afcdc9e95df3da0b6023c086964f4daf63bf28
MatthewLiuZJ <matthew.liu.zhenjie@gmail.com>
2023-03-09 11:11:18 +0800

    Update Duke.java

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -1,21 +1,35 @@
 package seedu.duke;
 
 import java.util.Scanner;
+//import seedu.duke.ui.Ui;
+//import seedu.duke.EventList;
+//import seedu.duke.parser.Parser;
 
 public class Duke {
     /**
      * Main entry-point for the java.duke.Duke application.
      */
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("What is your name?");
+//    private final TaskList tasks;
+//    private final Ui ui;
+//    private final Parser parser;
+    public Duke (){
+//        ui = new Ui();
+//        tasks = new TaskList();
+//        parser = new Parser();
+    }
+    public void run(){
+//        ui.showWelcome();
+//        boolean isExit = false;
+//        while(!isExit){
+//            String fullCommand = ui.getUserCommand();
+//            parser.parseInput(fullCommand, tasks);
+//            if (fullCommand.equalsIgnoreCase("bye")){
+//                isExit = true;
+//            }
+//        }
+    }
 
-        Scanner in = new Scanner(System.in);
-        System.out.println("Hello " + in.nextLine());
+    public static void main(String[] args) {
+        new Duke().run();
     }
 }
