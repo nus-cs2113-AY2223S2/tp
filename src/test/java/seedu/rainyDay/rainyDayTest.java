ef1e730d3a61a7d3df931a027770009e864a2621
lil1n <lilinongsc@gmail.com>
2023-03-09 16:56:43 +0800

    add JUnit test for generateReport method

--- src/test/java/seedu/rainyDay/rainyDayTest.java
+++ src/test/java/seedu/rainyDay/rainyDayTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.rainyDay.rainyDay.addFinancialStatement;
 import static seedu.rainyDay.rainyDay.deleteFinancialStatement;
+import static seedu.rainyDay.rainyDay.generateReport;
 
 import org.junit.jupiter.api.Test;
 
@@ -28,4 +29,19 @@ class rainyDayTest {
         assertEquals(expectedDeleteStatement, actualDeleteStatement);
     }
 
+    @Test
+    public void verifyReport() {
+        String actualReport = generateReport();
+        String expectedReport = "Your financial report is empty";
+        assertEquals(expectedReport, actualReport);
+
+        addFinancialStatement("Ipad", "out", 120);
+        addFinancialStatement("pork", "out", 5);
+        addFinancialStatement("angpao", "in", 3000);
+        actualReport = generateReport();
+        expectedReport = String.join(System.lineSeparator(), "1. Ipad -$120 (out)", "2. pork -$5 (out)",
+                "3. angpao +$3000 (in)" + System.lineSeparator(), "Inflow: $3000", "Outflow: $125",
+                "Remaining value: $2875");
+        assertEquals(expectedReport, actualReport);
+    }
 }
