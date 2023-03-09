a3cdca43b909e1888e3fcadcee0f327d97964695
Toh-HongFeng <hf.toh2@gmail.com>
2023-03-09 22:34:52 +0800

    add JUnit tests for Storage.java

--- src/main/java/seedu/duke/Storage.java
+++ src/main/java/seedu/duke/Storage.java
@@ -12,19 +12,29 @@ public class Storage {
      * Divider used to separate details of a task saved in file
      */
     private static final String SAVEFILESEPARATOR = ", ";
-    private static String filePath = "EventList.txt";
     private static File textFile;
     private static Scanner textFileScanner;
+    private static String filePath = "EventList.txt";
 
+    /**
+     * Constructor for Storage class
+     */
     public Storage() {
-        loadFromFile();
+        setupFile();
     }
 
     /**
-     * Sets up the file to be read
+     * Sets up the file to be read and written to
      */
     private static void setupFile() {
         textFile = new File(filePath);
+        
+        // create file if it does not exist
+        try {
+            textFile.createNewFile();
+        } catch (IOException e) {
+            System.out.println("The file already exists.");
+        }
 
         try {
             textFileScanner = new Scanner(textFile);
@@ -34,13 +44,13 @@ public class Storage {
     }
     
     /**
-     * Saves an ArrayList of tasks to DukeTaskList.txt file
+     * Saves an ArrayList of Events to EventList.txt file
      * 
-     * @param list ArrayList of tasks
+     * @param list ArrayList of Events
      */
-    public static void saveToFile(ArrayList<Event> list) {
+    public void saveToFile(ArrayList<Event> list) {
         String writeToFile = "";
-        
+        // TODO: Object to string conversion
         for (Event dukeTasks : list) {
             // extracting details from task object
             String[] dataToTextFile= {"", "", ""};
@@ -53,7 +63,6 @@ public class Storage {
 
         // write task list to text file
         try {
-            textFile = new File(filePath);
             FileWriter dukeWriter = new FileWriter(textFile);
             dukeWriter.write(writeToFile);
             dukeWriter.close();
@@ -63,24 +72,25 @@ public class Storage {
     }
 
     /**
-     * Returns an ArrayList of tasks from DukeTaskList.txt file
+     * Returns an ArrayList of Events from EventList.txt file
      * 
-     * @return ArrayList of tasks
+     * @return ArrayList of Events
      */
-    public static ArrayList<Event> loadFromFile() {
+    public ArrayList<Event> loadFromFile() {
         ArrayList<Event> savedList = new ArrayList<>();
-        setupFile();
+        //setupFile();
 
         while (textFileScanner.hasNext()) {
+            // TODO: Loading of objects from file
             String[] loadTaskInfo = new String[3];
             loadTaskInfo = textFileScanner.nextLine().split(SAVEFILESEPARATOR, 3);
             
             // create tasks individually
             switch (loadTaskInfo[0]) {
-            case "T":
+            case "CATEGORY":
                 break;
         
-            case "D":
+            case "EVENT":
                 break;
         
             default:
---
+++ src/test/java/seedu/duke/StorageTest.java
@@ -0,0 +1,72 @@
+package seedu.duke;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+public class StorageTest {
+    /** 
+     * Tests setupFile() method<p>
+     * Expected outcome: No exception thrown
+     */
+    @Test
+    public void setupFile_null_noExceptionThrown() {
+        assertDoesNotThrow(() -> {
+            Storage storage = new Storage();
+            System.out.println(storage.getClass());
+        });
+    }
+
+    /**
+     * Tests saveToFile() method<p>
+     * Input: ArrayList of 1 Event(description: "test", budget: 1234, expense: 5678)<p>
+     * Expected outcome: No exception thrown
+     */
+    @Test
+    public void saveToFile_descInput_exptOutcome() {
+        Storage storage = new Storage();
+        ArrayList<Event> list = new ArrayList<>();
+        list.add(new Event("test", 1234, 5678));
+        assertDoesNotThrow(() -> {
+            storage.saveToFile(list);
+        });
+    }
+
+    /** 
+     * Tests loadFromFile() method<p>
+     * Expected outcome: No exception thrown
+     */
+    @Test
+    public void loadFromFile_null_noExceptionThrown() {
+        try {
+            Storage storage = new Storage();
+            ArrayList<Event> list = storage.loadFromFile();
+            System.out.println(list);
+        } catch (Exception e) {
+            assertTrue(false, e.getMessage());
+        }
+    }
+
+    /** 
+     * Tests save and load from file sequence<p>
+     * Input: ArrayList of Events<p>
+     * Expected outcome: No exception thrown
+     */
+    @Test
+    public void saveAndLoadFromFile_descInput_exptOutcome() {
+        try {
+            Storage storage = new Storage();
+            ArrayList<Event> list = new ArrayList<>();
+            list.add(new Event("test", 1234, 5678));
+            list.add(new Event("test2", 9876, 5432));
+            storage.saveToFile(list);
+            ArrayList<Event> list2 = storage.loadFromFile();
+            System.out.println(list2);
+        } catch (Exception e) {
+            assertTrue(false, e.getMessage());
+        }
+    }
+}
