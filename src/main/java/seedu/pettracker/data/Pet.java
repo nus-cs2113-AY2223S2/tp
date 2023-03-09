fc28a52e8f07cdddfa161b808104004e47f1e995
pyongjie <pyjavatar@gmail.com>
2023-03-09 21:38:34 +0800

    Add getters and setters for Pet

--- src/main/java/seedu/pettracker/data/Pet.java
+++ src/main/java/seedu/pettracker/data/Pet.java
@@ -12,4 +12,32 @@ public class Pet {
         age = "";
         weight = "";
     }
+
+    public String getPetName() {
+        return petName;
+    }
+
+    public String getPetType() {
+        return petType;
+    }
+
+    public void setPetType(String petType) {
+        this.petType = petType;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
 }
