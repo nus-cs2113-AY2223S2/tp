8258fdbe5f00a87a7363003fa763e80b03a7b256
Poobalan Aatmika Lakshmi <88182404+PoobalanAatmikaLakshmi@users.noreply.github.com>
2023-03-09 20:32:00 +0800

    Update ParserTest.java

--- src/test/java/seedu/apollo/ParserTest.java
+++ src/test/java/seedu/apollo/ParserTest.java
@@ -12,7 +12,7 @@ import java.time.format.DateTimeFormatter;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertNull;
+
 
 
 class ParserTest {
