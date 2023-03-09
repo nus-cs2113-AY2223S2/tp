4ca981bc69af0f8afb278ffb737f743395b411ab
Mitch Malvin <e0773911@u.nus.edu>
2023-03-09 18:14:12 +0800

    write skeleton unit test for UserInterface

---
+++ src/test/java/utils/userinterface/UserInterfaceTest.java
@@ -0,0 +1,25 @@
+package utils.userinterface;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserInterfaceTest {
+
+    @Test
+    void getCommand() {
+
+    }
+
+    @Test
+    void printLine() {
+    }
+
+    @Test
+    void printGreeting() {
+    }
+
+    @Test
+    void printBye() {
+    }
+}
