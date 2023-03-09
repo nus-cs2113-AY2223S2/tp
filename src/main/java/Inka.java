e5031e8314c5312fce5c2645921e59cefe76f934
Mitch Malvin <e0773911@u.nus.edu>
2023-03-09 12:11:02 +0800

    add skeleton code for parser

--- src/main/java/Inka.java
+++ src/main/java/Inka.java
@@ -1,16 +1,22 @@
+import utils.parser.Parser;
 import utils.userinterface.UserInterface;
 
 public class Inka {
     private final UserInterface ui;
+    private final Parser parser;
     public Inka() {
         ui = new UserInterface();
+        parser = new Parser();
     }
 
     public void run() {
         ui.printGreeting();
 
-        //while parser.execute is running code should be placed here,
-        // String fullCommand = ui.getCommand();
+//        fill in the method here
+//        while(parser.getIsExecuting()) {
+//            String fullCommand = ui.getCommand();
+//        }
+
         ui.printBye();
     }
 
