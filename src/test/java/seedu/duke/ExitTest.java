7cf04f69407ada74f853a16407520c979a9749c7
avielcx <avielcx@gmail.com>
2023-03-09 21:37:24 +0800

    Update test file

--- src/test/java/seedu/duke/ExitTest.java
+++ src/test/java/seedu/duke/ExitTest.java
@@ -1,12 +1,9 @@
 package seedu.duke;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ExitTest {
 
