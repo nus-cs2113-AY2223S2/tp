94dd1b70165a8b923f9456ee057f7c9f356f2c88
Matthew Liu <matthew.liu.zhenjie@gmail.com>
2023-03-10 21:02:52 +0800

    Fix checkstyle error in Duke.java

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -1,6 +1,6 @@
 package seedu.duke;
 
-import java.util.Scanner;
+//import java.util.Scanner;
 //import seedu.duke.ui.Ui;
 //import seedu.duke.EventList;
 //import seedu.duke.parser.Parser;
@@ -9,24 +9,24 @@ public class Duke {
     /**
      * Main entry-point for the java.duke.Duke application.
      */
-//    private final TaskList tasks;
-//    private final Ui ui;
-//    private final Parser parser;
+    //private final TaskList tasks;
+    //private final Ui ui;
+    //private final Parser parser;
     public Duke (){
-//        ui = new Ui();
-//        tasks = new TaskList();
-//        parser = new Parser();
+        //ui = new Ui();
+        //tasks = new TaskList();
+        //parser = new Parser();
     }
     public void run(){
-//        ui.showWelcome();
-//        boolean isExit = false;
-//        while(!isExit){
-//            String fullCommand = ui.getUserCommand();
-//            parser.parseInput(fullCommand, tasks);
-//            if (fullCommand.equalsIgnoreCase("bye")){
-//                isExit = true;
-//            }
-//        }
+        //ui.showWelcome();
+        //boolean isExit = false;
+        //while(!isExit){
+            //String fullCommand = ui.getUserCommand();
+            //parser.parseInput(fullCommand, tasks);
+            //if (fullCommand.equalsIgnoreCase("bye")){
+                //isExit = true;
+            //}
+        //}
     }
 
     public static void main(String[] args) {
