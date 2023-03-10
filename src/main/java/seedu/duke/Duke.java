a7da4c05448d65f7e50d66d1d2994c721a468426
matthew-liu-zhenjie <matthew.liu.zhenjie@gmail.com>
2023-03-10 21:34:22 +0800

    Update Duke.java
    Fix main function.

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -1,9 +1,10 @@
 package seedu.duke;
 
-//import java.util.Scanner;
+import java.util.Scanner;
 //import seedu.duke.ui.Ui;
 //import seedu.duke.EventList;
 //import seedu.duke.parser.Parser;
+
 
 public class Duke {
     /**
@@ -30,6 +31,16 @@ public class Duke {
     }
 
     public static void main(String[] args) {
-        new Duke().run();
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("What is your name?");
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Hello " + in.nextLine());
     }
+}
 }
