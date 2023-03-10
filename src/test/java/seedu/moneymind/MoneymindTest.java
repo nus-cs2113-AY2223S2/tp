f0f5404e281595fbd1806a9eea40c66576a93c62
Li Mingyuan <e0773670@u.nus.edu>
2023-03-10 15:39:46 +0800

    Add test cases for main class

---
+++ src/test/java/seedu/moneymind/MoneymindTest.java
@@ -0,0 +1,20 @@
+package seedu.moneymind;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+public class MoneymindTest {
+    @Test
+    public void initialize_null_noExceptionThrown() {
+        assertDoesNotThrow(() -> {
+            Moneymind moneymind = new Moneymind();
+        });
+    }
+
+    @Test
+    public void run_null_noExceptionThrown() {
+        Moneymind moneymind = new Moneymind();
+        assertDoesNotThrow(moneymind::run);
+    }
+}
