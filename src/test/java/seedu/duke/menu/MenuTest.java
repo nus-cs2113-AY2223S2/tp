a042a4b1ab66e8adb325a864e7d83ecdb1e7f00d
JeraldChen <jeraldchenjunhan@gmail.com>
2023-03-10 23:44:17 +0800

    Add JUnit Test for register method in Menu Class

---
+++ src/test/java/seedu/duke/menu/MenuTest.java
@@ -0,0 +1,24 @@
+package seedu.duke.menu;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MenuTest {
+    @Test
+    public void registerPatient() {
+        String name = "Akshay";
+        String password1 = "iloveCS2113";
+        String password2 = "iloveCS2113";
+
+        HashMap<String, String> personalInfo = new HashMap<>();
+        if (password1 == password2) {
+            personalInfo.put(name, password1);
+        }
+
+        assertEquals(personalInfo.get(name), password1);
+    }
+
+}
