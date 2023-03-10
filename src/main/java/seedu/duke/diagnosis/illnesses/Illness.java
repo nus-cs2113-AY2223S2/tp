eff211925368916e04f17f796b12f61468eb03eb
Brennanzuz <timingout365@gmail.com>
2023-03-10 13:57:59 +0800

    Added illnesses and symptoms.
    Added Illness class to be inherited by all possible illnesses listed by Dr. Duke.
    Added a Symptoms enum to list down all possible reactions to the illness the patient may be facing.
    Added a Diagnosis class to be used as a way to convert a list of observed symptoms to a list of possible illnesses the patient may be facing.

---
+++ src/main/java/seedu/duke/diagnosis/illnesses/Illness.java
@@ -0,0 +1,10 @@
+package seedu.duke.diagnosis.illnesses;
+
+import seedu.duke.diagnosis.symptoms.Symptom;
+
+import java.util.ArrayList;
+
+//@@author Brennanzuz
+public class Illness {
+
+}
---
+++ src/main/java/seedu/duke/diagnosis/illnesses/allergic/Asthma.java
@@ -0,0 +1,15 @@
+package seedu.duke.diagnosis.illnesses.allergic;
+
+import seedu.duke.diagnosis.illnesses.Illness;
+import seedu.duke.diagnosis.symptoms.Symptom;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//@@author Brennanzuz
+public class Asthma extends Illness {
+    public static String illnessName;
+    public static int severityLevel;
+    public static boolean isChronic;
+    public static ArrayList<Symptom> symptoms;
+}
