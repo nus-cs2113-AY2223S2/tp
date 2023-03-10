f064c24c8d05ecbb448f9fb167775743d49ed19a
ZiqiuZeng <88576865+ZiqiuZeng@users.noreply.github.com>
2023-03-10 10:08:56 +0800

    delete unused import

--- src/main/java/seedu/badMaths/HelpManual.java
+++ src/main/java/seedu/badMaths/HelpManual.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 
 public class HelpManual {
     protected static String filePath = "docs/HelpManual.txt";
