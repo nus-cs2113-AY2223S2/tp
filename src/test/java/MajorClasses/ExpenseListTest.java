75d6d6265fc41c7a3f9714b223f2a93ece0c91dc
Ju-Can <ju.can.0860@gmail.com>
2023-03-11 18:14:57 +0800

    Update ExpenseListTest.java

--- src/test/java/MajorClasses/ExpenseListTest.java
+++ src/test/java/MajorClasses/ExpenseListTest.java
@@ -15,8 +15,7 @@ class ExpenseListTest {
     public void addExpense_successful() {
         testExpenseList.add(new Expense(2.5, "02/02/2012", "food", Currency.SGD));
         expenseList.addExpense("add amt/2.5 t/02/02/2012 cat/food");
-        // TODO: user getter and compare each component individually
-         assertEquals(testExpenseList.get(0), expenseList.getExpenseList().get(0));
+        assertEquals(testExpenseList.get(0), expenseList.getExpenseList().get(0));
     }
 
     @Test
