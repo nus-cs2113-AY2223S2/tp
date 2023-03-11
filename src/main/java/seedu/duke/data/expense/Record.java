6e6139c706ad56ee64cf0a62d7f9853a8f6c1de7
Rayleigh47 <yanming8870@outlook.com>
2023-03-11 22:11:05 +0800

    added record class

---
+++ src/main/java/seedu/duke/data/expense/Record.java
@@ -0,0 +1,46 @@
+package seedu.duke.data.expense;
+
+public class Record {
+    protected String category;
+    protected String description;
+    protected String date;
+    protected float value;
+
+    /**
+     * Generic constructor for Record class.
+     * @param category string of record category, i.e. expense or income
+     * @param description string of record description
+     * @param date string of record date
+     * @param value float of record amount
+     */
+    public Record(String category, String description, String date, float value) {
+        this.category = category;
+        this.description = description;
+        this.date = date;
+        this.value = value;
+
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public float getValue() {
+        return value;
+    }
+
+    public String toString() {
+        return "Category - " + getCategory() +
+                "| Description - " + getDescription() +
+                "| Date - " + getDate() +
+                "| Value - " + getValue();
+    }
+}
