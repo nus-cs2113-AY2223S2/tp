7579da1ab88f33ef72b03fef2df2bce5150b3f94
GohJW <aarongjw00@gmail.com>
2023-03-11 16:31:28 +0800

    Add getter for currency type in Expense.java

--- src/main/java/MajorClasses/Expense.java
+++ src/main/java/MajorClasses/Expense.java
@@ -29,6 +29,7 @@ public class Expense {
     public String getDescription() {
         return description;
     }
+    public Currency getCurrencyType() { return currencyType; }
 
     // The setter method will be used if the User want to change some information in their previous expense
     public void setExpenseAmount(double expenseAmount) {
