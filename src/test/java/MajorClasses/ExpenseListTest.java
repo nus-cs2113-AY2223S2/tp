ba7092da944532467decc2f3acb0124913b7dd7e
GohJW <aarongjw00@gmail.com>
2023-03-11 16:23:55 +0800

    Refactor 'add' feature and extract Duke from main into object

--- src/test/java/MajorClasses/ExpenseListTest.java
+++ src/test/java/MajorClasses/ExpenseListTest.java
@@ -1,6 +1,8 @@
 package MajorClasses;
 
+import Command.CommandAdd;
 import org.junit.jupiter.api.Test;
+import parser.Parser;
 
 import java.util.ArrayList;
 
@@ -9,11 +11,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class ExpenseListTest {
     public ArrayList<Expense> testExpenseList = new ArrayList<>();
     public ExpenseList expenseList = new ExpenseList();
+    public Parser parser = new Parser();
 
     @Test
     public void deleteExpense_successful() {
         testExpenseList.add(new Expense(2.5, "02/02/2012", "food", Currency.SGD));
-        expenseList.addExpense("add amt/2.5 t/02/02/2012 cat/food");
+        new CommandAdd(expenseList.getExpenseList(), parser.extractAddParameters("add amt/2.5 " +
+                "t/02/02/2012 cat/food")).run();
         testExpenseList.remove(0);
         expenseList.deleteExpense("delete 1");
         assertEquals(testExpenseList, expenseList.getExpenseList());
