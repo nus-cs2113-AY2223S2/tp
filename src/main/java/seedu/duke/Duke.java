e7052795619498a3edc9a8fed44a6822c8bf5259
Thunderdragon221 <81968036+Thunderdragon221@users.noreply.github.com>
2023-03-11 20:47:40 +0800

    Fix standard coding violation

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -1,15 +1,15 @@
 package seedu.duke;
 
+/*
 import seedu.duke.diagnosis.Diagnosis;
 import seedu.duke.diagnosis.IllnessMatch;
 import seedu.duke.diagnosis.symptoms.Symptom;
 
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+ */
 import java.util.Scanner;
 
-import seedu.duke.save.CorruptedDataException;
 import seedu.duke.ui.Menu;
 import seedu.duke.ui.Parser;
 import seedu.duke.save.Storage;
@@ -19,7 +19,7 @@ public class Duke {
      * Main entry-point for the java.duke.Duke application.
      */
     //@@author Jeraldchen
-    public static void main(String[] args) throws FileNotFoundException, CorruptedDataException {
+    public static void main(String[] args) {
         String logo = " ____         ____        _        \n"
                 + "|  _ \\  ___  |  _ \\ _  _ | | _____ \n"
                 + "| | | |/ _ \\ | | | | | | | |/ / _ \\\n"
@@ -33,16 +33,17 @@ public class Duke {
         Parser.parseWelcome(input);
 
 
-
-        //For testing of Diagnosis
-//        for (IllnessMatch illnessMatch : Diagnosis.getPossibleIllnesses(new ArrayList<Symptom>(List.of(Symptom.BLOCKED_NOSE,
-//                Symptom.RUNNY_NOSE,
-//                Symptom.SNEEZING,
-//                Symptom.CHILLS,
-//                Symptom.FATIGUE,
-//                Symptom.THROAT_IRRITATION)))) {
-//            System.out.println(illnessMatch.getIllness().getIllnessName());
-//            System.out.println(illnessMatch.getSimilarityPercentage());
-//        }
+        /*
+        For testing of Diagnosis
+        for (IllnessMatch test : Diagnosis.getPossibleIllnesses(new ArrayList<Symptom>(List.of(Symptom.BLOCKED_NOSE,
+                Symptom.RUNNY_NOSE,
+                Symptom.SNEEZING,
+                Symptom.CHILLS,
+                Symptom.FATIGUE,
+                Symptom.THROAT_IRRITATION)))) {
+            System.out.println(test.getIllness().getIllnessName());
+            System.out.println(test.getSimilarityPercentage());
+        }
+         */
     }
 }
