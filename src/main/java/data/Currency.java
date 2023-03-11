8351b290745e780dd9ada971314529cba0302d68
Ju-Can <ju.can.0860@gmail.com>
2023-03-12 01:50:25 +0800

    update coding style

--- src/main/java/data/Currency.java
+++ src/main/java/data/Currency.java
@@ -5,20 +5,20 @@ public enum Currency {
     // It will be replaced in later version by currency API;
     // Currently, we have USD;
     USD, SGD;
+
     public static Currency checkCurrency(String currency) {
-        if(currency == null) {
+        if (currency == null) {
             return SGD;
         }
         if (currency.equals("USD")) {
             return USD;
-        }
-        else {
+        } else {
             // Will change to other currency later on, currently just assume SGD
             return SGD;
         }
     }
 
-    public static double getExchangeRate(Currency from, Currency To) {
+    public static double getExchangeRate(Currency from, Currency to) {
         // Could return the currency exchange rate for other methods' usage
         return 1;
     }
