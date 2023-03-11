f3b58e1de304921fe5db201ae8d79ffac06e0e9c
Manoj364 <theerazor@outlook.com>
2023-03-11 17:28:06 +0800

    Add help-guide

---
+++ src/main/java/seedu/duke/Ui.java
@@ -0,0 +1,40 @@
+package seedu.duke;
+
+public class Ui {
+
+    public void showLine() {
+        System.out.println("____________________________________________________________");
+    }
+
+    public void showWelcome() {
+        showLine();
+        System.out.println("Hello! Welcome to EveNtUS!");
+        showLine();
+    }
+
+    public void showGuide() {
+        showLine();
+        showHelpText();
+        showLine();
+    }
+
+    private static void showHelpText() {
+        System.out.println("Here are the functions of the application!");
+        System.out.println("To add a company, type:");
+        System.out.println("        add n/<COMPANY_NAME> c/<CONTACT_NUMBER> e/<EMAIL>");
+        System.out.println(" ");
+        System.out.println("To list companies, type:");
+        System.out.println("                    list companies");
+        System.out.println(" ");
+        System.out.println("To list venues, type:");
+        System.out.println("                    list venues");
+        System.out.println(" ");
+        System.out.println("To delete, type:");
+        System.out.println("                    delete <INDEX>");
+        System.out.println(" ");
+        System.out.println("To display this guide at any time, type:");
+        System.out.println("                    show guide");
+        System.out.println(" ");
+    }
+
+}
