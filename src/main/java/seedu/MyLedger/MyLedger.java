c7746797ce9ecc97c1662249e5d0c797d939f270
itszhixuan <leezhixuan001@gmail.com>
2023-03-11 18:20:00 +0800

    Creating txt File on user's local device to store future values

--- src/main/java/seedu/MyLedger/MyLedger.java
+++ src/main/java/seedu/MyLedger/MyLedger.java
@@ -1,6 +1,13 @@
 package seedu.myledger;
 
 import seedu.expenditure.ExpenditureList;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class MyLedger {
@@ -15,6 +22,15 @@ public class MyLedger {
     }
 
     public static void main(String[] args) {
+
+        try {
+            fileAvailability();
+        } catch (FileNotFoundException e) {
+            System.out.println("File does not exist.");
+        }
+    }
+
+    public static void runMyLedger() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -22,8 +38,46 @@ public class MyLedger {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("What is your name?");
-
         Scanner in = new Scanner(System.in);
         System.out.println("Hello " + in.nextLine());
     }
+
+    public static void fileAvailability() throws FileNotFoundException {
+        File txtFile = createFile();
+
+        if (txtFile.exists()) {
+            // Runs the program
+            runMyLedger();
+        }
+    }
+
+    public static File createFile() {
+        Path path = Paths.get(System.getProperty("user.home"), "my_ledger_data");
+        checkIfFolderExists(path);
+        Path textFilePath = checkIfFileExists();
+        File textFile = textFilePath.toFile();
+        return textFile;
+    }
+
+    public static Path checkIfFileExists() {
+        Path filePath = Paths.get(System.getProperty("user.home"), "my_ledger_data", "my_ledger_inputs.txt");
+        try {
+            Files.createFile(filePath);
+        } catch(IOException e) {
+            System.out.println("File already exists!");
+        }
+        return filePath;
+    }
+
+    public static void checkIfFolderExists(Path path) {
+        boolean directoryExists = Files.exists(path);
+        if (!directoryExists) {
+            try {
+                Files.createDirectory(path);
+            } catch (IOException e) {
+                System.out.println("Folder cannot be created!");
+            }
+        }
+    }
+
 }
