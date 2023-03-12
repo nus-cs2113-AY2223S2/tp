3736fe1d510146df99975038a39c9b0d718074ba
wuzhzn <dagreatbao@gmail.com>
2023-03-12 10:19:07 +0800

    Add Venue Class

---
+++ src/main/java/seedu/duke/Venue.java
@@ -0,0 +1,16 @@
+package seedu.duke;
+
+public class Venue {
+    private String venueName;
+
+    private String venueLocation;
+
+    private int venueCapacity;
+
+    public Venue(String venueName, String venueLocation, int venueCapacity){
+        this.venueName = venueName;
+        this.venueLocation = venueLocation;
+        this.venueCapacity = venueCapacity;
+    }
+}
+
