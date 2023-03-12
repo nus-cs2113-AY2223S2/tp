4e995e261d5b8b56ab4878920214874869f5bef4
TzeLoong <tzeloong12@gmail.com>
2023-03-12 14:51:13 +0800

    Update Ui.java

--- src/main/java/seedu/ui/Ui.java
+++ src/main/java/seedu/ui/Ui.java
@@ -7,8 +7,10 @@ public class Ui {
             "\\_)(_/(__/  \\____/(____)(____/ \\___/(____)(__\\_)\n";
 
     public static final String WELCOMEMESSAGE = "Hello there, Welcome to\n";
-    public static final String INSTRUCTIONS = "Here are the list possible commands: add, delete, edit, list, borrow, lend, exit\n";
-    public static final String HELPMESSAGE = "If this is your first time here, Type <help> to learn more about the commands\n";
+    public static final String INSTRUCTIONS = "Here are the list possible commands: "
+            + "add, delete, edit, list, borrow, lend, exit\n";
+    public static final String HELPMESSAGE = "If this is your first time here, "
+            + "Type <help> to learn more about the commands\n";
 
     public static void greetUser() {
         System.out.println(WELCOMEMESSAGE + logo + INSTRUCTIONS + HELPMESSAGE);
