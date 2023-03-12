1e17e48f2c9f27625f5130e39225cedd2eafbb2e
Akmal_Hanis <akmalightning@gmail.com>
2023-03-12 13:39:02 +0800

    Companies List() method

--- src/main/java/CompanyList.java
+++ src/main/java/CompanyList.java
@@ -5,4 +5,16 @@ import java.util.ArrayList;
 public class CompanyList {
 
     ArrayList<Company> companies = new ArrayList<>();
+
+    public void list(){
+        if (companies.size() == 0){
+            System.out.println("You do not have any companies in this list");
+        }
+        else {
+            for (int i = 0; i < companies.size(); i += 1) {
+                System.out.println((i+1) + ". " + companies.get(1));
+
+            }
+        }
+    }
 }
