5ea6435121e1520c6988c8ac2e415da8d4e03383
hrithie <40565771+hrithie@users.noreply.github.com>
2023-03-13 02:06:58 +0800

    Update Parser.java

--- src/main/java/seedu/duke/Parser.java
+++ src/main/java/seedu/duke/Parser.java
@@ -1,59 +1,97 @@
- package seedu.duke;
+package seedu.duke;
 
- import java.util.ArrayList;
+import java.util.ArrayList;
+import java.util.Scanner;
 
- public class Parser {
+public class Ui {
+    /**
+     * Prints a line of dashes for
+     * better readability
+     */
+    public static void printDash() {
+        System.out.println("____________________________________________________________");
+    }
 
-     public static void parseCommand(String userInput, EventList eventList){
-         userInput = userInput.trim();
-         String command = userInput.substring(0, userInput.indexOf(" "));
-         String remainder = userInput.substring(userInput.indexOf(" ") + 1);
-         switch (command) {
-         case "add":
-             parseAddCommand(remainder, eventList);
-             break;
-         case "delete":
-             parseDeleteCommand(remainder, eventList);
-             break;
-         case "list":
-             parseListCommand(eventList);
-             break;
-         case "edit":
-             // parseEditCommand(remainder, eventList);
-             break;
-         default:
+    /**
+     * Obtains user input and interprets
+     * what needs to be performed by
+     * certain keywords.
+     */
+    public static void getUserCommand(EventList eventList) {
 
-             break;
-         }
-     }
+        Scanner in = new Scanner(System.in);
 
-     public static void parseAddCommand(String remainder, EventList eventList) {
-         // Note no "-" anywhere else.
-         String[] details = remainder.split("-");
-         String eventName = details[0];
-         String startTime = details[1];
-         String startDate = details[2];
-         String endTime = details[3];
+        String cmd;
+        cmd = in.nextLine();
 
-         if (details.length == 5) {
-             String endDate = details[4];
-             eventList.addEvent(eventName, startTime, startDate, endTime, endDate);
-         } else {
-             eventList.addEvent(eventName, startTime, startDate, endTime);
-         }
+        while (!(cmd.equals("bye"))) {
+            Parser.parseCommand(cmd, eventList);
+            cmd = in.nextLine();
+        }
 
-        //TODO: Show successful add on UI. (For all cases)
-         Ui.addSuccessMsg();
-     }
+    }
 
-     private static void parseDeleteCommand(String remainder, EventList eventList) {
-         eventList.deleteThisTask(Integer.parseInt(remainder));
+    /**
+     * Prints a welcome message for
+     * users when application is launched
+     */
+    public static void showWelcome() {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("What is your name?");
+        printDash();
+        Scanner in = new Scanner(System.in);
+        System.out.println("Hello " + in.nextLine());
+    }
 
-         //TODO: Show successful add on UI. (For all cases)
-         Ui.deleteSuccessMsg();
-     }
+    /**
+     * Prints success message for
+     * users when event is added
+     */
+    public static void addSuccessMsg() {
+        printDash();
+        System.out.println("Event successfully added!");
+        printDash();
+    }
 
-     private static void parseListCommand(EventList eventList) {
-         Ui.listTask(eventList.fullList());
-     }
- }
+    /**
+     * Prints success message for
+     * users when event is deleted
+     */
+    public static void deleteSuccessMsg() {
+        printDash();
+        System.out.println("Event successfully deleted!");
+        printDash();
+    }
+
+    /**
+     * Prints list of events
+     */
+    public static void listTask(ArrayList<Event> taskList) {
+        printDash();
+        if (taskList.size() == 0) {
+            System.out.println("There are no events!");
+            printDash();
+            return;
+        }
+        for (int i = 0; i < taskList.size(); i++) {
+            System.out.println("   > " + Integer.toString(i + 1) + "." + taskList.get(i).toString());
+        }
+        printDash();
+    }
+
+    /**
+     * Prints an exit message when
+     * user intends to exit Duke
+     */
+    public static void printExit() {
+        printDash();
+        System.out.println("Bye, see ya soon!");
+        printDash();
+    }
+
+}
