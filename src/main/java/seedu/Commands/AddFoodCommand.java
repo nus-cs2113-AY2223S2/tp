1bb6b92d0760218d7d706784fd9c290daf3c52a5
Mustafa Anis Hussain <mustafahussain793@gmail.com>
2023-03-12 21:20:49 +0800

    Update add food functionality

--- src/main/java/seedu/Commands/AddFoodCommand.java
+++ src/main/java/seedu/Commands/AddFoodCommand.java
@@ -3,20 +3,31 @@ package seedu.Commands;
 import seedu.Database.FoodStorage;
 import seedu.Database.MealStorage;
 import seedu.Database.UserStorage;
-// import seedu.Entities.Food;
+import seedu.Entities.Food;
+import seedu.Exceptions.InvalidCommandException;
 import seedu.Exceptions.LifeTrackerException;
 import seedu.Output.UI;
 
 public class AddFoodCommand extends Command{
-    // private Food newFood;
-    public AddFoodCommand (String commandDescriptor){
-        // int foodIndex;
-        // foodIndex = Integer.parseInt(commandDescriptor);
-        //this.newFood =
+    private Food newFood;
+    private int index;
+    public AddFoodCommand (String commandDescriptor) throws LifeTrackerException {
+        parseInput(commandDescriptor);
+    }
+    private void parseInput(String commandDescriptor) throws LifeTrackerException{
+        if (commandDescriptor.length() == 0){
+            throw new InvalidCommandException();
+        }
+        try {
+            this.index = Integer.parseInt(commandDescriptor);
+        } catch (NumberFormatException e) {
+            throw new InvalidCommandException();
+        }
     }
     @Override
     public void execute(UI ui, FoodStorage foodStorage, MealStorage mealStorage, UserStorage userStorage) 
             throws LifeTrackerException {
-
+        foodStorage.addFood(index);
+        ui.printNewFoodAdded(foodStorage, index);
     }
 }
