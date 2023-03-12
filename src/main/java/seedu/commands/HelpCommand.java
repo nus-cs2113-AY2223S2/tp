c99820b493dd637271cba29b65d38358fb35a25f
TzeLoong <tzeloong12@gmail.com>
2023-03-12 14:42:05 +0800

    Edit

--- src/main/java/seedu/commands/HelpCommand.java
+++ src/main/java/seedu/commands/HelpCommand.java
@@ -12,19 +12,6 @@ public class HelpCommand extends Command {
     @Override
     public CommandResult execute(ExpenditureList expenditures) {
         return new CommandResult(
-                "Here are the list of commands and their respective functions. Please take note of the FORMAT that is stated below\n"
-                        +
-                        "1. add: Add an expenditure to the record\n" +
-                        "Format: add d/DATE a/AMOUNT c/CATEGORY\n" +
-                        "2. delete: Deletes the specified expenditure from the record\n" +
-                        "Format: delete INDEX\n" +
-                        "3. edit: Edits an existing expenditure in the record\n" +
-                        "Format: edit INDEX d/DATE a/AMOUNT c/CATEGORY\n" +
-                        "4. list: Shows a list of expenditures and loans in the record based on existing categories\n" +
-                        "Format: list /TYPE\n" +
-                        "5. borrow: Keep a record of an incoming loan\n" +
-                        "Format: borrow a/AMOUNT n/LENDER_NAME d/BORROWED_DATE\n" +
-                        "6. lend: Add an expenditure to the record\n" +
-                        "Format: lend a/AMOUNT n/BORROWER_NAME d/LENT_DATE");
+                "The list of commands and their respective functions can be found at the userguide here: ... ");
     }
 }
--- src/main/java/seedu/ui/Ui.java
+++ src/main/java/seedu/ui/Ui.java
@@ -7,11 +7,11 @@ public class Ui {
             "\\_)(_/(__/  \\____/(____)(____/ \\___/(____)(__\\_)\n";
 
     public static final String WELCOMEMESSAGE = "Hello there, Welcome to\n";
-    public static final String INSTRUCTIONS = "Here are the list possible commands: add, delete, edit, list, borrow, lend, exit\n"
-            + "If this is your first time here, Type <help> to learn more about the commands";
+    public static final String INSTRUCTIONS = "Here are the list possible commands: add, delete, edit, list, borrow, lend, exit\n";
+    public static final String HELPMESSAGE = "If this is your first time here, Type <help> to learn more about the commands\n";
 
     public static void greetUser() {
-        System.out.println(WELCOMEMESSAGE + logo + INSTRUCTIONS);
+        System.out.println(WELCOMEMESSAGE + logo + INSTRUCTIONS + HELPMESSAGE);
     }
 
 }
