327df6e6895443af02d5fc9743a975ef1d153a68
JeraldChen <jeraldchenjunhan@gmail.com>
2023-03-13 20:03:18 +0800

    Add more JUnit tests and some assertions in functional code

---
+++ src/test/java/seedu/duke/information/InformationTest.java
@@ -0,0 +1,29 @@
+package seedu.duke.information;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class InformationTest {
+    @Test
+    public void resetDiagosisHistory() {
+        ArrayList<String> diagnosisHistory = new ArrayList<>();
+        diagnosisHistory.add("Diagnosis 1");
+        diagnosisHistory.add("Diagnosis 2");
+        diagnosisHistory.clear();
+        assertEquals(diagnosisHistory.size(), 0);
+    }
+
+    @Test
+    public void checkPassword() {
+        HashMap<String, String> personalInfo = new HashMap<>();
+        String password = "iloveCS2113";
+        String name = "Akshay";
+        personalInfo.put(name, password);
+        assertEquals(personalInfo.containsKey(password), true);
+
+    }
+}
