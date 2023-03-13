a280a22a415f69185c918f9b6fb5f35c5a529d4e
darrenangwx <darrenawx@gmail.com>
2023-03-13 15:33:31 +0800

    added JUnit Test for DiinerDirector Class

--- src/test/java/dinerdirector/DinerDirectorTest.java
+++ src/test/java/dinerdirector/DinerDirectorTest.java
@@ -2,10 +2,33 @@ package dinerdirector;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import commands.Command;
+import commands.ExitCommand;
+import commands.HelpCommand;
+import commands.IncorrectCommand;
 import org.junit.jupiter.api.Test;
+import utils.Parser;
+
+import java.util.ArrayList;
+
 public class DinerDirectorTest {
+
     @Test
-    public void sampleTest() {
-        assertTrue(true);
+    void runCommandLoopUntilExit_userInput_command() {
+        ArrayList<String> listOfCommands = new ArrayList<>();
+        listOfCommands.add("help");
+        listOfCommands.add("exit");
+        listOfCommands.add("asdf");
+
+        for (String listOfCommand : listOfCommands) {
+            Command command = new Parser().parseCommand(listOfCommand);
+            if (listOfCommand.equals("help")) {
+                assertTrue(command instanceof HelpCommand);
+            } else if (listOfCommand.equals("exit")) {
+                assertTrue(command instanceof ExitCommand);
+            } else {
+                assertTrue(command instanceof IncorrectCommand);
+            }
+        }
     }
 }
