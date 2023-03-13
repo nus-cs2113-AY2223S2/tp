a394ea9a7d4cec31bc55c375a03f181138b4a9fe
Vignesh-30 <30vignesh@gmail.com>
2023-03-13 14:36:11 +0800

    Added try catch statements for error handling

--- src/main/java/functionalities/Parser.java
+++ src/main/java/functionalities/Parser.java
@@ -22,27 +22,33 @@ public class Parser {
         return command;
     }
 
-    private static void parseViewCommand(String task) {
-        String userCommand = "view";
-        String uId = task.split(" ", 2)[1];
-        command = new Command(userCommand, 0, uId);
+    private static void parseViewCommand(String task) throws SniffException {
+        try {
+            String userCommand = "view";
+            int uId = Integer.parseInt(task.split(" ", 2)[1]);
+            command = new Command(userCommand, 0, uId);
+        } catch (ArrayIndexOutOfBoundsException emptyView) {
+            throw new SniffException(" The view command description cannot be empty!");
+        }  catch (NumberFormatException e) {
+            throw new SniffException(" The user Id to view appointment details must be a number!");
+        }
     }
 
     private static void parseByeCommand() {
         String userCommand = "bye";
-        command = new Command(userCommand, 0, null);
+        command = new Command(userCommand, 0, 0);
     }
 
     private static void parseListCommand() {
         String userCommand = "list";
-        command = new Command(userCommand, 0, null);
+        command = new Command(userCommand, 0, 0);
     }
 
     private static void parseRemoveCommand(String task) throws SniffException {
         try {
             String userCommand = "remove";
             int apptNum = Integer.parseInt(task.split(" ", 2)[1]);
-            command = new Command(userCommand, apptNum, null);
+            command = new Command(userCommand, apptNum, 0);
         } catch (ArrayIndexOutOfBoundsException e) {
             throw new SniffException(" The remove command description cannot be empty!");
         } catch (NumberFormatException e) {
