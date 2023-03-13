97eefcc13893054018231e9f2275a2aa91a4ee79
wuzhzn <dagreatbao@gmail.com>
2023-03-13 15:19:51 +0800

    Reformat duke file

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -1,5 +1,6 @@
 package seedu.duke;
 
+import java.util.Scanner;
 public class Duke {
     /**
      * Main entry-point for the java.duke.Duke application.
@@ -12,6 +13,7 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("What is your name?");
-
+        Scanner in = new Scanner(System.in);
+        System.out.println("Hello " + in.nextLine());
     }
 }
