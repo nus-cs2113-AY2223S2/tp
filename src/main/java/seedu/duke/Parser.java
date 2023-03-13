991a3d6ca587875c9f0363634c19296947743a8c
wuzhzn <dagreatbao@gmail.com>
2023-03-13 10:47:02 +0800

    Reformat code

--- src/main/java/seedu/duke/Parser.java
+++ src/main/java/seedu/duke/Parser.java
@@ -7,28 +7,31 @@ import seedu.duke.Command.ListVenueCommand;
 
 public interface Parser {
     static Command parse(String input){
+
+        Ui ui = new Ui();
         String[] inputWords = input.split(" ");
         String command = inputWords[0];
-        switch (command){
-            case "list" :
-                if (inputWords[1].equals("companies")){
-                    ListCompanyCommand companyCommand = new ListCompanyCommand(command);
-                    return companyCommand;
-                } else if (inputWords[1].equals("venues")) {
-                    ListVenueCommand venueCommand = new ListVenueCommand(command);
-                    return venueCommand;
-                }
-            case "add":
-                System.out.println(input.replaceFirst(command, "").trim());
-            case "delete":
-                int taskNum = Integer.parseInt(inputWords[1]) - 1;
-                DeleteCommand deleteCommand = new DeleteCommand(command, taskNum);
-                return deleteCommand;
-            case "help":
-                Ui.showGuide();
-            case "exit":
-                Ui.showExitMessage();
-            default: System.out.println("Unknown input!");
+        switch (command) {
+        case "list":
+            if (inputWords[1].equals("companies")) {
+                ListCompanyCommand companyCommand = new ListCompanyCommand(command);
+                return companyCommand;
+            } else if (inputWords[1].equals("venues")) {
+                ListVenueCommand venueCommand = new ListVenueCommand(command);
+                return venueCommand;
+            }
+        case "add":
+            System.out.println(input.replaceFirst(command, "").trim());
+        case "delete":
+            int taskNum = Integer.parseInt(inputWords[1]) - 1;
+            DeleteCommand deleteCommand = new DeleteCommand(command, taskNum);
+            return deleteCommand;
+        case "help":
+            ui.showGuide();
+        case "exit":
+            ui.showExitMessage();
+        default:
+            System.out.println("Unknown input!");
         }
         return null;
     }
