6adc4615461a8c7106abf01806497b1d29f8b0cd
Jin Xuan Ow Yong <jinxuan.owyong@gmail.com>
2023-03-13 17:52:22 +0800

    Fix checkstyle violations

--- src/main/java/seedu/duke/constants/StorageConstants.java
+++ src/main/java/seedu/duke/constants/StorageConstants.java
@@ -3,8 +3,8 @@ package seedu.duke.constants;
 import java.nio.file.Paths;
 
 public class StorageConstants {
+    public static final String DELIMITER = ",";
     private static final String PATH_HOME = System.getProperty("user.dir");
     public static final String RELATIVE_FILE_NAME = Paths.get(PATH_HOME, "data", "storage.txt").toString();
     public static final String PATH_LOG_OUTPUT = Paths.get(PATH_HOME, "logs", "pocketpal.txt").toString();
-    public static final String DELIMITER = ",";
 }
