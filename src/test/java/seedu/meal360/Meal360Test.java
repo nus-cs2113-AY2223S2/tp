0ad279de116a393bc537f0076c635f9584b6931c
JuneNita <junenita@gmail.com>
2023-03-13 22:06:06 +0800

    add testListRecipe

--- src/test/java/seedu/meal360/Meal360Test.java
+++ src/test/java/seedu/meal360/Meal360Test.java
@@ -25,4 +25,27 @@ class Meal360Test {
         assertTrue(recipe.getName().equals("test recipe name"));
         assertTrue(recipe.getIngredients().get("test ingredient") == 100);
     }
+
+    @Test
+    public void testListRecipe() {
+        RecipeList recipes = new RecipeList();
+        Parser parser = new Parser();
+
+        HashMap<String, Integer> testIngredients = new HashMap<>();
+        testIngredients.put("test ingredient1", 10);
+        testIngredients.put("test ingredient2", 20);
+        testIngredients.put("test ingredient3", 30);
+        Recipe testR = new Recipe("test recipe1", testIngredients);
+        recipes.addRecipe(testR);
+
+        RecipeList recipeListToPrint = parser.parseListRecipe(recipes);
+        assertTrue(recipeListToPrint.size() == 1);
+        int order = 1;
+        for (Recipe recipe : recipes) {
+            if (order == 1) {
+                assertTrue(recipe.getNumOfIngredients() == 3);
+            }
+            order++;
+        }
+    }
 }
