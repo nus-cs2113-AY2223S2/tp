9cb276ed4b66e9f09fa8b0d49a1cd00a33032217
Koh_Jing_Jie_Marcus <88085589+Koh-Jing-Jie-Marcus@users.noreply.github.com>
2023-03-14 15:51:28 +0800

    Add AddMealCommandTest class

---
+++ src/test/java/seedu/Commands/AddMealCommandTest.java
@@ -0,0 +1,14 @@
+package seedu.Commands;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddMealCommandTest {
+    @Test
+    void parseInput_EmptyInput_ExpectException() {
+        String commandDescriptor = "";
+        assertThrows(NumberFormatException.class,
+                () -> Integer.parseInt(commandDescriptor));
+    }
+}
