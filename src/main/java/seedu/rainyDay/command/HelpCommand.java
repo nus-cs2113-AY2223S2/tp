a79f7be1d57c688113ac6b895b21c95b0e08d57e
BenjaminPoh <e0726717@u.nus.edu>
2023-03-14 23:05:32 +0800

    Refactored Error messages to Exceptions class from UI Class

---
+++ src/main/java/seedu/rainyDay/command/HelpCommand.java
@@ -0,0 +1,7 @@
+package seedu.rainyDay.command;
+
+public class HelpCommand extends Command {
+    public void execute() {
+        System.out.println("Have you tried reading the UG?");
+    }
+}
--- src/main/java/seedu/rainyDay/data/Parser.java
+++ src/main/java/seedu/rainyDay/data/Parser.java
@@ -4,21 +4,21 @@ import seedu.rainyDay.command.AddCommand;
 import seedu.rainyDay.command.Command;
 import seedu.rainyDay.command.DeleteCommand;
 import seedu.rainyDay.command.ViewCommand;
+import seedu.rainyDay.command.HelpCommand;
 import seedu.rainyDay.exceptions.ErrorMessage;
 import seedu.rainyDay.exceptions.RainyDayException;
-import seedu.rainyDay.modules.Ui;
 
 public class Parser {
     public static Command parseUserInput(String userInput) throws IllegalArgumentException, RainyDayException {
         String action = userInput.split("\\s+")[0];
         if (action.equalsIgnoreCase(Command.COMMAND_ADD)) {
             return addStatement(userInput);
-        } else if (action.equalsIgnoreCase(Command.COMMAND_DELETE)) {
+        } if (action.equalsIgnoreCase(Command.COMMAND_DELETE)) {
             return deleteStatement(userInput);
-        } else if (action.equalsIgnoreCase(Command.COMMAND_VIEW)) {
+        } if (action.equalsIgnoreCase(Command.COMMAND_VIEW)) {
             return generateReport();
-            //} else if (action.equalsIgnoreCase(Command.COMMAND_HELP)) {
-            //    UI.displayHelp();
+        } if (action.equalsIgnoreCase(Command.COMMAND_HELP)) {
+            return displayHelp();
         } else {
             throw new RainyDayException(ErrorMessage.UNRECOGNIZED_INPUT.toString());
         }
@@ -34,24 +34,28 @@ public class Parser {
             String amount = data[1];
             return new AddCommand(description, flowDirection, Integer.parseInt(amount));
         } catch (Exception e) {
-            throw new IllegalArgumentException(Ui.WRONG_ADD_FORMAT);
+            throw new IllegalArgumentException(ErrorMessage.WRONG_ADD_FORMAT.toString());
         }
     }
 
     public static DeleteCommand deleteStatement(String userInput) throws IllegalArgumentException {
         String[] tokens = userInput.split("\\s+");
-        if (tokens.length < 2) {
-            throw new IllegalArgumentException(Ui.NO_DELETE_INDEX);
-        }
         try {
+            if (tokens.length < 2) {
+                throw new IllegalArgumentException(ErrorMessage.NO_DELETE_INDEX.toString());
+            }
             int index = Integer.parseInt(tokens[1]);
             return new DeleteCommand(index);
         } catch (Exception e) {
-            throw new IllegalArgumentException(Ui.WRONG_DELETE_INDEX);
+            throw new IllegalArgumentException(ErrorMessage.WRONG_DELETE_INDEX.toString());
         }
     }
 
     public static ViewCommand generateReport() {
         return new ViewCommand();
+    }
+
+    public static HelpCommand displayHelp() {
+        return new HelpCommand();
     }
 }
