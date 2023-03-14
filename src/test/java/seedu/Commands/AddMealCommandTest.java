e896a2b8e0ecba0c0b1b62b248b6cd3bc0583e34
Mustafa Anis Hussain <mustafahussain793@gmail.com>
2023-03-14 23:18:41 +0800

    Update AddMealCommandTest and DeleteFoodCommandTest

--- src/test/java/seedu/Commands/AddMealCommandTest.java
+++ src/test/java/seedu/Commands/AddMealCommandTest.java
@@ -1,10 +1,31 @@
-package seedu.Commands;
+package seedu.commands;
 
 import org.junit.jupiter.api.Test;
+import seedu.database.FoodStorage;
+import seedu.database.MealStorage;
+import seedu.entities.Food;
+import seedu.entities.Meal;
+import seedu.exceptions.InvalidArgumentsException;
+import java.util.ArrayList;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class AddMealCommandTest {
+
+    private final FoodStorage foodStorage = new FoodStorage("./data/foodData.csv");
+    private final MealStorage mealStorage = new MealStorage("./data/mealData.csv", foodStorage);
+    private ArrayList<Food> foodList = new ArrayList<>();
+
+    @Test
+    void addMealSingleMealAddedExpectListSizeIncrease() throws InvalidArgumentsException {
+        int oldSize = mealStorage.getMealCount();
+        foodList.add(foodStorage.getFoodById(2));
+        String date = "1/1/2023";
+        mealStorage.saveMeal(new Meal(foodList, date));
+        int newSize = mealStorage.getMealCount();
+        assertEquals(oldSize + 1, newSize);
+    }
     @Test
     void parseInputEmptyInputExpectException() {
 
