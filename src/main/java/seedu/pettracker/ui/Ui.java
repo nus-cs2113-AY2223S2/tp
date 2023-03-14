b0c6d9e46a23778ef360924e1779045a4e0a97c8
Marek <marek.pinto@gmail.com>
2023-03-14 17:47:35 +0800

    Add JavaDoc comments

--- src/main/java/seedu/pettracker/ui/Ui.java
+++ src/main/java/seedu/pettracker/ui/Ui.java
@@ -2,47 +2,82 @@ package seedu.pettracker.ui;
 
 import java.util.Scanner;
 
+/**
+ * Class to handle all output from commands and get user input from Standard Input.
+ */
 public class Ui {
 
     Scanner in;
 
+    /**
+     * Constructor to initialize Scanner object.
+     */
     public Ui () {
         in = new Scanner(System.in);
     }
 
+    /**
+     * Message that welcomes the user when program loads.
+     */
     public void showWelcomeMessage() {
         System.out.println("Welcome to the Pet Health Tracker!");
     }
 
     /**
-     * Message that prints when the exit command is executed
+     * Message that prints when the exit command is executed.
      */
     public void exitCommandMessage() {
         in.close();
         System.out.println("Goodbye! See you soon.");
     }
 
+    /**
+     * Message that prints when a new pet is added.
+     * @param petName String containing the name of the added pet.
+     */
     public void addPetCommandMessage(String petName) {
         System.out.println("Successfully added new pet: " + petName);
     }
 
+    /**
+     * Message that prints when a new stat is added for a pet.
+     * @param petName String containing the name of the corresponding pet.
+     * @param statName String indicating the name of the added stat.
+     * @param statValue String indicating the value for that stat.
+     */
     public void addStatCommandMessage(String petName, String statName, String statValue) {
-        System.out.println("Updated " + statName " to " + statValue + " for " + petName);
+        System.out.println("Updated " + statName + " to " + statValue + " for " + petName);
     }
 
+    /**
+     * Message that prints when an invalid command is passed to the program.
+     */
     public void invalidCommandMessage() {
         System.out.println("Please enter a valid command!");
     }
 
+    /** 
+     * Message that prints when a pet is removed.
+     * @param petName String representing the name of the removed pet.
+     */
     public void removePetCommandMessage(String petName) {
         System.out.println("Successfully removed pet: " + petName);
     }
 
+    /** 
+     * Message that prints when a stat is removed from a pet.
+     * @param petName String containing name of the corresponding pet.
+     * @param statName String containing name of the stat that was removed.
+     */
     public void removeStatCommandMessage(String petName, String statName) {
         System.out.println("Successfully removed " + statName + " from " + petName);
     }
 
 
+    /** 
+     * Gets input from the user from Standard Input.
+     * @return String containing the input from the user.
+     */
     public String getUserInput() {
         System.out.println();
         String input = in.nextLine();
