e70a686f3e3103609805812131b112cbf3b8b27c
ZiqiuZeng <88576865+ZiqiuZeng@users.noreply.github.com>
2023-03-14 22:06:24 +0800

    Modify imports regarding to coding standard

--- src/main/java/seedu/badMaths/HelpManual.java
+++ src/main/java/seedu/badMaths/HelpManual.java
@@ -6,7 +6,8 @@ import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.logging.*;
+import java.util.logging.Logger;
+import java.util.logging.Level;
 
 public class HelpManual {
     protected static String filePath = "docs/HelpManual.txt";
