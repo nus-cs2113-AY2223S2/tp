4ea175508964b65bb608b2f2a9e9ab80eccf1224
ChongQiRong <e07272229@u.nus.edu>
2023-03-14 12:32:15 +0800

    Add Logger to RainyDay

--- src/main/java/seedu/rainyDay/RainyDay.java
+++ src/main/java/seedu/rainyDay/RainyDay.java
@@ -1,5 +1,6 @@
 package seedu.rainyDay;
 
+import seedu.rainyDay.exceptions.RainyDayException;
 import seedu.rainyDay.modules.Storage;
 import seedu.rainyDay.modules.UI;
 import seedu.rainyDay.command.Command;
@@ -9,15 +10,20 @@ import seedu.rainyDay.data.Parser;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class RainyDay {
     public static String filePath = "rainyDay.txt";
     public static FinancialReport financialReport = new FinancialReport(new ArrayList<>());
+    private static Logger logger = Logger.getLogger("RainyDayLog.log");
 
     private RainyDay(String filePath) {
         try {
             financialReport = new FinancialReport(Storage.loadFromFile(filePath));
+            logger.log(Level.INFO, "File loaded successfully.");
         } catch (IOException | ClassNotFoundException e) {
+            logger.log(Level.INFO, "No valid save file detected. Starting with empty financial data.");
             UI.noFileExist();
         }
     }
@@ -35,14 +41,19 @@ public class RainyDay {
                 }
                 Command specificCommand = Parser.parseUserInput(userInput);
                 specificCommand.execute();
-            } catch (Exception e) {
+            } catch (RainyDayException e) {
+                logger.log(Level.INFO, "RainyDayException caught");
                 System.out.println(e.getMessage());
+            } catch (Exception e) {
+                logger.log(Level.WARNING, e.getMessage());
             }
         }
         UI.sayFarewellToUser();
     }
 
     public static void main(String[] args) {
+        logger.log(Level.INFO, "Starting RainyDay");
         new RainyDay(filePath).run();
+        logger.log(Level.INFO, "Quitting RainyDay");
     }
 }
