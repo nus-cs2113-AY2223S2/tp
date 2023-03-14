649b0dcc01707c9ced1818c55153d54d33b87ddd
DESKTOP-ULU427O\jingy <jingya.lin02@gmail.com>
2023-03-15 00:19:00 +0800

    Add JUnit test for ingredient storage class

--- src/main/java/seedu/duke/storage/IngredientStorage.java
+++ src/main/java/seedu/duke/storage/IngredientStorage.java
@@ -16,9 +16,9 @@ import java.nio.file.Path;
 
 public class IngredientStorage {
 
+    public String filename = "ingredients.txt";
     private Gson gson = new Gson();
     private BufferedWriter bufferedWriter = null;
-    private String filename = "ingredients.txt";
 
     /**
      * Checks for existing file containing ingredients and initialises buffered writer accordingly
@@ -35,6 +35,7 @@ public class IngredientStorage {
             }
             return;
         }
+        assert ingredientFileExists;
         processStoredIngredients(ingredients);
         try {
             this.bufferedWriter = new BufferedWriter(new FileWriter(filename, true));
