c7ffe998a239fb8a51a79977cdbbd66ac4eb84a6
Manoj364 <theerazor@outlook.com>
2023-03-14 15:40:15 +0800

    Add JUnit test code for Ui class

---
+++ src/test/java/seedu/duke/UiTest.java
@@ -0,0 +1,30 @@
+package seedu.duke;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class UiTest {
+
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+    private Ui ui;
+
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @Test
+    public void showWelcome_onStartup_displayWelcomeMessage() {
+        ui.showWelcome();
+        assertEquals("Hello! Welcome to EveNtUS!", outputStreamCaptor.toString()
+                .trim());
+    }
+
+}
