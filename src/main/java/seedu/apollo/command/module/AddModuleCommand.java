68923076b3ecdec50575ec1b96ba672c3ee6f0f3
T-Wan-Lin <tanwanlin.c2021>
2023-03-14 15:09:50 +0800

    Add Assertions and Logging for module commands

--- src/main/java/seedu/apollo/command/module/AddModuleCommand.java
+++ src/main/java/seedu/apollo/command/module/AddModuleCommand.java
@@ -8,12 +8,15 @@ import seedu.apollo.module.Module;
 import seedu.apollo.module.ModuleList;
 import seedu.apollo.task.TaskList;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.logging.*;
 
 
 public class AddModuleCommand extends Command {
-
+    private static Logger logger = Logger.getLogger("AddModuleCommand");
     private Module module;
+
 
     /**
      * Constructor for AddModuleCommand.
@@ -23,12 +26,37 @@ public class AddModuleCommand extends Command {
      * @throws InvalidModule If the module code is invalid.
      */
     public AddModuleCommand(String moduleCode, ModuleList allModules) throws InvalidModule {
+        assert module != null : "Module should not be null!";
+        assert module.getCode().equals(moduleCode) : "Module code should be the same";
+        AddModuleCommand.setUpLogger();
         Module toAdd = allModules.findModule(moduleCode);
+
         if (toAdd == null) {
+
             throw new InvalidModule();
         }
-
         module = toAdd;
+
+    }
+    public static void setUpLogger() {
+        LogManager.getLogManager().reset();
+        logger.setLevel(Level.ALL);
+        ConsoleHandler logConsole = new ConsoleHandler();
+        logConsole.setLevel(Level.SEVERE);
+        logger.addHandler(logConsole);
+        try {
+
+            if (!new File("apollo.log").exists()) {
+                new File("apollo.log").createNewFile();
+            }
+
+            FileHandler logFile = new FileHandler("apollo.log", true);
+            logFile.setLevel(Level.FINE);
+            logger.addHandler(logFile);
+
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "File logger not working.", e);
+        }
     }
 
     @Override
@@ -41,6 +69,7 @@ public class AddModuleCommand extends Command {
             }
             storage.updateModule(moduleList);
         } catch (IOException e) {
+            logger.log(Level.SEVERE, "IO Exception", e);
             ui.printErrorForIO();
         }
     }
--- src/main/java/seedu/apollo/command/module/DeleteModuleCommand.java
+++ src/main/java/seedu/apollo/command/module/DeleteModuleCommand.java
@@ -7,21 +7,47 @@ import seedu.apollo.module.ModuleList;
 import seedu.apollo.ui.Ui;
 import seedu.apollo.task.TaskList;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.logging.*;
 
 /**
      * For {@code delmod} command.
      * Delete Module Command class that finds the module using moduleCode and removes it from the ModuleList
      */
 public class DeleteModuleCommand extends Command {
-
+    private static Logger logger = Logger.getLogger("DeleteModuleCommand");
     protected String moduleCode;
     public DeleteModuleCommand(String moduleCode){
         this.moduleCode = moduleCode;
+        DeleteModuleCommand.setUpLogger();
+    }
+    public static void setUpLogger() {
+        LogManager.getLogManager().reset();
+        logger.setLevel(Level.ALL);
+        ConsoleHandler logConsole = new ConsoleHandler();
+        logConsole.setLevel(Level.SEVERE);
+        logger.addHandler(logConsole);
+        try {
+
+            if (!new File("apollo.log").exists()) {
+                new File("apollo.log").createNewFile();
+            }
+
+            FileHandler logFile = new FileHandler("apollo.log", true);
+            logFile.setLevel(Level.FINE);
+            logger.addHandler(logFile);
+
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "File logger not working.", e);
+        }
     }
 
     @Override
     public void execute(TaskList taskList, Ui ui, Storage storage, ModuleList moduleList){
+        assert moduleList != null : "ModuleList should not be null!";
+        assert taskList != null : "TaskList should not be null!";
+        assert storage != null : "Storage should not be null!";
         try {
             Module toDelete = moduleList.findModule(moduleCode);
             if (toDelete == null) {
