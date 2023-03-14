3c0de6c036f811269435a64aa18778fd4b279731
ZiqiuZeng <88576865+ZiqiuZeng@users.noreply.github.com>
2023-03-14 21:08:07 +0800

    Add assertions for HelpManual()

--- src/main/java/seedu/badMaths/HelpManual.java
+++ src/main/java/seedu/badMaths/HelpManual.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class HelpManual {
     protected static String filePath = "docs/HelpManual.txt";
@@ -18,10 +20,16 @@ public class HelpManual {
                 InputStreamReader read = new InputStreamReader(new FileInputStream(file));
                 BufferedReader bufferedReader = new BufferedReader(read);
                 String lineTxt;
+                StringBuilder contentBuilder = new StringBuilder();
                 while ((lineTxt = bufferedReader.readLine()) != null) {
                     System.out.println(lineTxt);
+                    contentBuilder.append(lineTxt).append("\n");
                 }
                 read.close();
+                // Add an assertion to check if the contents of the file match the expected contents
+                String expectedContent = new String(Files.readAllBytes(Paths.get("docs/HelpManual.txt")));
+                String actualContent = contentBuilder.toString();
+                assert actualContent.equals(expectedContent) : "Contents of file do not match expected contents.";
             } else {
                 System.out.println("Cannot find Help Manual. Please try again.");
             }
