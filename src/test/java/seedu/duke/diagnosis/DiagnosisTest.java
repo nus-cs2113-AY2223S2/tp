48caac705eeaa4f60d14c30b13862a627d37d8ff
Brennanzuz <timingout365@gmail.com>
2023-03-14 15:36:36 +0800

    Removed redundant lines
    Removed redundant package imports to pass gradle test.

--- src/test/java/seedu/duke/diagnosis/DiagnosisTest.java
+++ src/test/java/seedu/duke/diagnosis/DiagnosisTest.java
@@ -1,15 +1,11 @@
 package seedu.duke.diagnosis;
 
 import org.junit.jupiter.api.Test;
-import seedu.duke.diagnosis.illnesses.Illness;
-import seedu.duke.diagnosis.illnesses.viral.GeneralCold;
 import seedu.duke.diagnosis.symptoms.Symptom;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 //@@Brennanzuz
