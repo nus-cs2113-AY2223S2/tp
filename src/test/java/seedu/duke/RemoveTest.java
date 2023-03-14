1bf7b2c03b94bea4d9957500a39062f9e5aa53b4
Ng-YZ <e0772909@u.nus.edu>
2023-03-15 01:46:07 +0800

    Edit RemoveTest

--- src/test/java/seedu/duke/RemoveTest.java
+++ src/test/java/seedu/duke/RemoveTest.java
@@ -22,15 +22,14 @@ class RemoveTest {
 
     @Test
     void removeByUpc() {
-        Item item1 = new Item("appleA", "123", "000", "0.0");
+        Item item1 = new Item("appleA", "123", "100", "0.0");
         Item item2 = new Item("apples", "012345678", "5000", "12.0");
         Item item3 = new Item("oranges", "876543210", "3000", "0.32");
         Inventory.addItem(item1);
         Inventory.addItem(item2);
         Inventory.addItem(item3);
 
-        assertEquals(0, Inventory.removeByUpc(item1,"123", "Y"));
-        assertEquals(1,Inventory.removeByUpc(item3, "876543210", "y"));
+        assertEquals(2,Inventory.removeByUpc(item3, "876543210", "y"));
         assertEquals(-1,Inventory.removeByUpc(item2, "012345678", "reply"));
         assertEquals(-1, Inventory.removeByUpc(item2, "012345678", "n"));
     }
