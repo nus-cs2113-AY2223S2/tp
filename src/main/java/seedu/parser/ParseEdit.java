47273a2794d42691c3ec00c51020efddeac0c6ce
itszhixuan <leezhixuan001@gmail.com>
2023-03-14 14:23:52 +0800

    Extracted parsing individual value to a seperate class and completed parsing for edit

--- src/main/java/seedu/parser/ParseEdit.java
+++ src/main/java/seedu/parser/ParseEdit.java
@@ -3,55 +3,28 @@ package seedu.parser;
 import seedu.commands.Command;
 import seedu.commands.EditCommand;
 import seedu.commands.InvalidCommand;
-import seedu.exceptions.EmptyStringException;
-import seedu.exceptions.ExceptionChecker;
 
 public class ParseEdit {
     private final String userInput;
-    private static final int OFFSET_DELIMITER = 2;
+    private static final String BLANK = "";
+    private static final String DSLASH = "d/";
+    private static final String ASLASH = "a/";
+    private static final String SSLASH = "s/";
     public ParseEdit(String userInput) {
         this.userInput = userInput;
     }
 
     public Command editItem() {
         // Format: edit, index, d/date, a/amount, s/description
-        System.out.println(userInput);
-        String indexVal = fetchInput("", "d/");
-        System.out.println(indexVal);
-        String dateVal = fetchInput("d/", "a/");
-        String amountVal = fetchInput("a/", "s/");
-        String descriptionVal = fetchInput("s/", "");
-        System.out.println(dateVal);
-        System.out.println(amountVal);
-        System.out.println(descriptionVal);
-
+        String indexVal = ParseIndividualValue.parseIndividualValue(userInput, BLANK, DSLASH);
+        String dateVal = ParseIndividualValue.parseIndividualValue(userInput, DSLASH, ASLASH);
+        String amountVal = ParseIndividualValue.parseIndividualValue(userInput, ASLASH, SSLASH);
+        String descriptionVal = ParseIndividualValue.parseIndividualValue(userInput, SSLASH, BLANK);
         try {
             int indexIntVal = Integer.parseInt(indexVal);
             return new EditCommand(indexIntVal, dateVal, amountVal, descriptionVal);
         } catch (NumberFormatException numberFormatException) {
-            return new InvalidCommand("Invalid");
-        }
-    }
-
-    public String fetchInput(String front, String back) {
-        try {
-            int positionOfFirstSlash;
-            if (!front.equals("")) {
-                positionOfFirstSlash = userInput.indexOf(front);
-            } else {
-                positionOfFirstSlash = -OFFSET_DELIMITER;
-            }
-            int positionOfSecondSlash;
-            if (!back.equals("")) {
-                positionOfSecondSlash = userInput.indexOf(back);
-            } else {
-                positionOfSecondSlash = userInput.length();
-            }
-            String value = userInput.substring(positionOfFirstSlash + OFFSET_DELIMITER, positionOfSecondSlash).trim();
-            ExceptionChecker.checkEmptyString(value);
-            return value;
-        } catch (StringIndexOutOfBoundsException | EmptyStringException e) {
-            return e.getMessage();
+            return new InvalidCommand("Input command in wrong format!");
         }
     }
 }
