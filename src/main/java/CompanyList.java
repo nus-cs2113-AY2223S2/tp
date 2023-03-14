a72cb9039574e2ea06c5f7a37405ce27fc764938
Yi Daeun <dawnayi2002@gmail.com>
2023-03-14 11:48:01 +0800

    Add add method to companylist and handle input mismatch exception

--- src/main/java/CompanyList.java
+++ src/main/java/CompanyList.java
@@ -5,4 +5,14 @@ import java.util.ArrayList;
 public class CompanyList {
 
     ArrayList<Company> companies = new ArrayList<>();
+
+    public boolean add(String companyName, int contactNumber, String contactEmail){
+        try{
+            Company newCompany = new Company(companyName, contactNumber, contactEmail);
+            companies.add(newCompany);
+            return true;
+        } catch(seedu.duke.InputMismatchException e){
+            return false;
+        }
+    }
 }
---
+++ src/main/java/seedu/duke/InputMismatchException.java
@@ -0,0 +1,4 @@
+package seedu.duke;
+
+public class InputMismatchException extends Exception{
+}
