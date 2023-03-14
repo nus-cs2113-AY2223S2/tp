0458720c8d53739719e59aaa8e1b1221d0616c4c
Rayleigh47 <yanming8870@outlook.com>
2023-03-14 23:13:24 +0800

    change income and invalid command

--- src/main/java/chching/parser/Incomes.java
+++ src/main/java/chching/parser/Incomes.java
@@ -1,50 +1,22 @@
 package chching.parser;
 
-import chching.record.IncomeList;
+import chching.record.Income;
 
 import java.util.HashMap;
 
 public class Incomes {
-    public static chching.record.Income parseIncome(HashMap<String, String> argumentsByField) {
-        chching.record.Income inc = null;
+    public static Income parseIncome(HashMap<String, String> argumentsByField) {
+        Income inc = null;
         try {
-            String incomeCategory = argumentsByField.get("c");
             String incomeDescription = argumentsByField.get("de");
             String incomeDate = argumentsByField.get("da");
             float incomeValue = Float.parseFloat(argumentsByField.get("v"));
+            inc = new Income(incomeDescription, incomeDate, incomeValue);
             assert incomeValue > 0 : "incomeValue has to be more than 0";
-            inc = new chching.record.Income(incomeCategory, incomeDescription, incomeDate, incomeValue);
+            inc = new Income(incomeDescription, incomeDate, incomeValue);
         } catch (Exception e) {
             System.out.println("Trouble adding income");
         }
         return inc;
-    }
-
-    public static void editIncome(HashMap<String, String> argumentsByField, IncomeList incomeList) {
-        try {
-            int index = Integer.parseInt(argumentsByField.get("i"));
-            assert index >= 0 : "index should be greater than or equal to zero";
-            String updateCategory = null;
-            String updateDescription = null;
-            String updateDate = null;
-            float updateValue = 0;
-            if (argumentsByField.containsKey("c")) {
-                updateCategory = argumentsByField.get("c");
-            }
-            if (argumentsByField.containsKey("de")) {
-                updateDescription = argumentsByField.get("de");
-            }
-            if (argumentsByField.containsKey("da")) {
-                updateDate = argumentsByField.get("da");
-            }
-            if (argumentsByField.containsKey("v")) {
-                updateValue = Float.parseFloat(argumentsByField.get("v"));
-                assert updateValue > 0 : "updateValue has to be more than 0";
-            }
-            incomeList.editIncome(index, updateCategory, updateDescription, updateDate, updateValue);
-            System.out.println("Income modified");
-        } catch (Exception e) {
-            System.out.println("Trouble editing income");
-        }
     }
 }
--- src/main/java/chching/record/ExpenseList.java
+++ src/main/java/chching/record/ExpenseList.java
@@ -29,22 +29,8 @@ public class ExpenseList extends RecordList{
         } catch (IndexOutOfBoundsException e) {
             throw new IndexOutOfBoundsException("There is no expense with this index");
         }
-    }   
-    public void editExpense(int i, String category, String description, String date, float value) {
-        Expense exp = (Expense) expenseList.get(i - 1);
-        if(!(category == null)) {
-            exp.category = category;
-        }
-        if(!(description == null)) {
-            exp.description = description;
-        }
-        if(!(date == null)) {
-            exp.date = date;
-        }
-        if(value > 0) {
-            exp.value = value;
-        }
     }
+
     public void printExpenseList() {
         for (int i = 1; i <= expenseList.size(); i++) {
             Record record = expenseList.get(i - 1);
