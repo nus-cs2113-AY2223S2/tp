f4976488e4e5ee634f3ed096d9154ca7487d4163
itszhixuan <leezhixuan001@gmail.com>
2023-03-14 14:05:46 +0800

    Update ParseEdit to cleaner code when extracting values

--- src/main/java/seedu/parser/ParseEdit.java
+++ src/main/java/seedu/parser/ParseEdit.java
@@ -3,35 +3,55 @@ package seedu.parser;
 import seedu.commands.Command;
 import seedu.commands.EditCommand;
 import seedu.commands.InvalidCommand;
+import seedu.exceptions.EmptyStringException;
+import seedu.exceptions.ExceptionChecker;
 
 public class ParseEdit {
-    public static Command editItem(String line) {
+    private final String userInput;
+    private static final int OFFSET_DELIMITER = 2;
+    public ParseEdit(String userInput) {
+        this.userInput = userInput;
+    }
+
+    public Command editItem() {
         // Format: edit, index, d/date, a/amount, s/description
-        int space = line.indexOf(" ");
-        String[] allDetails = SplitCommand.splitCommand(space, line);
-        String details = allDetails[1];
-
-        int posDSlash = details.indexOf('/');
-        String[] furtherDetails = SplitCommand.splitCommand(posDSlash, details);
-        String indexVal = furtherDetails[0];
-        String editDetails = furtherDetails[1];
-
-        int posASlash = editDetails.indexOf('/');
-        String[] dateAmountDescription = SplitCommand.splitCommand(posASlash,editDetails);
-        String dateVal = dateAmountDescription[0];
-        String editPriceAndDescription = dateAmountDescription[1];
-
-        int posSSlash = editPriceAndDescription.indexOf('/');
-        String[] amountDescription = SplitCommand.splitCommand(posSSlash,editPriceAndDescription);
-        String amountVal = amountDescription[0];
-        String descriptionVal = dateAmountDescription[1];
+        System.out.println(userInput);
+        String indexVal = fetchInput("", "d/");
+        System.out.println(indexVal);
+        String dateVal = fetchInput("d/", "a/");
+        String amountVal = fetchInput("a/", "s/");
+        String descriptionVal = fetchInput("s/", "");
+        System.out.println(dateVal);
+        System.out.println(amountVal);
+        System.out.println(descriptionVal);
 
         try {
             int indexIntVal = Integer.parseInt(indexVal);
-            System.out.println(indexIntVal);
             return new EditCommand(indexIntVal, dateVal, amountVal, descriptionVal);
         } catch (NumberFormatException numberFormatException) {
             return new InvalidCommand("Invalid");
+        }
+    }
+
+    public String fetchInput(String front, String back) {
+        try {
+            int positionOfFirstSlash;
+            if (!front.equals("")) {
+                positionOfFirstSlash = userInput.indexOf(front);
+            } else {
+                positionOfFirstSlash = -OFFSET_DELIMITER;
+            }
+            int positionOfSecondSlash;
+            if (!back.equals("")) {
+                positionOfSecondSlash = userInput.indexOf(back);
+            } else {
+                positionOfSecondSlash = userInput.length();
+            }
+            String value = userInput.substring(positionOfFirstSlash + OFFSET_DELIMITER, positionOfSecondSlash).trim();
+            ExceptionChecker.checkEmptyString(value);
+            return value;
+        } catch (StringIndexOutOfBoundsException | EmptyStringException e) {
+            return e.getMessage();
         }
     }
 }
