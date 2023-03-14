203dbed32b505ceadf90915c015706f8c0913812
ZiqiuZeng <88576865+ZiqiuZeng@users.noreply.github.com>
2023-03-14 22:39:02 +0800

    Modify the testReadHelpManual() method

--- src/test/java/seedu/badMaths/HelpManualTest.java
+++ src/test/java/seedu/badMaths/HelpManualTest.java
@@ -13,19 +13,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class HelpManualTest {
     @Test
     public void testReadHelpManual() throws IOException {
-        // Redirect System.out to a ByteArrayOutputStream
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-
         // Call the readHelpManual() method
         HelpManual.readHelpManual();
 
         // Check if the output matches the expected output
         String expectedOutput = new String(Files.readAllBytes(Paths.get("docs/HelpManual.txt")));
-        assertEquals(expectedOutput, outContent.toString());
-
-        // Reset System.out
-        System.setOut(System.out);
+        assertEquals(expectedOutput, HelpManual.getContent());
     }
 
 }
