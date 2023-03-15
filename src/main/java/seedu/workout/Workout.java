c9bc716b3ac4313c77d8abeb0781b22050df820c
calebcjl <calebchan2000@gmail.com>
2023-03-16 02:59:09 +0800

    Refactor code for more OOP

--- src/main/java/seedu/workout/Workout.java
+++ src/main/java/seedu/workout/Workout.java
@@ -1,26 +1,22 @@
 package seedu.workout;
 
-import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Date;
-import java.text.SimpleDateFormat;
 
 public class Workout {
     private Date date;
     private ArrayList<Exercise> workoutExercises;
 
-    public Workout(String dateString) throws ParseException {
-        // Create a SimpleDateFormat object with the desired date format
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yy");
-
-        // Parse the input dateString to create a Date object
-        this.date = dateFormat.parse(dateString);
+    public Workout(Date date) {
+        this.date = date;
         workoutExercises = new ArrayList<>();
     }
 
     public void addExercise(Exercise exercise) {
         workoutExercises.add(exercise);
+        System.out.println("Added " + exercise);
     }
+
     public Date getDate() {
         return date;
     }
