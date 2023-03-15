678087825015fb48716252c2c4de07334916a613
TJW0911 <jianwei0911@gmail.com>
2023-03-15 13:10:12 +0800

    Extracted printing of help commands to a few methods

--- src/main/java/seedu/duke/ui/DukeUI.java
+++ src/main/java/seedu/duke/ui/DukeUI.java
@@ -50,26 +50,54 @@ public class DukeUI {
      */
 
     public void printHelpMessages() {
+        PrintHelpMessageHeader();
+        printListIngredientsHelp();
+        printAddCommandHelp();
+        PrintRemoveCommandHelp();
+        PrintRecipePossibleHelp();
+        PrintRecipeAllHelp();
+        PrintByeCommandHelp();
+    }
+
+    private static void PrintHelpMessageHeader() {
         System.out.println("COMMAND SUMMARY:");
         System.out.println(DIVIDER);
-        System.out.println("Command: list");
-        System.out.println("Lists out current ingredients and their respective quantities stored in the fridge.");
+    }
+
+    private static void PrintByeCommandHelp() {
+        System.out.println("Command: bye");
+        System.out.println("Exits the program");
         System.out.println(DIVIDER);
-        System.out.println("Command: add <ingredient> /qty <quantity>");
-        System.out.println("Add an ingredient of specified quantity to your ingredient inventory.");
+    }
+
+    private static void PrintRecipeAllHelp() {
+        System.out.println("Command: recipe all");
+        System.out.println("Lists out all the recipes currently stored in the recipe inventory.");
         System.out.println(DIVIDER);
-        System.out.println("Command: remove <ingredient> /qty <quantity>");
-        System.out.println("Remove an ingredient of specified quantity from your ingredient inventory.");
-        System.out.println(DIVIDER);
+    }
+
+    private static void PrintRecipePossibleHelp() {
         System.out.println("Command: recipe possible");
         System.out.println("Lists out all the recipes that can be made by the ingredients that are currently " +
                 "in the ingredient inventory.");
         System.out.println(DIVIDER);
-        System.out.println("Command: recipe all");
-        System.out.println("Lists out all the recipes currently stored in the recipe inventory.");
+    }
+
+    private static void PrintRemoveCommandHelp() {
+        System.out.println("Command: remove <ingredient> /qty <quantity>");
+        System.out.println("Remove an ingredient of specified quantity from your ingredient inventory.");
         System.out.println(DIVIDER);
-        System.out.println("Command: bye");
-        System.out.println("Exits the program");
+    }
+
+    private static void printAddCommandHelp() {
+        System.out.println("Command: add <ingredient> /qty <quantity>");
+        System.out.println("Add an ingredient of specified quantity to your ingredient inventory.");
+        System.out.println(DIVIDER);
+    }
+
+    private static void printListIngredientsHelp() {
+        System.out.println("Command: list");
+        System.out.println("Lists out current ingredients and their respective quantities stored in the fridge.");
         System.out.println(DIVIDER);
     }
 }
