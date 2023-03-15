9bc518af664c715eb174829c3524722ec89cf547
ChubbsBunns <troskproductions@gmail.com>
2023-03-15 13:03:25 +0800

    Fix help error and update available commands

--- src/main/java/seedu/duke/util/OuputText.java
+++ src/main/java/seedu/duke/util/OuputText.java
@@ -2,9 +2,7 @@ package seedu.duke.util;
 
 public class OuputText {
     public void showAvailableCommands() {
-
         System.out.println("These are some commands available: ");
-        System.out.println("Get a quick exercise: quick x (where x is a positive integer)");
         System.out.println("Generate a specific list of exercises: generate PARAM1 PARAM2 ... x ");
         System.out.println("PARAM stands for a specific requirement you want to include in your exercise.\n");
         System.out.println("The following parameters are available for Generate command:");
