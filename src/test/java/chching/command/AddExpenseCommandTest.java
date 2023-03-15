69d12b2217022c2c1e91de3edf6c55f921bbf5c9
thomasjlalba <e0773507@u.nus.edu>
2023-03-15 12:17:46 +0800

    Add JUnit testing for AddExpenseCommand class

---
+++ src/test/java/chching/command/AddExpenseCommandTest.java
@@ -0,0 +1,75 @@
+package chching.command;
+
+import chching.record.Expense;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class AddExpenseCommandTest {
+    @Test
+    void AddExpenseCommand_nullExpense_exceptionThrown() {
+        try {
+            new AddExpenseCommand(null);
+            fail(); // test should not reach this line
+        } catch (Exception e) {
+            assertEquals("No fields found", e.getMessage());
+        }
+    }
+    
+    @Test
+    void AddExpenseCommand_missingCategoryField_exceptionThrown() {
+        Expense input = new Expense(null, "public transport", "1st apr 2023", (float) 1.50);
+        try {
+            new AddExpenseCommand(input);
+            fail();
+        } catch (Exception e) {
+            assertEquals("Missing category field", e.getMessage());
+        }
+    }
+    
+    @Test
+    void AddExpenseCommand_missingDescriptionField_exceptionThrown() {
+        Expense input = new Expense("transport", null, "1st apr 2023", (float) 1.50);
+        try {
+            new AddExpenseCommand(input);
+            fail();
+        } catch (Exception e) {
+            assertEquals("Missing description field", e.getMessage());
+        }
+    }
+    
+    @Test
+    void AddExpenseCommand_missingDateField_exceptionThrown() {
+        Expense input = new Expense("transport", "public transport", null, (float) 1.50);
+        try {
+            new AddExpenseCommand(input);
+            fail();
+        } catch (Exception e) {
+            assertEquals("Missing date field", e.getMessage());
+        }
+    }
+    
+    @Test
+    void AddExpenseCommand_zeroValueField_exceptionThrown() {
+        Expense input = new Expense("transport", "public transport", "1st apr 2023", (float) 0);
+        try {
+            new AddExpenseCommand(input);
+            fail();
+        } catch (Exception e) {
+            assertEquals("Invalid/Missing expense value", e.getMessage());
+        }
+    }
+    
+    @Test
+    void AddExpenseCommand_negativeValueField_exceptionThrown() {
+        Expense input = new Expense("transport", "public transport", "1st apr 2023", (float) -1.50);
+        try {
+            new AddExpenseCommand(input);
+            fail();
+        } catch (Exception e) {
+            assertEquals("Invalid/Missing expense value", e.getMessage());
+        }
+    }
+    
+    
+}
