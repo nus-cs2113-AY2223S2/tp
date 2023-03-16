e49567aec864072b0e1aebf673f1e47340ef8711
SSzeWen <seetszewen@gmail.com>
2023-03-16 23:08:16 +0800

    Add DeleteModuleCommand Class

---
+++ src/main/java/seedu/duke/command/DeleteModuleCommand.java
@@ -0,0 +1,31 @@
+package seedu.duke.command;
+
+import seedu.duke.Module;
+import seedu.duke.Parser;
+import seedu.duke.Storage;
+
+import java.util.ArrayList;
+
+public class DeleteModuleCommand extends Command {
+
+    private Storage storage;
+
+    private int indexToRemove;
+
+    private ArrayList<Module> modules;
+
+    public DeleteModuleCommand(Storage storage, int indexToRemove, ArrayList<Module> modules) {
+        this.storage = storage;
+        this.indexToRemove = indexToRemove;
+        this.modules = modules;
+    }
+
+    @Override
+    public void execute() {
+        boolean isDeleteSuccessful = Parser.deleteModule(indexToRemove, modules, storage);
+        if (!isDeleteSuccessful) {
+            return;
+        }
+        ui.printDeleteModMessage();
+    }
+}
