e6f488cc981f4a20472d3ee8f5ed7acd920b6cef
Wilson Lee Jun Wei <leejunwei2000@gmail.com>
2023-03-16 19:32:37 +0800

    Add comment for NotesTest

--- src/test/java/seedu/badMaths/NotesTest.java
+++ src/test/java/seedu/badMaths/NotesTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
-class NotesTest {
+class NotesTest { // this class contains the various test methods
     ArrayList<String> cache = new ArrayList<>();
     @Test
     public void handleCache() {
