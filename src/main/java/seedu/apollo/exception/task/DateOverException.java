4cd23d3b0e5edb2b604fa57504028eade904e452
Hong Lin Shang <e0774890@u.nus.edu>
2023-03-16 13:03:13 +0800

    Add JavaDoc, accessibility

--- src/main/java/seedu/apollo/exception/task/DateOverException.java
+++ src/main/java/seedu/apollo/exception/task/DateOverException.java
@@ -3,15 +3,29 @@ package seedu.apollo.exception.task;
 import java.time.LocalDateTime;
 
 import static seedu.apollo.task.Task.printPattern;
+import static seedu.apollo.ui.Parser.COMMAND_DEADLINE_WORD;
+import static seedu.apollo.ui.Parser.COMMAND_EVENT_WORD;
 
+/**
+ * Exception class for when the Task being added occurs before the current date.
+ */
 public class DateOverException extends Throwable {
 
-    String type;
-    String description;
-    LocalDateTime by;
-    LocalDateTime from;
-    LocalDateTime to;
+    private final String type;
+    private final String description;
+    private final LocalDateTime by;
+    private final LocalDateTime from;
+    private final LocalDateTime to;
 
+    /**
+     * Initialises the exception with details of the Task that was not successfully added.
+     *
+     * @param type Type of task (Deadline or Event).
+     * @param description Description of task.
+     * @param by Due date (if task is Deadline).
+     * @param from Start date (if task is Event).
+     * @param to End date (if task is Event).
+     */
     public DateOverException(String type, String description, LocalDateTime by, LocalDateTime from, LocalDateTime to) {
         this.type = type;
         this.description = description;
@@ -27,9 +41,9 @@ public class DateOverException extends Throwable {
     public String toString() {
         assert (type.equals("deadline") | type.equals("event"));
         switch (type) {
-        case "deadline":
+        case COMMAND_DEADLINE_WORD:
             return type + ": " + description + " (by: " + by.format(printPattern) + ")";
-        case "event":
+        case COMMAND_EVENT_WORD:
             return type + ": " + description + " (from: " + from.format(printPattern) +
                     " to: " + to.format(printPattern) + ")";
         default:
