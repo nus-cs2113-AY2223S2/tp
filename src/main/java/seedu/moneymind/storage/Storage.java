9611736e3fa9670ba6c1760530dda1e383682c33
Toh-HongFeng <hf.toh2@gmail.com>
2023-03-16 15:49:10 +0800

    add logger to Storage.java

--- src/main/java/seedu/moneymind/storage/Storage.java
+++ src/main/java/seedu/moneymind/storage/Storage.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.logging.Logger;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -20,6 +21,7 @@ import static seedu.moneymind.Strings.NEW_LINE;
 public class Storage {
     private static File textFile;
     private static String filePath = "EventList.txt";
+    private static Logger logger = Logger.getLogger("Storage");
 
     /**
      * Constructor for Storage class
@@ -32,12 +34,15 @@ public class Storage {
      * Sets up the file to be read and written to
      */
     private static void setupFile() {
+        logger.info("Setting up file");
+        // create file object
         textFile = new File(filePath);
         
         // create file if it does not exist
         try {
             textFile.createNewFile();
         } catch (IOException e) {
+            logger.warning("File already exists");
             System.out.println("The file already exists.");
         }
     }
@@ -56,6 +61,7 @@ public class Storage {
             dukeWriter.write(writeToFile);
             dukeWriter.close();
         } catch (IOException e) {
+            logger.warning("File cannot be accessed");
             System.out.println("I cannot seem to access the saved file. Did you perhaps delete it?");
         }
     }
@@ -77,6 +83,7 @@ public class Storage {
                 fileString += textFileScanner.nextLine() + System.lineSeparator();
             }
         } catch (FileNotFoundException e) {
+            logger.warning("File cannot be accessed");
             System.out.println("I cannot seem to access the saved tasks. Did you perhaps lock it away?");
         }
         // split fileString into 2 parts using STORAGE_CATEGORY_MAP
@@ -84,6 +91,7 @@ public class Storage {
         // convert fileString to ArrayList of Category
         ArrayList<Category> savedList = StringToCategory.stringToCategory(splitString[0]);
         if (splitString.length == 2) {
+            logger.info("Loading category map");
             LoadToCategoryMap.loadToCategoryMap(splitString[1]);
         }
         return savedList;
@@ -93,6 +101,7 @@ public class Storage {
      * Saves the current list of Category to EventList.txt file
      */
     public void save() {
+        logger.info("Saving to file");
         saveToFile(CategoryList.categories);
     }
 
@@ -100,6 +109,7 @@ public class Storage {
      * Loads the list of Category from EventList.txt file
      */
     public void load() {
+        logger.info("Loading from file");
         CategoryList.categories = loadFromFile();
     }
 }
