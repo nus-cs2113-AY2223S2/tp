2ad294c260795acbedaaef6fae86eea2ebc26ca0
darrenangwx <darrenawx@gmail.com>
2023-03-17 13:06:27 +0800

    Update assert in DinerDirector.java

--- src/main/java/dinerdirector/DinerDirector.java
+++ src/main/java/dinerdirector/DinerDirector.java
@@ -40,7 +40,7 @@ public class DinerDirector {
         while (!isExit) {
             String userInput = ui.getUserInput();
             Command command = new Parser().parseCommand(userInput);
-            assert command.equals(command.getClass()) : "command should be a command class";
+            assert command instanceof Command : "command should be a command class";
             command.execute(ui);
             isExit = command.isExit();
         }
