f00c150d2e0c25a65a73be89dd33a15f25462aff
TJ\user <e0773494@u.nus.edu>
2023-03-17 14:44:21 +0800

    Logging and Assertions for calculateCaloricNeeds method

--- src/main/java/seedu/entities/User.java
+++ src/main/java/seedu/entities/User.java
@@ -1,10 +1,16 @@
 package seedu.entities;
+
+import java.util.Objects;
+import java.util.logging.*;
+
 public class User {
     private String name;
     private float weight;
     private float height;
     private int age;
     private String gender;
+
+    private static Logger logger = Logger.getLogger(Logger.class.getName());
 
     private double caloricLimit;
     public User() {
@@ -70,15 +76,24 @@ public class User {
     }
 
     public static double calculateCaloricNeeds (float weight, float height, int age, String gender) {
+        logger.log(Level.INFO, "going to start processing");
         double caloricNeeds;
         switch(gender.toLowerCase()) {
         case "male":
             caloricNeeds = 66 + (13.7 * weight) + (5 * height) - (4.7 * age);
             assert caloricNeeds > 0: "Caloric Needs should be more than O";
+            if (caloricNeeds < 0) {
+                logger.log(Level.WARNING, "Input error");
+            }
+            logger.log(Level.INFO, "end of processing");
             return caloricNeeds;
         case "female":
             caloricNeeds = 655 + (9.6 * weight) + (1.8 * height) - (4.7 * age);
             assert caloricNeeds > 0: "Caloric Needs should be more than O";
+            if (caloricNeeds < 0) {
+                logger.log(Level.WARNING, "Input error");
+            }
+            logger.log(Level.INFO, "end of processing");
             return caloricNeeds;
         default:
             System.out.println("Gender not provided, cannot calculate caloric needs accurately");
@@ -96,8 +111,11 @@ public class User {
 
     public String[] toWriteFormat() {
         String weight = Float.toString(this.weight);
+        assert !Objects.equals(weight, "0") : "Weight should be non 0";
         String height = Float.toString(this.height);
+        assert !Objects.equals(height, "0"): "Height should be non 0";
         String age = Integer.toString(this.age);
+        assert !Objects.equals(age, "0"): "Age should be non 0";
         String[] value = { this.name, weight, height, age, this.gender };
         return value;
     }
