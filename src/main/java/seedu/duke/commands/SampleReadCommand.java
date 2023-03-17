969d0ba343baa45212f0d11cc01ed8c8e1ff6630
Eugene Ang <eugeneang654871@gmail.com>
2023-03-18 03:02:50 +0800

    Add JavaDoc

--- src/main/java/seedu/duke/commands/SampleReadCommand.java
+++ src/main/java/seedu/duke/commands/SampleReadCommand.java
@@ -10,10 +10,23 @@ import seedu.duke.userdata.Session;
 public class SampleReadCommand extends Command {
     private UserCareerData userCareerData;
 
+    /**
+     * Sets the userCareerData as a local variable for use later
+     *
+     * @param userCareerData
+     */
+
     public SampleReadCommand (UserCareerData userCareerData) {
         this.userCareerData = userCareerData;
     }
 
+    /**
+     * Simply prints the getName() of each exercise in session
+     *
+     * @param ui Ui class to handle all ui related events
+     * @param exerciseGenerator generates exercises
+     * @throws DukeError occurs when there is an invalid command
+     */
     public void executeCommand (Ui ui, GenerateExercise exerciseGenerator) throws DukeError {
         for (Session session : userCareerData.getTotalUserCareerSessions()) {
             for (ExerciseData exercise : session.getSessionExercises()) {
--- src/main/java/seedu/duke/commands/SampleSavingCommand.java
+++ src/main/java/seedu/duke/commands/SampleSavingCommand.java
@@ -12,12 +12,26 @@ public class SampleSavingCommand extends Command {
     private final Session session;
     private UserCareerData userCareerData;
 
+    /**
+     * Sets the updated / appended userCareerData by appending a newly created session.class from arraylist of
+     * exercises (session exercises)
+     *
+     * @param userCareerData all previous user career data
+     * @param sessionExercises the exercises completed in the current session to be added
+     */
+
     public SampleSavingCommand (UserCareerData userCareerData, ArrayList<ExerciseData> sessionExercises) {
         this.session = new Session(sessionExercises);
         this.userCareerData = userCareerData;
         this.userCareerData.addWorkoutSession(session);
     }
 
+    /**
+     * Writes the UserCareerData to json file and prints the exercises added as a confirmatory message
+     *
+     * @param ui handles all ui events (to be used later)
+     * @param exerciseGenerator (unused here)
+     */
     public void executeCommand (Ui ui, GenerateExercise exerciseGenerator) {
         WriteUserData.writeToJson("userData.json", this.userCareerData);
         System.out.println("I have written:");
