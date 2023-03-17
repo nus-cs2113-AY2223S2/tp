434170d380d716f41b4b0f65bd0a84e410c09bb4
namsengi11 <namsengi11@gmail.com>
2023-03-17 14:31:13 +0800

    Add logs and assertions for future troubleshooting

--- src/main/java/seedu/brokeMan/parser/StringToTime.java
+++ src/main/java/seedu/brokeMan/parser/StringToTime.java
@@ -8,10 +8,13 @@ import static seedu.brokeMan.common.Messages.MESSAGE_INVALID_TIME;
 public class StringToTime {
     public static LocalDateTime convertStringToTime(String timeInString) throws DateTimeException {
         String[] timeByWord = timeInString.split(" ");
-        assert (timeByWord.length == 5) : MESSAGE_INVALID_TIME;
-        if (!areWordsValidTimeNumbers(timeByWord)) {
+        if (timeByWord.length != 5) {
+            throw new DateTimeException(MESSAGE_INVALID_TIME);
+        } else if (!areWordsValidTimeNumbers(timeByWord)) {
             throw new DateTimeException(MESSAGE_INVALID_TIME);
         }
+        // dummy assert
+        assert (timeByWord.length == 5) : MESSAGE_INVALID_TIME;
 
         Integer[] timeByNumber = new Integer[5];
         for (int i = 0; i < 5; i++) {
