e6bc56c45078cb6553e88f57c4e962fdc19dab36
ZIZI-czh <czh1196354073@gmail.com>
2023-03-18 23:14:45 +0800

    solve the bug

--- src/test/java/seedu/command/ListCommandTest.java
+++ src/test/java/seedu/command/ListCommandTest.java
@@ -8,7 +8,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
+
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -21,7 +21,6 @@ public class ListCommandTest {
         // and add some sample data to the workoutList
 
         SimpleDateFormat format = new SimpleDateFormat("dd/MM/yy");
-        Date date = format.parse("11/11/23");
 
 
         // Redirect the console output to a stream for testing
@@ -31,11 +30,12 @@ public class ListCommandTest {
 
         // Call the method to be tested
         WorkoutList testList = new WorkoutList();
-        testList.addWorkout(new Workout(date));
+        testList.addWorkout(new Workout(format.parse("11/11/23")));
+        testList.addWorkout(new Workout(format.parse("11/10/23")));
         testList.showWorkoutList();
 
         // Assert that the console output matches the expected output
-        assertEquals("Here are the list of dates for your workout: \n11-11-23",
+        assertEquals("Here are the list of dates for your workout: \n11-11-23\n11-10-23",
                 outContent.toString());
 
     }
