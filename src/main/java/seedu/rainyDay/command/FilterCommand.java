84b6eb2f88d0ec5ff11f45a6d22dff26398fa097
lil1n <lilinongsc@gmail.com>
2023-03-19 22:26:55 +0800

    improve FilterCommand to be more OOP

--- src/main/java/seedu/rainyDay/command/FilterCommand.java
+++ src/main/java/seedu/rainyDay/command/FilterCommand.java
@@ -2,9 +2,6 @@ package seedu.rainyDay.command;
 
 
 import seedu.rainyDay.data.FinancialStatement;
-import seedu.rainyDay.exceptions.ErrorMessage;
-import seedu.rainyDay.exceptions.RainyDayException;
-import seedu.rainyDay.modules.Ui;
 
 import java.util.ArrayList;
 import java.util.logging.FileHandler;
@@ -13,7 +10,7 @@ import java.util.logging.LogManager;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
-public class FilterCommand extends Command implements ReportFormat {
+public class FilterCommand extends Command implements FormatReport {
 
     private static final Logger logger = Logger.getLogger(FilterCommand.class.getName());
     private final String description;
@@ -31,37 +28,13 @@ public class FilterCommand extends Command implements ReportFormat {
             logger.addHandler(fileHandler);
         } catch (Exception e) {
             System.out.println("unable to log FilterCommand class");
-            logger.log(Level.SEVERE, "File logger not working.", e); // todo check if useless
+            logger.log(Level.SEVERE, "File logger not working.", e);
         }
     }
 
-//    @Override
-//    public void execute() throws RainyDayException{
-//        setupLogger();
-//        logger.log(Level.INFO, "starting FilterCommand.execute()");
-//
-//        ArrayList<FinancialStatement> filteredList = (ArrayList<FinancialStatement>) financialReport
-//                .getFinancialReport().stream()
-//                .filter(t -> t.getDescription().contains(this.description))
-//                .collect(Collectors.toList());
-//
-//        if (filteredList.size() == 0) {
-//            throw new RainyDayException(ErrorMessage.NO_STATEMENTS_MATCH_DESCRIPTION.toString());
-//        } else {
-//            for (int i = 0; i < filteredList.size(); i += 1) {
-//                logger.log(Level.INFO, "starting statement " + i);
-//                FinancialStatement currentStatement = filteredList.get(i);
-//
-//                Ui.printFinancialStatement(i + 1, currentStatement);
-//                logger.log(Level.INFO, "passed statement " + i);
-//            }
-//        }
-//
-//        logger.log(Level.INFO, " end of FilterCommand.execute()");
-//    }
-
+    // todo
     @Override
-    public CommandResult execute() throws RainyDayException {
+    public CommandResult execute() {
         setupLogger();
         logger.log(Level.INFO, "starting FilterCommand.execute()");
 
@@ -69,16 +42,17 @@ public class FilterCommand extends Command implements ReportFormat {
                 .getFinancialReport().stream()
                 .filter(t -> t.getDescription().contains(this.description))
                 .collect(Collectors.toList());
+        String outcome = "";
 
         if (filteredList.size() == 0) {
-            throw new RainyDayException(ErrorMessage.NO_STATEMENTS_MATCH_DESCRIPTION.toString());
+            outcome = "We could not find any matches for your description in your report";
         } else {
-            String outcome = "";
+            outcome = ""; // todo
             for (int i = 0; i < filteredList.size(); i += 1) {
                 logger.log(Level.INFO, "starting statement " + i);
                 FinancialStatement currentStatement = filteredList.get(i);
 
-                Ui.printFinancialStatement(i + 1, currentStatement);
+                outcome += FormatReport.formatFinancialStatement(i + 1, currentStatement);
                 logger.log(Level.INFO, "passed statement " + i);
             }
         }
