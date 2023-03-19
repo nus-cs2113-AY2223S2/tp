a017b243ce8e4064783ce960a22783ff8ad2271d
cleoncheng2000 <e0725796@u.nus.edu>
2023-03-19 22:28:59 +0800

    Add newAppointment class

---
+++ src/main/java/functionalities/newAppointment.java
@@ -0,0 +1,19 @@
+package functionalities;
+
+public class newAppointment {
+    protected String uid;
+    protected Animal animal;
+    protected Owner owner;
+    protected boolean status;
+
+    public newAppointment(String uid, Animal animal, Owner owner) {
+        this.uid = uid;
+        this.animal = animal;
+        this.owner = owner;
+        this.status = false;
+    }
+
+    public String toString() {
+        return "";
+    }
+}
