ebab22f5cf843ad31d23f085dbed009e199c7d94
MuxPotato <maxchewjw@gmail.com>
2023-03-21 04:01:09 +0800

    Add list commands JUNIT testing

---
+++ src/test/java/seedu/duke/command/ListPuCommandTest.java
@@ -0,0 +1,50 @@
+package seedu.duke.command;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import seedu.duke.UI;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class ListPuCommandTest {
+    private final UI ui = new UI();
+
+    //@@author dfa-reused
+    //Reused from https://stackoverflow.com/questions/1119385/junit-test-for-system-out-println
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @BeforeAll
+    public void setUpStream() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterAll
+    public void restoreStream() {
+        System.setOut(originalOut);
+    }
+    //@@ author dfa
+
+    //Solution below adapted from https://stackoverflow.com/questions/1119385/junit-test-for-system-out-println
+    @Test
+    void execute() {
+        ListPuCommand listPuCommand = new ListPuCommand();
+        listPuCommand.execute();
+        assertEquals("This is the list of PUs:" + System.lineSeparator()
+                + "____________________________________________________________\n" + System.lineSeparator() +
+                "____________________________________________________________\n" +
+                System.lineSeparator() +
+                "1. KOREA UNIVERSITY KU" + System.lineSeparator() +
+                "2. KOREA ADVANCED INSTITUTE OF SCIENCE & TECHNOLOGY KAIST" + System.lineSeparator() +
+                "3. POHANG UNIVERSITY OF SCIENCE & TECHNOLOGY POSTECH" + System.lineSeparator() +
+                "4. SEOUL NATIONAL UNIVERSITY SNU" + System.lineSeparator() +
+                "5. YONSEI UNIVERSITY YU" + System.lineSeparator() +
+                "____________________________________________________________\n" + System.lineSeparator()
+                , outContent.toString());
+    }
+}
---
+++ src/test/java/seedu/duke/command/ListPuModulesCommandTest.java
@@ -0,0 +1,61 @@
+package seedu.duke.command;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import seedu.duke.UI;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class ListPuModulesCommandTest {
+    private final UI ui = new UI();
+
+    //@@author dfa-reused
+    //Reused from https://stackoverflow.com/questions/1119385/junit-test-for-system-out-println
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @BeforeAll
+    public void setUpStream() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterAll
+    public void restoreStream() {
+        System.setOut(originalOut);
+    }
+    //@@ author dfa
+
+    //Solution below adapted from https://stackoverflow.com/questions/1119385/junit-test-for-system-out-println
+    @Test
+    void execute() {
+        ListPuModulesCommand listPuModulesCommand = new ListPuModulesCommand(5,"YONSEI UNIVERSITY");
+        listPuModulesCommand.execute();
+        assertEquals("YONSEI UNIVERSITY Modules" + System.lineSeparator()
+                + "____________________________________________________________\n" +
+                System.lineSeparator() + "1. [EEE3314][INTRODUCTION ARTIFICIAL INTELLIGENCE][3]" + System.lineSeparator() +
+                "   maps to ----> [ME3663][Technical Elective][4]" + System.lineSeparator() +
+                "2. [MEU3710][NANO MECHANICAL ENGINEERING][3]" + System.lineSeparator() +
+                "   maps to ----> [ME3662][Technical Elective][4]" + System.lineSeparator() +
+                "3. [MEU3700][BIOMECHANICS][3]" + System.lineSeparator() +
+                "   maps to ----> [ME3661][Technical Elective][4]" + System.lineSeparator() +
+                "4. [MEU3600][ADVANCED MECHANICS OF MATERIALS][3]" + System.lineSeparator() +
+                "   maps to ----> [ME2114][Mechanics of Materials][4]" + System.lineSeparator() +
+                "5. [MEU3680][MECHANICAL SYSTEM CONTROL][3]" + System.lineSeparator() +
+                "   maps to ----> [ME2142][Feedback Control Systems][4]" + System.lineSeparator() +
+                "6. [MEU3010][Micro Mechanical system][3]" + System.lineSeparator() +
+                "   maps to ----> [ME3281][Microsystems Design and Applications][4]" + System.lineSeparator() +
+                "7. [MEU5370][Finite Element Method][3]" + System.lineSeparator() +
+                "   maps to ----> [ME4291][Finite Element Analysis][4]" + System.lineSeparator() +
+                "8. [MEU6230][VISCOUS FLUID DYNAMICS][3]" + System.lineSeparator() +
+                "   maps to ----> [ME2135][Intermediate Fluid Mechanics][4]" + System.lineSeparator() +
+                "9. [DAA3250][CHEM ENG THERMODYNAMICS I][0]" + System.lineSeparator() +
+                "   maps to ----> [ME3221][Sustainable Energy Conversion][4]" + System.lineSeparator() +
+                "____________________________________________________________\n" + System.lineSeparator(), outContent.toString());
+
+    }
+}
