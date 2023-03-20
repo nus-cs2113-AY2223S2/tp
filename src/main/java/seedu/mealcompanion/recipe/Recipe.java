d30b7285925176ec698acdc97c31761e8875fee4
Jjzeng123 <E0725292@u.nus.edu>
2023-03-21 00:26:26 +0800

    Add calorie counts to recipes, add command to print calorie counts

--- src/main/java/seedu/mealcompanion/recipe/Recipe.java
+++ src/main/java/seedu/mealcompanion/recipe/Recipe.java
@@ -4,17 +4,23 @@ import seedu.mealcompanion.ingredient.IngredientList;
 
 public class Recipe {
     private String name;
+    private int calorieCount;
     private IngredientList ingredients;
     private InstructionList instructions;
 
-    public Recipe(String name, IngredientList ingredients, InstructionList instructions) {
+    public Recipe(String name, int calorieCount, IngredientList ingredients, InstructionList instructions) {
         this.name = name;
+        this.calorieCount = calorieCount;
         this.ingredients = ingredients;
         this.instructions = instructions;
     }
 
     public String getName() {
         return name;
+    }
+
+    public int getCalorieCount() {
+        return calorieCount;
     }
 
     public IngredientList getIngredients() {
