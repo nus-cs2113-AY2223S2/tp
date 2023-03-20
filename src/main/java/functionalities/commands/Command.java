6e732b08e386bcd7f89bef3f7a730e65d2dfd14b
cleoncheng2000 <e0725796@u.nus.edu>
2023-03-20 16:03:28 +0800

    Update Commands class

--- src/main/java/functionalities/commands/Command.java
+++ src/main/java/functionalities/commands/Command.java
@@ -1,49 +1,16 @@
-package functionalities;
+package functionalities.commands;
 import exception.SniffException;
+import functionalities.SniffTasks;
+import seedu.sniff.Sniff;
 
-public class Command {
-    private final String command;
-    private final String uid;
-    private final String type;
-    private final String animal;
-    private final String name;
-    private final String date;
-
-
-    public Command(String userCommand, String uId, String type, String animal, String name, String date) {
-        this.command = userCommand;
-        this.uid = uid;
-        this.type = type;
-        this.animal = animal;
-        this.name = name;
-        this.date = date;
-    }
-
-    public void executeSniffCommands(SniffTasks tasks) throws SniffException {
-        if (command.equalsIgnoreCase("c")) {
-            tasks.addConsultation(uid, animal, owner, date, time);
-            Ui.showUserMessage("Consultation added successfully");
-        } else if (command.equalsIgnoreCase("v")) {
-            tasks.addVaccination(uid, animal, owner, date, time););
-            Ui.showUserMessage(" Vaccination added successfully");
-        } else if (command.equalsIgnoreCase("s")) {
-            tasks.addSurgery(uid, animal, owner, date, time););
-            Ui.showUserMessage(" Surgery added successfully");
-        } else if (command.equalsIgnoreCase("remove")) {
-            tasks.removeAppointment(Integer.parseInt(uId));
-            Ui.showUserMessage(" Task removed successfully");
-        } else if (command.equalsIgnoreCase("list")) {
-            tasks.listAppointments();
-        } else if (command.equalsIgnoreCase("view")) {
-            tasks.viewAppointment(uid);
-        } else if (command.equalsIgnoreCase("bye")) {
-            Ui.showUserMessage(" Bye. Hope to see you again soon!");
-        } else {
-            throw new SniffException(" Not a recognized Sniff Command!");
-        }
+public abstract class Command {
+    public boolean isExit;
+    protected Command() {
+        this.isExit = false;
     }
 
     public boolean isExit() {
-        return command.equals("bye");
+        return isExit;
     }
+    public void executeCommand(SniffTasks tasks) throws SniffException {}
 }
