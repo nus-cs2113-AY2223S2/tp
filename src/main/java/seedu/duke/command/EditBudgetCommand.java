bd8b3760c5449a2a790ce3171dd829e1e40785ef
SSzeWen <seetszewen@gmail.com>
2023-03-21 15:57:04 +0800

    Add skeleton Command classes for Budget

---
+++ src/main/java/seedu/duke/command/EditBudgetCommand.java
@@ -0,0 +1,17 @@
+package seedu.duke.command;
+
+import seedu.duke.budget.BudgetPlanner;
+
+public class EditBudgetCommand extends BudgetCommand {
+
+    private int budget;
+    public EditBudgetCommand(int budget, BudgetPlanner budgetPlanner) {
+        super(budgetPlanner);
+        this.budget = budget;
+    }
+    @Override
+    public void execute() {
+        budgetPlanner.setBudget(budget);
+        System.out.println("Budget has been changed to: " + budget);
+    }
+}
