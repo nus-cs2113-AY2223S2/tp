5a814dc9cc532733f61a7d7fe71844014c21eb83
Vignesh-30 <30vignesh@gmail.com>
2023-03-21 16:56:21 +0800

    Changed viewCommand to findCommand and created if else statements for category of find command

---
+++ src/main/java/functionalities/commands/FindCommand.java
@@ -0,0 +1,28 @@
+package functionalities.commands;
+
+import exception.SniffException;
+import functionalities.SniffTasks;
+
+
+public class FindCommand extends Command{
+
+    protected String category;
+    protected String details;
+    public FindCommand(String category, String details) {
+        this.category = category;
+        this.details = details;
+    }
+    @Override
+    public void executeCommand(SniffTasks tasks) throws SniffException {
+        if (category.equals("appointment")) {
+            tasks.findAppointment(details);
+        } else if (category.equals("animal")) {
+            tasks.findAnimal(details);
+        } else if (category.equals("type")) {
+            tasks.findType(details);
+        } else {
+            throw new SniffException("Unsupported find command!");
+        }
+    }
+
+}
