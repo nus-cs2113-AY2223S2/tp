3a24deaea05cda0cee5915f38fa7f2aff3106b35
darrenlsx <darren.lim@live.com.sg>
2023-03-22 16:40:25 +0800

    Add JUnit testing and make minor modifications in Parser

---
+++ src/test/java/seedu/duke/command/ExitCommandTest.java
@@ -0,0 +1,38 @@
+package seedu.duke.command;
+
+import org.junit.jupiter.api.Test;
+import seedu.duke.Module;
+import seedu.duke.Parser;
+import seedu.duke.Storage;
+import seedu.duke.University;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ExitCommandTest {
+
+    boolean setExit = false;
+
+    @Test
+    void userInput_exit_correctVariableType_success() {
+        String userInput = "exit";
+        ArrayList<University> universities = new ArrayList<>();
+        ArrayList<Module> modules = new ArrayList<>();
+        ArrayList<Module> puModules = new ArrayList<>();
+        Storage storage = new Storage();
+        Parser parser = new Parser();
+        assertTrue(parser.parseUserCommand(userInput, universities, modules, puModules, storage)
+                instanceof ExitCommand);
+    }
+
+    @Test
+    void execute_exit_success() {
+        ExitCommand exitCommand = new ExitCommand();
+        exitCommand.execute();
+        setExit = exitCommand.getIsExit();
+        assertEquals(true, setExit);
+    }
+
+}
