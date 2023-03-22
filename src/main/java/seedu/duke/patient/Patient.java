993e54eecce7e27017c9cfb320757ae18ea90f72
geeeetyx <garethgtyx@gmail.com>
2023-03-22 16:33:52 +0800

    feat: update user guide, improve UI

--- src/main/java/seedu/duke/patient/Patient.java
+++ src/main/java/seedu/duke/patient/Patient.java
@@ -49,9 +49,10 @@ public class Patient {
     public void updatePatientDiagnosisHistory(String diagnosis) {
         patientDiagnosisHistory.add(diagnosis);
     }
+
     /**
      * Updates patient's medicine history.
-     * @author tanyizhe
+     * @author tanyizhe, Brennanzuz
      * @param medicines ArrayList describing medicines patient has been prescribed
      */
     public void updatePatientMedicineHistory(String date, ArrayList<Medicine> medicines) {
@@ -86,6 +87,7 @@ public class Patient {
      * Prints the medication history of patient.
      */
     public void printPatientMedicineHistory() {
+        System.out.println("---------------------------------------------------");
         System.out.println("Medication History:");
         List<String> dates = Collections.list(patientMedicineHistory.keys());
         Collections.sort(dates);
--- src/main/java/seedu/duke/ui/Information.java
+++ src/main/java/seedu/duke/ui/Information.java
@@ -5,20 +5,19 @@ import seedu.duke.patient.Patient;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-
-import static seedu.duke.save.Storage.saveData;
-
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import static seedu.duke.save.Storage.saveData;
 
 /**
  * This class stores information on all patients registered by DoctorDuke.
  * @author Jeraldchen
  */
 public class Information {
-    private static HashMap<Integer, Patient> patientsList = new HashMap<>();
+    private static final HashMap<Integer, Patient> patientsList = new HashMap<>();
 
-    private static Logger logger = Logger.getLogger(Information.class.getName());
+    private static final Logger logger = Logger.getLogger(Information.class.getName());
 
     //storePatientInfo(personalInfo(name), patient)
     public static void storePatientInfo(int hash, Patient patient) {
@@ -31,12 +30,25 @@ public class Information {
         return patientsList.get(hash);
     }
 
+    /**
+     *
+     * @author JeraldChen, Geeeetyx
+     *
+     */
     public static void printDiagnosisHistory(int hash) {
         logger.log(Level.INFO, "Printing diagnosis history");
+
         Patient patient = patientsList.get(hash);
-        System.out.println("Your diagnosis history is: ");
-        for (int i = 0; i < patient.getPatientDiagnosisHistory().size(); i++) {
-            System.out.println(patient.getPatientDiagnosisHistory().get(i));
+
+        System.out.println("---------------------------------------------------");
+        if (patient.getPatientDiagnosisHistory().isEmpty()) {
+            System.out.println("You have no past diagnoses");
+        } else {
+            System.out.println("Your diagnosis history is: ");
+            System.out.println("---------------------------------------------------");
+            for (int i = 0; i < patient.getPatientDiagnosisHistory().size(); i++) {
+                System.out.println(patient.getPatientDiagnosisHistory().get(i));
+            }
         }
     }
 
@@ -47,9 +59,11 @@ public class Information {
      */
     public static void resetDiagnosisHistory(int hash) {
         logger.log(Level.INFO, "Resetting diagnosis history");
+
         Patient patient = patientsList.get(hash);
         patient.getPatientDiagnosisHistory().clear();
         assert patient.getPatientDiagnosisHistory().size() == 0 : "Diagnosis history should be empty";
+        System.out.println("---------------------------------------------------");
         System.out.println("Your diagnosis history has been reset.");
         saveData();
     }
@@ -78,13 +92,17 @@ public class Information {
 
     /**
      * Resets the symptom choice of the patient.
-     * @author Jeraldchen
+     * @author Jeraldchen, Geeeetyx
      * @param symptoms The symptom choice of the patient.
      */
     public static void resetSymptomChoice(ArrayList<Symptom> symptoms) {
         if (symptoms.size() != 0) {
             symptoms.clear();
+            System.out.println("---------------------------------------------------");
             System.out.println("Your symptom choice has been reset.");
+        } else {
+            System.out.println("---------------------------------------------------");
+            System.out.println("You have not entered any symptoms. No symptoms to reset.");
         }
     }
 
