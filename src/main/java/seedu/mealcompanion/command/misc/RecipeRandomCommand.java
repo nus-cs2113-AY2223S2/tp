381b6e98b65f4b933b79fec71802efbe3a5690ed
ngyida <e0726193@u.nus.edu>
2023-03-22 18:37:30 +0800

    Make RecipeRandom Command more defensive

--- src/main/java/seedu/mealcompanion/command/misc/RecipeRandomCommand.java
+++ src/main/java/seedu/mealcompanion/command/misc/RecipeRandomCommand.java
@@ -1,5 +1,6 @@
 package seedu.mealcompanion.command.misc;
 
+import seedu.mealcompanion.MealCompanionException;
 import seedu.mealcompanion.MealCompanionSession;
 import seedu.mealcompanion.command.ExecutableCommand;
 import seedu.mealcompanion.ingredient.Ingredient;
@@ -13,39 +14,52 @@ import java.util.Random;
 
 public class RecipeRandomCommand extends ExecutableCommand {
 
-    private int getRandomInt() {
+    private int getRandomIndex(RecipeList recipes) throws MealCompanionException {
+        // throw exception if there is no existing recipe
+        if (recipes.size() == 0) {
+            throw new MealCompanionException("No recipe is available!");
+        }
+
         Random rand = new Random();
-        return rand.nextInt();
+        int index = rand.nextInt() % recipes.size();
+        assert(index >= 0 && index < recipes.size()) : "index is out of range";
+        return index;
     }
 
     @Override
-    public void execute(MealCompanionSession mealCompanionSession) {
-        RecipeList recipes = mealCompanionSession.getRecipes();
-        int index = getRandomInt() % recipes.size();
-        assert(index >= 0 && index < recipes.size());
-        Recipe recipe = recipes.getRecipe(index);
-        mealCompanionSession.getUi().printMessage("A random recipe is chosen:");
-        mealCompanionSession.getUi().printMessage("Recipe for " + recipe.getName());
-        mealCompanionSession.getUi().printMessage(""); //print newline for all OS
-        mealCompanionSession.getUi().printMessage("Calories: " + recipe.getCalorieCount());
-        mealCompanionSession.getUi().printMessage(""); //print newline for all OS
-        mealCompanionSession.getUi().printMessage("Ingredients:");
-        IngredientList ingredients = recipe.getIngredients();
-        for (int i = 0; i < ingredients.size(); i++) {
-            Ingredient ingredient = ingredients.get(i);
-            IngredientMetadata metadata = ingredient.getMetadata();
-            String ingredientName = metadata.getName();
-            String unitLabel = metadata.getUnitLabel();
-            double qty = ingredient.getQuantity();
-            mealCompanionSession.getUi().printMessage(Integer.toString(i+1) + ". " + ingredientName + " "
-                    +Double.toString(qty) + " " + unitLabel);
-        }
-        mealCompanionSession.getUi().printMessage(""); //print newline for all OS
-        mealCompanionSession.getUi().printMessage("Instructions:");
-        InstructionList instructions = recipe.getInstructions();
-        for (int i = 0; i < instructions.size(); i++) {
-            Instruction instruction = instructions.get(i);
-            mealCompanionSession.getUi().printMessage(Integer.toString(i+1) + ". " + instruction.getInstruction());
+    public void execute(MealCompanionSession mealCompanionSession){
+        try {
+            RecipeList recipes = mealCompanionSession.getRecipes();
+            mealCompanionSession.getUi().printMessage("before getRandomIndex()");
+            int index = getRandomIndex(recipes);
+            mealCompanionSession.getUi().printMessage("after getRandomIndex()");
+            Recipe recipe = recipes.getRecipe(index);
+            mealCompanionSession.getUi().printMessage("index = " + index);
+            mealCompanionSession.getUi().printMessage("A random recipe is chosen:");
+            mealCompanionSession.getUi().printMessage("Recipe for " + recipe.getName());
+            mealCompanionSession.getUi().printMessage(""); //print newline for all OS
+            mealCompanionSession.getUi().printMessage("Calories: " + recipe.getCalorieCount());
+            mealCompanionSession.getUi().printMessage(""); //print newline for all OS
+            mealCompanionSession.getUi().printMessage("Ingredients:");
+            IngredientList ingredients = recipe.getIngredients();
+            for (int i = 0; i < ingredients.size(); i++) {
+                Ingredient ingredient = ingredients.get(i);
+                IngredientMetadata metadata = ingredient.getMetadata();
+                String ingredientName = metadata.getName();
+                String unitLabel = metadata.getUnitLabel();
+                double qty = ingredient.getQuantity();
+                mealCompanionSession.getUi().printMessage(Integer.toString(i+1) + ". " + ingredientName + " "
+                        +Double.toString(qty) + " " + unitLabel);
+            }
+            mealCompanionSession.getUi().printMessage(""); //print newline for all OS
+            mealCompanionSession.getUi().printMessage("Instructions:");
+            InstructionList instructions = recipe.getInstructions();
+            for (int i = 0; i < instructions.size(); i++) {
+                Instruction instruction = instructions.get(i);
+                mealCompanionSession.getUi().printMessage(Integer.toString(i+1) + ". " + instruction.getInstruction());
+            }
+        } catch (MealCompanionException e) {
+            mealCompanionSession.getUi().printMessage(String.valueOf(e));
         }
     }
 }
