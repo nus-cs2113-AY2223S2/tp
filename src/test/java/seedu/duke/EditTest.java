2ce44dccbf0e54369621b9450b5bda2f3de431ab
ArtemiszenN <artemiszenn@gmail.com>
2023-03-22 22:42:47 +0800

    Re-introduce edit and trie tests

--- src/test/java/seedu/duke/EditTest.java
+++ src/test/java/seedu/duke/EditTest.java
@@ -1,27 +1,29 @@
-//package seedu.duke;
-//
-//import org.junit.jupiter.api.Test;
-//import seedu.duke.exceptions.EditErrorException;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.fail;
-//
-//class EditTest {
-//
-//    Inventory inventory = new Inventory();
-//    Parser testParser = new Parser();
-//
-//    @Test
-//    public void sampleTest() {
-//        testParser.parseAdd("n/orange upc/123 qty/5 p/5");
-//        testParser.parseEdit("upc/123 n/apple");
-//        String[] data = {"upc/123"};
-//        Item updatedItem = null;
-//        try {
-//            updatedItem = Inventory.retrieveItemFromHashMap(data);
-//        } catch (EditErrorException eee) {
-//            fail();
-//        }
-//        assertEquals("apple", updatedItem.getName(), "Name not changed to apple.");
-//    }
-//}
+package seedu.duke;
+
+import org.junit.jupiter.api.Test;
+import seedu.duke.commands.SearchCommand;
+import seedu.duke.exceptions.EditErrorException;
+import seedu.duke.objects.Inventory;
+import seedu.duke.objects.Item;
+import seedu.duke.utils.parser.Parser;
+import seedu.duke.types.Types;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+class EditTest {
+
+    Inventory inventory = new Inventory();
+    Parser testParser = new Parser(inventory);
+
+    @Test
+    public void sampleTest() {
+        testParser.parseAdd("n/orange upc/123 qty/5 p/5",inventory);
+        testParser.parseEdit("upc/123 n/apple",inventory);
+        String[] data = {"upc/123"};
+        Item updatedItem = null;
+        SearchCommand searchCommand = new SearchCommand(inventory, "123", Types.SearchType.UPC);
+        updatedItem = searchCommand.searchUPC();
+        assertEquals("apple", updatedItem.getName(), "Name not changed to apple.");
+    }
+}
--- src/test/java/seedu/duke/TrieTest.java
+++ src/test/java/seedu/duke/TrieTest.java
@@ -1,35 +1,35 @@
-//package seedu.duke;
-//
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//
-//import org.junit.jupiter.api.Test;
-//import seedu.duke.trie.Trie;
-//
-//import java.util.ArrayList;
-//
-//public class TrieTest {
-//    @Test
-//    public void findTest(){
-//        Trie trie = new Trie();
-//        trie.add("Hamburger");
-//        trie.add("Hamburgers");
-//        trie.add("Marshmallows");
-//        assertTrue(trie.find("Hamburger")==true);
-//        assertTrue(trie.find("hamburger")==true);
-//        assertTrue(trie.find("hamb")==false);
-//    }
-//    @Test
-//    public void findPrefixTest(){
-//        Trie trie = new Trie();
-//        trie.add("Hamburger");
-//        trie.add("Hamburgers");
-//        trie.add("Marshmallows");
-//        ArrayList<String> ans = new ArrayList<>();
-//        ans.add("hamburger");
-//        ans.add("hamburgers");
-//        assertTrue(trie.prefixFind("Hamb").equals(ans));
-//        assertTrue(trie.prefixFind("Hamburger").equals(ans));
-//        ans.remove("hamburger");
-//        assertTrue(trie.prefixFind("Hamburgersss").equals(ans));
-//    }
-//}
+package seedu.duke;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import seedu.duke.utils.trie.Trie;
+
+import java.util.ArrayList;
+
+public class TrieTest {
+    @Test
+    public void findTest(){
+        Trie trie = new Trie();
+        trie.add("Hamburger");
+        trie.add("Hamburgers");
+        trie.add("Marshmallows");
+        assertTrue(trie.find("Hamburger")==true);
+        assertTrue(trie.find("hamburger")==true);
+        assertTrue(trie.find("hamb")==false);
+    }
+    @Test
+    public void findPrefixTest(){
+        Trie trie = new Trie();
+        trie.add("Hamburger");
+        trie.add("Hamburgers");
+        trie.add("Marshmallows");
+        ArrayList<String> ans = new ArrayList<>();
+        ans.add("hamburger");
+        ans.add("hamburgers");
+        assertTrue(trie.prefixFind("Hamb").equals(ans));
+        assertTrue(trie.prefixFind("Hamburger").equals(ans));
+        ans.remove("hamburger");
+        assertTrue(trie.prefixFind("Hamburgersss").equals(ans));
+    }
+}
