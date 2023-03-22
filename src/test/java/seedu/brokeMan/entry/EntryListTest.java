01c0ae328a9179f5f3045d409e6c55cc0a7ec024
sistine-yu <sistineyu715@gmail.com>
2023-03-22 15:30:44 +0800

    Add Junit tests for some methods in entryList class

---
+++ src/test/java/seedu/brokeMan/entry/EntryListTest.java
@@ -0,0 +1,50 @@
+package seedu.brokeMan.entry;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static seedu.brokeMan.entry.EntryList.*;
+import static seedu.brokeMan.parser.StringToTime.convertStringToTime;
+
+
+public class EntryListTest {
+
+    Entry entry1;
+    Entry entry2;
+    LinkedList<Entry> expenseList;
+
+    @BeforeEach
+    public void SetUp() throws Exception {
+        entry1 = new Expense(4, "lunch", convertStringToTime("2022 09 08 12 14"));
+        entry2 = new Expense(6, "dinner",  convertStringToTime("2023 12 12 20 01"));
+        expenseList = new LinkedList<Entry>(Arrays.asList(entry1, entry2));
+    }
+
+
+    @Test
+    public void deleteEntry_inBoundIndex_success() {
+            deleteEntry(2, expenseList);
+            assertEquals(expenseList, new LinkedList<>(Arrays.asList(entry1)));
+    }
+
+    @Test
+    public void deleteEntry_outBoundIndex_exceptionThrown() {
+        try {
+            deleteEntry(3, expenseList);
+        } catch (IndexOutOfBoundsException | IllegalArgumentException e) {
+            assertThrows(IndexOutOfBoundsException.class, () -> deleteEntry(3,expenseList));
+        }
+
+    }
+
+    @Test
+    public void getTotalAmount_inputList_success() {
+        assertEquals(10, getTotalAmount(expenseList));
+    }
+
+
+}
