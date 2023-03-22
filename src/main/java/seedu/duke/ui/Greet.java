db1deefe469fa7c6a8d92113450841e6418b6871
L-K-Chng <lipkuang02@gmail.com>
2023-03-22 22:57:30 +0800

    fixed checkstyle

--- src/main/java/seedu/duke/ui/Greet.java
+++ src/main/java/seedu/duke/ui/Greet.java
@@ -2,7 +2,8 @@ package seedu.duke.ui;
 
 public class Greet {
 
-    private static final String LOGO = "    _______ __                          ____        __      " + System.lineSeparator() +
+    private static final String LOGO = "    _______ __                          ____        __      " +
+            System.lineSeparator() +
             "   / ____(_) /_____  ___  __________   / __ \\__  __/ /_____ " + System.lineSeparator() +
             "  / /_  / / __/ __ \\/ _ \\/ ___/ ___/  / / / / / / / //_/ _ \\" + System.lineSeparator() +
             " / __/ / / /_/ / / /  __(__  |__  )  / /_/ / /_/ / ,< /  __/" + System.lineSeparator() +
