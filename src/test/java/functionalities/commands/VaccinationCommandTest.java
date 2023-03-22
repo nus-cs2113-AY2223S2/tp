8c6253a7a099f900c18610cad558ecb088ecbe4f
cleoncheng2000 <e0725796@u.nus.edu>
2023-03-22 10:19:08 +0800

    Add VaccinationCommandTest class

---
+++ src/test/java/functionalities/commands/VaccinationCommandTest.java
@@ -0,0 +1,23 @@
+package functionalities.commands;
+
+import exception.SniffException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VaccinationCommandTest {
+
+    @Test
+    void VaccinationCommandNotNull() {
+        String animalName = "";
+        String animalType = "";
+        String ownerName = "";
+        String contactNumber = "";
+        String vaccine = "";
+        String date = "";
+        String time = "";
+        VaccinationCommand c = new VaccinationCommand(animalType, animalName, ownerName,
+                contactNumber, vaccine, date, time);
+        assertNotNull(c);
+    }
+}
