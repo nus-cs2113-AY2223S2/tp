77957af81f1ad413a3f9a8beb265106060e07b72
GohJW <aarongjw00@gmail.com>
2023-03-22 13:22:48 +0800

    Update variable order

--- src/test/java/data/CurrencyTest.java
+++ src/test/java/data/CurrencyTest.java
@@ -7,13 +7,14 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
+
 import org.threeten.extra.Temporals;
 
 
 class CurrencyTest {
-    String inputDate = "06-02-2023";
+    public String inputDate = "06-02-2023";
+    public HashMap<String, BigDecimal> exchangeRate = new HashMap<>();
     public DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-    public HashMap<String , BigDecimal> exchangeRate = new HashMap<>();
 
 
     public LocalDate localDate = LocalDate.parse(inputDate, formatter).plusDays(1)
@@ -21,10 +22,10 @@ class CurrencyTest {
     //plus 1 day as previousWorkingDay method not inclusive of the day itself
 
     @Test
-    public  void sendHTTPRequestTest() {
+    public void sendHTTPRequestTest() {
         try {
             Currency.sendHTTPGetRequest(localDate.toString(), exchangeRate);
-        }catch (IOException e) {
+        } catch (IOException e) {
             assert false;
         }
 
