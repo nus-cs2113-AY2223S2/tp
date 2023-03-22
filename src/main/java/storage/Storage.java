64f65c5d920aa830b4b9e455ad66e77c0c33a935
Ju-Can <ju.can.0860@gmail.com>
2023-03-22 15:43:56 +0800

    Fix bug when data file does not exist

--- src/main/java/storage/Storage.java
+++ src/main/java/storage/Storage.java
@@ -1,13 +1,8 @@
 package storage;
 
-import data.Account;
 import data.ExpenseList;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
 
 // https://kodejava.org/how-do-i-store-objects-in-file/
 
@@ -16,22 +11,16 @@ public class Storage {
     private static final String READ_STORAGE_SUCCESSFUL = "All past expenses retrieved successfully!";
     private static final String READ_EXPENSELIST_ERROR = "Error reading expense list.";
     private static final String WRITE_TO_EXPENSELIST_ERROR = "Error writing to expense list.";
+    private static final String CREATE_FILE_ERROR = "Error creating file.";
+    private static final String INITIAL_WELCOME_MESSAGE = "";
 
     private ExpenseList expenseList;
-    private Account account;
 
 
     public Storage(ExpenseList expenseList) {
         this.expenseList = expenseList;
     }
 
-    public Storage(Account account) {
-        this.account = account;
-    }
-
-    public Account getAccount() {
-        return account;
-    }
 
     public ExpenseList getExpenseList() {
         return expenseList;
@@ -51,22 +40,41 @@ public class Storage {
         }
     }
 
+    public boolean createFile() {
+        try {
+            File f = new File("expenselist.dat");
+            if (f.createNewFile()) {
+                // first time that the programme is being run, update welcome message later on
+                System.out.println(INITIAL_WELCOME_MESSAGE);
+                return true;
+            }
+        } catch (IOException e) {
+            System.out.println(CREATE_FILE_ERROR);
+        }
+        return false;
+    }
+
     /**
      * Reads ExpenseList object from dat file and store as expenseList.
      */
 
     public ExpenseList initialiseExpenseList() {
         try {
-            FileInputStream fis = new FileInputStream("expenselist.dat");
-            ObjectInputStream ois = new ObjectInputStream(fis);
-            expenseList = (ExpenseList) ois.readObject();
-            System.out.println(READ_STORAGE_SUCCESSFUL);
-
+            boolean isFileCreated = createFile();
+            if (isFileCreated) {
+                expenseList = new ExpenseList();
+            } else {
+                FileInputStream fis = new FileInputStream("expenselist.dat");
+                ObjectInputStream ois = new ObjectInputStream(fis);
+                expenseList = (ExpenseList) ois.readObject();
+                System.out.println(READ_STORAGE_SUCCESSFUL);
+            }
         } catch (IOException | ClassNotFoundException e) {
             System.out.println(READ_EXPENSELIST_ERROR);
         }
         return expenseList;
     }
+
 
     public void clearContent() {
         try {
