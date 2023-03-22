e8c869aff95d94f9635accc2e83f4786f94d3467
Bui Phuong Nam <e0851451@u.nus.edu>
2023-03-22 23:07:55 +0700

    Fix on txt file

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -1,20 +1,14 @@
 #!/usr/bin/env bash
-
 # change to script directory
-# shellcheck disable=SC2164
 cd "${0%/*}"
-
-# shellcheck disable=SC2103
 cd ..
 ./gradlew clean shadowJar
-
 cd text-ui-test
 
-# shellcheck disable=SC2046
 java  -jar $(find ../build/libs/ -mindepth 1 -print -quit) < input.txt > ACTUAL.TXT
 
+
 diff EXPECTED-UNIX.TXT ACTUAL.TXT
-# shellcheck disable=SC2181
 if [ $? -eq 0 ]
 then
     echo "Test passed!"
