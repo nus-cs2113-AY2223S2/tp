4a7d1e23bedfa08f448eb5b3df16e0f52488df7d
dendendenden04 <dennisokq@gmail.com>
2023-03-24 01:49:58 +0800

    Reformat code to fit coding standards

--- src/main/java/exception/SniffException.java
+++ src/main/java/exception/SniffException.java
@@ -3,12 +3,11 @@ package exception;
 public class SniffException extends Exception {
     protected static String errorMessage;
 
-    public static String getErrorMessage() {
-        return errorMessage;
-    }
-
     public SniffException(String e) {
         errorMessage = e;
+    }
+    public static String getErrorMessage() {
+        return errorMessage;
     }
     public static void showError() {
         System.out.println(errorMessage);
