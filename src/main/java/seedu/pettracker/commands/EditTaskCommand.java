841e688807910a07f1a3f7e5330daf747fbdeb4b
Marek <marek.pinto@gmail.com>
2023-03-24 17:18:50 +0800

    add EditTaskCommand class

---
+++ src/main/java/seedu/pettracker/commands/EditTaskCommand.java
@@ -0,0 +1,52 @@
+package seedu.pettracker.commands;
+
+import seedu.pettracker.ui.Ui;
+import seedu.pettracker.data.PetList;
+
+public class EditTaskCommand extends Command {
+
+    protected int taskNumber;
+    protected String newDescription;
+
+
+
+    public EditTaskCommand(String commandArgs) {
+        super();
+        this.taskNumber = Integer.parseInt(parseArgs(commandArgs)[0]);
+        this.newDescription = parseArgs(commandArgs)[1];
+    }
+
+    // TODO: Implement this method
+
+    /**
+     * Executes the given command
+     *
+     * @param ui Ui to do printing if required
+     */
+    @Override
+    public void execute(Ui ui) {
+        PetList.editTask
+        ui.editStatCommandMessage(petName, statName, statValue);
+    }
+
+    /**
+     * Parses the arguments of the command
+     *
+     * @param commandArgs String containing the arguments of the command
+     * @return String array containing the arguments of the command
+     */
+    @Override
+    public String[] parseArgs(String commandArgs) {
+        return commandArgs.split(" ", 2);
+    }
+
+    /**
+     * Sets isExit to be true to exit the program
+     *
+     * @return isExit boolean value for program to exit
+     */
+    @Override
+    public boolean isExit() {
+        return false;
+    }
+}
