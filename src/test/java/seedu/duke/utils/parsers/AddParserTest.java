3f5e57552e3510fcf1778e0c0b35f6c931e2e59c
firwer <firwer@gmail.com>
2023-03-24 19:20:52 +0800

    test: Improve comprehensiveness of add function JUnit tests

---
+++ src/test/java/seedu/duke/utils/parsers/AddParserTest.java
@@ -0,0 +1,139 @@
+package seedu.duke.utils.parsers;
+
+import org.junit.jupiter.api.Test;
+import seedu.duke.exceptions.MissingParametersException;
+import seedu.duke.objects.Inventory;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Conducts the following test cases:
+ * 1. Add item with empty parameters
+ * 2. Add item with missing parameters
+ * 3. Add item with all parameters
+ */
+class AddParserTest {
+
+    @Test
+    void addItemWithAllParameters() {
+        String commandInfo = "n/Test Item 1 upc/12345678910 qty/56 p/2.1";
+        Inventory inventory = new Inventory();
+        AddParser addParser = new AddParser(commandInfo, inventory);
+        addParser.run();
+        assertEquals(1, inventory.getItemInventory().size());
+    }
+
+    @Test
+    void addItemWithOneMissingParameter() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        String commandInfo = "n/Test Item 1 qty/56 p/2.1";
+        Inventory inventory = new Inventory();
+        AddParser addParser = new AddParser(commandInfo, inventory);
+        addParser.run();
+        assertEquals(0, inventory.getItemInventory().size());
+        String expectedOutput = "Tip: Ensure that your UPC, quantity and price are all in numbers and within " +
+                "valid range";
+        assertTrue(outContent.toString().contains(expectedOutput));
+    }
+
+    @Test
+    void addItemWithMultipleMissingParameters() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        String commandInfo = "n/Test Item 1 qty/56";
+        Inventory inventory = new Inventory();
+        AddParser addParser = new AddParser(commandInfo, inventory);
+        addParser.run();
+        assertEquals(0, inventory.getItemInventory().size());
+        String expectedOutput = "Tip: Ensure that your UPC, quantity and price are all in numbers and within " +
+                "valid range";
+        assertTrue(outContent.toString().contains(expectedOutput));
+    }
+
+    @Test
+    void addItemWithEmptyParameter() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        String commandInfo = "";
+        Inventory inventory = new Inventory();
+        AddParser addParser = new AddParser(commandInfo, inventory);
+        addParser.run();
+        assertEquals(0, inventory.getItemInventory().size());
+        String expectedOutput = "Tip: Ensure that your UPC, quantity and price are all in numbers and within " +
+                "valid range";
+        assertTrue(outContent.toString().contains(expectedOutput));
+    }
+
+    @Test
+    void addItemWithOutOfRangeQty() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        String commandInfo = "n/Test Item 1 upc/12345678910 qty/56000000000000 p/2.1";
+        Inventory inventory = new Inventory();
+        AddParser addParser = new AddParser(commandInfo, inventory);
+        addParser.run();
+        assertEquals(0, inventory.getItemInventory().size());
+        String expectedOutput = "Tip: Ensure that your UPC, quantity and price are all in numbers and within " +
+                "valid range";
+        assertTrue(outContent.toString().contains(expectedOutput));
+    }
+
+    @Test
+    void addItemWithEmptyValues() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        String commandInfo = "n/Test Item 1 upc/ qty/ p/";
+        Inventory inventory = new Inventory();
+        AddParser addParser = new AddParser(commandInfo, inventory);
+        addParser.run();
+        assertEquals(0, inventory.getItemInventory().size());
+        String expectedOutput = "Tip: Ensure that your UPC, quantity and price are all in numbers and within " +
+                "valid range";
+        assertTrue(outContent.toString().contains(expectedOutput));
+    }
+
+    @Test
+    void addItemWithWrongUPCType() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        String commandInfo = "n/Test Item 1 upc/abcdefg qty/1 p/2.5";
+        Inventory inventory = new Inventory();
+        AddParser addParser = new AddParser(commandInfo, inventory);
+        addParser.run();
+        assertEquals(0, inventory.getItemInventory().size());
+        String expectedOutput = "Tip: Ensure that your UPC, quantity and price are all in numbers and within " +
+                "valid range";
+        assertTrue(outContent.toString().contains(expectedOutput));
+    }
+    @Test
+    void addItemWithWrongQtyType() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        String commandInfo = "n/Test Item 1 upc/12345678910 qty/abc p/2.5";
+        Inventory inventory = new Inventory();
+        AddParser addParser = new AddParser(commandInfo, inventory);
+        addParser.run();
+        assertEquals(0, inventory.getItemInventory().size());
+        String expectedOutput = "Tip: Ensure that your UPC, quantity and price are all in numbers and within " +
+                "valid range";
+        assertTrue(outContent.toString().contains(expectedOutput));
+    }
+
+    @Test
+    void addItemWithWrongPriceType() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        String commandInfo = "n/Test Item 1 upc/12345678910 qty/2 p/ttthhhasasad";
+        Inventory inventory = new Inventory();
+        AddParser addParser = new AddParser(commandInfo, inventory);
+        addParser.run();
+        assertEquals(0, inventory.getItemInventory().size());
+        String expectedOutput = "Tip: Ensure that your UPC, quantity and price are all in numbers and within " +
+                "valid range";
+        assertTrue(outContent.toString().contains(expectedOutput));
+    }
+}
