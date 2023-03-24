56c7d7140020fa15e051320efb539405b38b590e
khooyourun <khooyourun@hotmail.com>
2023-03-24 23:20:46 +0800

    Fixed JUnit Test for Parser

--- src/test/java/seedu/badMaths/ParserTest.java
+++ src/test/java/seedu/badMaths/ParserTest.java
@@ -9,24 +9,16 @@ class ParserTest {
     @Test
     void getCommandShouldReturnCommandWithoutDot() {
         String userInput = "Store. store this";
-        String output;
-        if (userInput.contains(".")) {
-            output =  userInput.substring(0, userInput.indexOf("."));
-        } else {
-            output = "Invalid command";
-        }
-        assertEquals("Store", output);
+        Parser parserTest = new Parser(userInput);
+        String commandTest = parserTest.getCommand();
+        assertEquals(commandTest, "Store");
     }
 
     @Test
     void getToDoShouldReturnInvalidForSingleWordInput() {
         String userInput = "Help.";
-        String output;
-        if (userInput.contains(".") && userInput.contains(" ")) {
-            output = userInput.substring(userInput.indexOf(".") + 2);
-        } else {
-            output = "Invalid todo";
-        }
-        assertEquals("Invalid todo", output);
+        Parser parserTest = new Parser(userInput);
+        String toDoTest = parserTest.getToDo();
+        assertEquals(toDoTest, "Invalid todo");
     }
 }
