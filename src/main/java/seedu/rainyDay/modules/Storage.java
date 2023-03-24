fcf72feaf972d0972f29d24a92b94b306ca35007
KN-CY <kaemoncyng@gmail.com>
2023-03-24 23:34:13 +0800

    Add save date to .csv

--- src/main/java/seedu/rainyDay/modules/Storage.java
+++ src/main/java/seedu/rainyDay/modules/Storage.java
@@ -12,6 +12,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.time.LocalDate;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.LogManager;
@@ -90,8 +91,9 @@ public class Storage {
                 value = Double.toString(-currStatement.getValue());
             }
             String category = currStatement.getCategory();
+            String date = currStatement.getDate().toString();
 
-            String[] tableRow = {statementID, description, value, category};
+            String[] tableRow = {statementID, description, value, category, date};
             tableWriter.writeNext(tableRow);
         }
     }
@@ -110,7 +112,7 @@ public class Storage {
         FileWriter outputFile = new FileWriter(CSVFilePath);
         CSVWriter tableWriter = new CSVWriter(outputFile);
 
-        String[] tableHeader = {"ID", "Description", "Value", "Category"};
+        String[] tableHeader = {"ID", "Description", "Amount", "Category", "Date"};
         tableWriter.writeNext(tableHeader);
         fillTableBody(report, tableWriter);
 
