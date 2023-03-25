c7c95a61b891fef7b63ff5ae8217582502bd32dd
Shawn Tang <shawntang83@gmail.com>
2023-03-25 14:39:46 +0800

    Add Storage file to save pets

--- src/main/java/seedu/pettracker/storage/Storage.java
+++ src/main/java/seedu/pettracker/storage/Storage.java
@@ -1,2 +1,55 @@
-package seedu.pettracker.storage;public class Storage {
+package seedu.pettracker.storage;
+
+
+import seedu.pettracker.data.Pet;
+import seedu.pettracker.ui.Ui;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Storage {
+    private final String filePath;
+
+    public Storage(String filePath, Ui ui) {
+        this.filePath = filePath;
+        try {
+            createFile();
+        } catch (IOException e) {
+            ui.fileIOErrorMessage();
+        }
+    }
+
+    private void createFile() throws IOException {
+        File file = new File(filePath);
+        if (file.exists()) {
+            return;
+        }
+        if (!file.getParentFile().exists()) {
+            file.getParentFile().mkdirs();
+        }
+        file.createNewFile();
+    }
+
+    private void writePetsToFile(ArrayList<Pet> petList) throws IOException {
+        FileWriter fw = new FileWriter(filePath);
+        for (Pet pet : petList) {
+            if (pet != null) {
+                fw.write(pet.saveFormat() + System.lineSeparator());
+            }
+        }
+
+        fw.close();
+    }
+
+    public void savePets(ArrayList<Pet> petList, Ui ui) {
+        try {
+            writePetsToFile(petList);
+        } catch (IOException e) {
+            ui.fileIOErrorMessage();
+        }
+    }
+
+
 }
