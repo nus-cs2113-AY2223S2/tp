f16a196d9ca92172920fb7364729e1fe4c69df4b
kishore-a00 <skippyepic@gmail.com>
2023-03-25 21:51:12 +0800

    Add function to create company in companyList using parsed company information

--- src/main/java/seedu/duke/storage/CompanyListDecoder.java
+++ src/main/java/seedu/duke/storage/CompanyListDecoder.java
@@ -12,9 +12,23 @@ import java.util.Scanner;
 public class CompanyListDecoder extends Storage {
 
     public static final String filePath = "data/companyList.txt";
-    
+
     private static String[] parseInput(String line) {
         String[] parsedline = line.split("\\|");
         return parsedline;
     }
+
+    private static void writeToCompanyList(CompanyList companyList, String[] parsedInput) throws InvalidIndexException {
+        String companyName = parsedInput[0];
+        int contactNumber = Integer.parseInt(parsedInput[1]);
+        String contactEmail = parsedInput[2];
+        int status = Integer.parseInt(parsedInput[3]);
+        String industry = parsedInput[4];
+        companyList.add(companyName, industry, contactNumber, contactEmail);
+        int currCompany = companyList.getNumberOfCompanies() - 1;
+        // If status was previously indicated as confirmed, mark the status of the company as confirmed in companyList
+        if (status == 1) {
+            Company company = companyList.getCompany(currCompany);
+            company.markConfirmed();
+        }
 }
