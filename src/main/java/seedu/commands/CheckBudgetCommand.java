8967b5914d379fb478492df3549501451ea87863
itszhixuan <leezhixuan001@gmail.com>
2023-03-25 16:00:14 +0800

    Considered how borrow reduces thier total expenditure

--- src/main/java/seedu/commands/CheckBudgetCommand.java
+++ src/main/java/seedu/commands/CheckBudgetCommand.java
@@ -13,27 +13,34 @@ public class CheckBudgetCommand extends Command {
     public CommandResult execute(ExpenditureList expenditures) {
         double budget = expenditures.getBudgetSet();
         double totalAmount = 0;
+        double borrowedAmount = 0;
         for (Expenditure individualExpenditure : expenditures.getExpenditures()){
-            totalAmount += individualExpenditure.getValue();
+            if (individualExpenditure.getExpenditureType().equals("B")) {
+                totalAmount -= individualExpenditure.getValue();
+                borrowedAmount += individualExpenditure.getValue();
+            } else {
+                totalAmount += individualExpenditure.getValue();
+            }
         }
-        return getCommandResult(budget, totalAmount);
+        return getCommandResult(budget, totalAmount, borrowedAmount);
     }
 
-    private static CommandResult getCommandResult(double budget, double totalAmount) {
+    private static CommandResult getCommandResult(double budget, double totalAmount, double borrowedAmount) {
         if (budget == 0) {
             return new CommandResult("Your current budget is set at 0, please use the 'set' command to set a budget.");
         } else if (budget >= totalAmount) {
             // Remaining budget available
             double difference = budget - totalAmount;
-            return new CommandResult(String.format("You are $%.2f away from hitting your budget of $%.2f." +
-                    " Your current spending stands at $%.2f",
-                    difference, budget, totalAmount
+            return new CommandResult(String.format("You are $%.2f away from exceeding your budget of $%.2f." +
+                    " Your current spending stands at $%.2f with $%.2f of borrowed money.",
+                    difference, budget, totalAmount, borrowedAmount
             ));
         } else {
             double difference = totalAmount - budget;
             return new CommandResult(String.format(
-                    "You have exceeded your budget of $%.2f by $%.2f! Your current spending stands at $%.2f",
-                    budget, difference, totalAmount
+                    "You have exceeded your budget of $%.2f by $%.2f! " +
+                    "Your current spending stands at $%.2f with $%.2f of borrowed money.",
+                    budget, difference, totalAmount, borrowedAmount
             ));
         }
     }
