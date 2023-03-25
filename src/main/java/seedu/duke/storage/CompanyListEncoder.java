3eafb89a3773281f9cf2095c88b8431c88374c63
kishore-a00 <skippyepic@gmail.com>
2023-03-25 19:07:00 +0800

    Create CompanyListEncoder class to store company information to disk

---
+++ src/main/java/seedu/duke/storage/CompanyListEncoder.java
@@ -0,0 +1,13 @@
+package seedu.duke.storage;
+
+import seedu.duke.company.Company;
+import seedu.duke.company.CompanyList;
+import seedu.duke.exception.InvalidIndexException;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CompanyListEncoder extends Storage {
+
+
+}
