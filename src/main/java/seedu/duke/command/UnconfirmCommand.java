437dec16412dbfde56a0696587b823ff3146e5a0
kishore-a00 <skippyepic@gmail.com>
2023-03-25 23:28:23 +0800

    Add save to txt file function after company has been marked as unconfirmed

--- src/main/java/seedu/duke/command/UnconfirmCommand.java
+++ src/main/java/seedu/duke/command/UnconfirmCommand.java
@@ -3,6 +3,9 @@ package seedu.duke.command;
 import seedu.duke.company.Company;
 import seedu.duke.company.CompanyList;
 import seedu.duke.exception.InvalidIndexException;
+import seedu.duke.storage.CompanyListEncoder;
+
+import java.io.IOException;
 
 public class UnconfirmCommand extends Command {
     protected int companyNum;
@@ -12,12 +15,12 @@ public class UnconfirmCommand extends Command {
         this.companyNum = companyNum;
     }
     @Override
-    public void execute(CompanyList company){
+    public void execute(CompanyList companyList){
         try {
-            Company companyToConfirm = company.getCompany(companyNum);
+            Company companyToConfirm = companyList.getCompany(companyNum);
             companyToConfirm.markUnconfirmed();
-
-        } catch (InvalidIndexException err){
+            CompanyListEncoder.write(companyList);
+        } catch (InvalidIndexException | IOException e) {
             System.out.println("Invalid index provided! Please try again");
         }
     }
