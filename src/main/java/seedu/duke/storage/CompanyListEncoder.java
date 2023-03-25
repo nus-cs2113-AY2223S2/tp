651af7f0401d386ce1ded7e1e8e7d81a3d0a5aa2
kishore-a00 <skippyepic@gmail.com>
2023-03-25 19:14:09 +0800

    Add function to iterate through companyList and store all company information in specified txt file

--- src/main/java/seedu/duke/storage/CompanyListEncoder.java
+++ src/main/java/seedu/duke/storage/CompanyListEncoder.java
@@ -9,6 +9,28 @@ import java.io.IOException;
 
 public class CompanyListEncoder extends Storage {
 
+    public static final String filePath = "data/companyList.txt";
+
+    public static void write(CompanyList companyList) throws IOException, InvalidIndexException {
+        try {
+            FileWriter fw = new FileWriter(filePath);
+            int numberOfCompanies = companyList.getNumberOfCompanies();
+            // If the last company in the array list is deleted clear the entire text file
+            // Since the for loop only iterates if there is more than one company in companyList
+            if (numberOfCompanies == 0) {
+                fw.write("");
+                fw.close();
+            }
+            // Iterate through company in companyList and parse company information for storage
+            for (int i = 0; i < numberOfCompanies; i += 1) {
+                Company company = companyList.getCompany(i);
+                parseAndWriteToFile(company, fw);
+            }
+            fw.close();
+        } catch (IOException | InvalidIndexException e) {
+            System.out.println("File not found");
+        }
+    }
 
     private static void parseAndWriteToFile(Company company, FileWriter fw) throws IOException {
         String companyName = company.getCompanyName();
@@ -19,7 +41,7 @@ public class CompanyListEncoder extends Storage {
         fw.write(companyName + "|" + contactNumber + "|" + contactEmail + "|" + confirmStatus + "|" +
                 industry + System.lineSeparator());
     }
-    
+
     private static int convertConfirmStatusToBinary(String confirmStatus) {
         if (confirmStatus.equals("Confirmed")) {
             return 1;
