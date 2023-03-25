a35756bd4de424e5c27602200a6c5d78632d897c
ysl-28 <88529907+ysl-28@users.noreply.github.com>
2023-03-25 15:28:03 +0800

    Add test for adding alert

---
+++ src/test/java/seedu/duke/commands/AddAlertCommandTest.java
@@ -0,0 +1,25 @@
+package seedu.duke.commands;
+
+import org.junit.jupiter.api.Test;
+import seedu.duke.objects.Inventory;
+import seedu.duke.objects.Item;
+import seedu.duke.objects.Alert;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+public class AddAlertCommandTest {
+
+    Inventory inventory;
+    @Test
+    void addMinAlert() {
+        inventory = new Inventory();
+        Item newItem = new Item("apples", "1234", 6, 15.0);
+        Command command = new AddCommand(inventory, newItem);
+        command.run();
+
+        String inventoryUpc = inventory.getItemInventory().get(0).getUpc();
+        Alert alert = new Alert(inventoryUpc, "min", "2");
+        Command addAlertCommand = new AddAlertCommand(inventory, alert);
+        addAlertCommand.run();
+
+        assertTrue(inventory.getAlertList().getMinAlertUpcs().containsKey(inventoryUpc));
+    }
+}
