0885485d053c7295fef2eeb128ac79f3ba01fc66
kishore-a00 <skippyepic@gmail.com>
2023-03-25 19:10:08 +0800

    Create function to parse company information for storage to text file

--- src/main/java/seedu/duke/storage/CompanyListEncoder.java
+++ src/main/java/seedu/duke/storage/CompanyListEncoder.java
@@ -9,6 +9,17 @@ import java.io.IOException;
 
 public class CompanyListEncoder extends Storage {
 
+
+    private static void parseAndWriteToFile(Company company, FileWriter fw) throws IOException {
+        String companyName = company.getCompanyName();
+        int contactNumber = company.getContactNumber();
+        String contactEmail = company.getContactEmail();
+        int confirmStatus = convertConfirmStatusToBinary(company.getConfirmStatus());
+        String industry = company.getIndustry();
+        fw.write(companyName + "|" + contactNumber + "|" + contactEmail + "|" + confirmStatus + "|" +
+                industry + System.lineSeparator());
+    }
+    
     private static int convertConfirmStatusToBinary(String confirmStatus) {
         if (confirmStatus.equals("Confirmed")) {
             return 1;
