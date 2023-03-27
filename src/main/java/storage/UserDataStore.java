30e29c632c16bf045350e4c4c808875a474e7947
Bui Phuong Nam <e0851451@u.nus.edu>
2023-03-28 00:46:55 +0700

    Update checkstyle #3

--- src/main/java/storage/UserDataStore.java
+++ src/main/java/storage/UserDataStore.java
@@ -2,40 +2,59 @@ package storage;
 import java.util.HashMap;
 import java.util.Map;
 
-public class UserDataStore { //my attempt to user map, don't need to use for now
-    // This class is a singleton. Call getInstance() instead.
-    private UserDataStore(){}
+/** Class for storing user data in memory using a map. */
+public final class UserDataStore {
 
-    private static UserDataStore instance = new UserDataStore();
+    /** Map of usernames to their passwords. */
+    private final Map<String, String> userMap;
 
-    /**
-     * Map of usernames to their passwords.
-     */
-    private Map<String, String> userMap = new HashMap<>();
+    /** Singleton instance of UserDataStore. */
+    private static final UserDataStore INSTANCE = new UserDataStore();
 
-    public static UserDataStore getInstance(){
-        return instance;
+    /** Private constructor to prevent instantiation from outside the class. */
+    private UserDataStore() {
+        userMap = new HashMap<>();
     }
 
-    public boolean isUsernameTaken(String username){
+    /** Returns the singleton instance of UserDataStore. */
+    public static UserDataStore getInstance() {
+        return INSTANCE;
+    }
+
+    /**
+     * Checks if a username is already taken.
+     *
+     * @param username the username to check
+     * @return true if the username is taken, false otherwise
+     */
+    public boolean isUsernameTaken(String username) {
         return userMap.containsKey(username);
     }
 
-    public void registerUser(String username, String password){
+    /**
+     * Registers a new user with the given username and password.
+     *
+     * @param username the username of the new user
+     * @param password the password of the new user
+     */
+    public void registerUser(String username, String password) {
         userMap.put(username, password);
     }
 
+    /**
+     * Checks if a login attempt with the given username and password is correct.
+     *
+     * @param username the username of the user attempting to log in
+     * @param password the password of the user attempting to log in
+     * @return true if the login attempt is correct, false otherwise
+     */
     public boolean isLoginCorrect(String username, String password) {
-
-        // username isn't registered
-        if(!userMap.containsKey(username)){
+        // Username isn't registered
+        if (!userMap.containsKey(username)) {
             return false;
         }
 
         String storedPassword = userMap.get(username);
-
         return password.equals(storedPassword);
     }
-
-
 }
