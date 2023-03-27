49430acae19712608964f057cae6ffb88efd4fea
ZiqiuZeng <88576865+ZiqiuZeng@users.noreply.github.com>
2023-03-27 21:24:43 +0800

    make note more OOP

--- src/main/java/seedu/badMaths/BadMaths.java
+++ src/main/java/seedu/badMaths/BadMaths.java
@@ -2,14 +2,23 @@ package seedu.badMaths;
 
 import seedu.badMaths.ui.Ui;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
+import java.io.File;
+import java.io.IOException;
 
 public class BadMaths {
+    private static final String path = "data/notes.txt";
 
-    public static void commandChecker(String userInput, String command) {
+    private static final Set<String> VALID_COMMANDS = new HashSet<>(Arrays.asList(
+            "Graph", "Bye", "List", "Store", "Matrix", "Help", "FindInfo", "FindPrior", "FindMark", "FindUnmark", "Low",
+            "Medium", "High", "Delete", "Mark", "Unmark", "Clear", "Rank"
+    ));
+    public static void commandChecker(String command) {
         try {
-            if (!(command.equals("Graph") || command.equals("Bye") || command.equals("Delete") || command.equals("List")
-                    || command.equals("Store") || command.equals("Matrix") || command.equals("Help"))) {
+            if (!VALID_COMMANDS.contains(command)) {
                 throw new IllegalArgumentException();
             }
         } catch (IllegalArgumentException e) {
@@ -17,20 +26,35 @@ public class BadMaths {
         }
     }
 
+    public static void notesCreator(String path) {
+        File notesFile = new File(path);
+        if (!notesFile.exists()) {
+            if (!notesFile.getParentFile().exists()) {
+                notesFile.getParentFile().mkdirs();
+            }
+            try {
+                notesFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("This is BadMaths. You can type 'Help.' to learn what I can do for you :)");
         Command inputCommand = null;
 
+        notesCreator(path);
+        NotesList notes = new NotesList(Storage.loadFile(path));
+
         while (true) {
             Scanner scanner = new Scanner(System.in);
-            String userInput = scanner.nextLine(); // "List. 1"
-            Parser parser = new Parser(userInput);
-            String command = parser.getCommand(); // List
-            String toDo = parser.getToDo(); // 1
-            commandChecker(userInput, command);
+            String userInput = scanner.nextLine();
 
-            // command == List
-            // toDo == 1
+            Parser parser = new Parser(userInput);
+            String command = parser.getCommand();
+            String toDo = parser.getToDo();
+            commandChecker(command);
 
             if (inputCommand == null) {
                 inputCommand = new Command(command, toDo);
@@ -40,7 +64,7 @@ public class BadMaths {
                 inputCommand.setToDo(toDo);
             }
 
-            inputCommand.executeCommand();
+            inputCommand.executeCommand(notes);
             if (userInput.equals("Bye.")) {
                 break;
             }
