74d2f26c8aabd38894881d061485aeea0c7b8bc9
Er Jun Ze <junzeer@u.nus.edu>
2023-03-27 22:59:50 +0800

    Fix checkstyle issues

--- src/test/java/seedu/todolist/logic/ParserUtilTest.java
+++ src/test/java/seedu/todolist/logic/ParserUtilTest.java
@@ -19,15 +19,15 @@ class ParserUtilTest {
     @Test
     void parseIdTest() {
         // Invalid ids should throw an exception
-        final String[] INVALID_IDS = {"2.5", "", "abc"};
-        for (String invalidId : INVALID_IDS) {
+        final String[] invalid_ids = {"2.5", "", "abc"};
+        for (String invalidId : invalid_ids) {
             assertThrows(InvalidIdException.class, () -> ParserUtil.parseId(invalidId));
         }
 
         // Valid ids get parsed successfully
-        final int[] VALID_IDS = {3, 17, 84725};
+        final int[] valid_ids = {3, 17, 84725};
         try {
-            for (int validId : VALID_IDS) {
+            for (int validId : valid_ids) {
                 assertEquals(ParserUtil.parseId(Integer.toString(validId)), validId);
             }
         } catch (InvalidIdException e) {
@@ -38,17 +38,17 @@ class ParserUtilTest {
     @Test
     void parseDeadlineTest() {
         // Invalid dates should throw an exception
-        final String[] INVALID_DEADLINES = {"30/02/2023 16:40", "lalala", "10-50-2032 22:06", ""};
-        for (String invalidDeadline : INVALID_DEADLINES) {
+        final String[] invalid_deadlines = {"30/02/2023 16:40", "lalala", "10-50-2032 22:06", ""};
+        for (String invalidDeadline : invalid_deadlines) {
             assertThrows(InvalidDateException.class, () -> ParserUtil.parseDeadline(invalidDeadline));
         }
 
         // Valid dates get parsed successfully
         final LocalDateTime now = LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES);
-        final LocalDateTime[] VALID_DEADLINES = {now, now.plusDays(1), now.minusWeeks(1), now.plusYears(10)};
+        final LocalDateTime[] valid_deadlines = {now, now.plusDays(1), now.minusWeeks(1), now.plusYears(10)};
         DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern(Formats.TIME_IN_1.getFormat());
         try {
-            for (LocalDateTime validDeadline : VALID_DEADLINES) {
+            for (LocalDateTime validDeadline : valid_deadlines) {
                 assertEquals(ParserUtil.parseDeadline(validDeadline.format(inputFormatter)), validDeadline);
             }
         } catch (InvalidDateException e) {
@@ -59,15 +59,15 @@ class ParserUtilTest {
     @Test
     void parseEmailTest() {
         // Invalid emails should throw an exception
-        final String[] INVALID_EMAILS = {"a@b@c@d.com", "example.com", "hello", "", "where@how"};
-        for (String invalidEmail : INVALID_EMAILS) {
+        final String[] invalid_emails = {"a@b@c@d.com", "example.com", "hello", "", "where@how"};
+        for (String invalidEmail : invalid_emails) {
             assertThrows(InvalidEmailFormatException.class, () -> ParserUtil.parseEmail(invalidEmail));
         }
 
         // Valid emails get parsed successfully
-        final String[] VALID_EMAILS = {"xyz@xyz.com", "Idk@lalala.com", "legit@gmail.com"};
+        final String[] valid_emails = {"xyz@xyz.com", "Idk@lalala.com", "legit@gmail.com"};
         try {
-            for (String validEmail : VALID_EMAILS) {
+            for (String validEmail : valid_emails) {
                 assertEquals(ParserUtil.parseEmail(validEmail), validEmail);
             }
         } catch (InvalidEmailFormatException e) {
@@ -78,8 +78,8 @@ class ParserUtilTest {
     @Test
     void parseRepeatDurationTest() {
         // Invalid repeat durations should throw an exception
-        final String[] INVALID_DURATIONS = {"$^8g", "1.36", "vb 2s"};
-        for (String invalidDuration : INVALID_DURATIONS) {
+        final String[] invalid_durations = {"$^8g", "1.36", "vb 2s"};
+        for (String invalidDuration : invalid_durations) {
             assertThrows(InvalidDurationException.class,
                     () -> ParserUtil.parseRepeatDuration(invalidDuration, LocalDateTime.now()));
         }
@@ -92,9 +92,9 @@ class ParserUtilTest {
         }
 
         // Valid repeat durations get parsed successfully
-        final int[] VALID_DURATIONS = {3, 17, 84725};
+        final int[] valid_durations = {3, 17, 84725};
         try {
-            for (int validDuration : VALID_DURATIONS) {
+            for (int validDuration : valid_durations) {
                 assertEquals(ParserUtil.parseRepeatDuration(Integer.toString(validDuration),
                         LocalDateTime.now()), validDuration);
             }
@@ -103,7 +103,7 @@ class ParserUtilTest {
         }
 
         // Missing deadline should throw an exception
-        for (int validDuration : VALID_DURATIONS) {
+        for (int validDuration : valid_durations) {
             assertThrows(InvalidDateException.class,
                     () -> ParserUtil.parseRepeatDuration(Integer.toString(validDuration), null));
         }
