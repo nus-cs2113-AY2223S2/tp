53f0f44130c46f5bd1de8ae2655c73322460b4b1
ychui <huuyucong2000@gmail.com>
2023-03-28 21:35:19 +0800

    Add JUnit tests for TrigoGraphAnalyser and TrigoGraphVisualiserStub

--- src/main/java/seedu/badMaths/trigograph/TrigoGraphVisualiserStub.java
+++ src/main/java/seedu/badMaths/trigograph/TrigoGraphVisualiserStub.java
@@ -2,9 +2,12 @@ package seedu.badMaths.trigograph;
 
 import seedu.badMaths.ui.Ui;
 
-import javax.swing.*;
-import java.awt.*;
-
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.Toolkit;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 public class TrigoGraphVisualiserStub {
     private String trig;
 
