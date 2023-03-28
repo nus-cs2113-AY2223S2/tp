a3610e3d5799e10e24ba0c68fbe75a45a8352b3c
11-Navya <e0913130@u.nus.edu>
2023-03-28 13:35:22 +0800

    made changes ~/n

--- src/main/java/functionalities/appointments/Appointment.java
+++ src/main/java/functionalities/appointments/Appointment.java
@@ -8,13 +8,12 @@ public abstract class Appointment {
     public String uid;
     public Animal animal;
     protected Owner owner;
-    public boolean isDone;
+    public boolean isDone = false;
 
-    public Appointment(String uid, Animal animal, Owner owner, boolean isDone) {
+    public Appointment(String uid, Animal animal, Owner owner) {
         this.uid = uid;
         this.animal = animal;
         this.owner = owner;
-        this.isDone = false;
     }
 
     public abstract String toString();
