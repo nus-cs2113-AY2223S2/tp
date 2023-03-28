4b028f199d92fe6586d2def1f423b3e0b3104d20
Mnsd05 <thangngdc03bn@gmail.com>
2023-03-28 17:49:06 +0800

    Add Junit tests for help and bye commands

--- src/test/java/seedu/moneymind/CommandTest.java
+++ src/test/java/seedu/moneymind/CommandTest.java
@@ -426,7 +426,7 @@ public class CommandTest {
     }
 
     @Test
-    void viewAll_expectEverythingToBePrintedOut() {
+    void viewCommand_viewAll_expectEverythingToBePrintedOut() {
         setup();
         String terminalOutput = executeInput("view").toString();
         String expected = "1.food (budget: 0)" + System.lineSeparator()
@@ -440,12 +440,51 @@ public class CommandTest {
     }
 
     @Test
-    void viewCategory_expectCategoryToBePrintedOut() {
+    void viewCommand_viewOneCategory_expectCategoryToBePrintedOut() {
         setup();
         String terminalOutput = executeInput("view food").toString();
         String expected = "1. salad [expense]100" + System.lineSeparator()
                 + "2. pizza [expense]200" + System.lineSeparator();
         assertEquals(expected, terminalOutput);
+        clear();
+    }
+
+    @Test
+    void helpCommand_typeHelp_expectHelpMessage() {
+        setup();
+        String terminalOutput = executeInput("help").toString();
+        String expected = "Here are the commands you can use:" + System.lineSeparator()
+                + "1. help - show instructions on how to use the app\n" +
+                "Format: help\n" + "Example: help\n" + System.lineSeparator()
+                + "2. category - add a category to your list\n" +
+                "Format: category NAME [(optional) b/<budget number>]\n" +
+                "Example: category food b/2000\n" + System.lineSeparator()
+                + "3. event - add an event to a category\n" +
+                "Format: event NAME e/<expense number> [(optional) t/<time>]Example: event lunch e/10 t/August\n"
+                + System.lineSeparator()
+                + "4. view - view all the events in a category or all the categories\n" +
+                "You can view all the categories by entering view without any category name\n" +
+                "Format: view <category name>\n" +
+                "Example: view food\n" + System.lineSeparator()
+                + "5. edit - edit the expense for an event\n" +
+                "edit c/<category name> e/<event index>\n" +
+                "Example: edit c/food e/1\n" + System.lineSeparator()
+                + "6. delete - delete an event or a category\n" +
+                "delete c/<category name> [(optional) e/<event index>]\n" +
+                "Example: delete c/food e/1\n" +
+                "Example: delete c/food\n" + System.lineSeparator() +
+                "7. bye - exit the app\n" + "Format: bye\n" + "Example: bye\n" + System.lineSeparator();
+        assertEquals(expected, terminalOutput);
+        clear();
+    }
+
+    @Test
+    void byeCommand_typeBye_expectByeMessage() {
+        setup();
+        String terminalOutput = executeInput("bye").toString();
+        assertEquals("Bye. Hope to see you again soon!" + System.lineSeparator()
+                + "____________________________________________________________"
+                + System.lineSeparator(), terminalOutput);
         clear();
     }
 
