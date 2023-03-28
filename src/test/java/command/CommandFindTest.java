bbdd78bbcda78b2043664fe238c160c8b6a972e1
FanZixian <u3577161@connect.hku.hk>
2023-03-28 12:11:45 +0800

    Update the test cases

--- src/test/java/command/CommandFindTest.java
+++ src/test/java/command/CommandFindTest.java
@@ -50,15 +50,71 @@ public class CommandFindTest {
         input = "fish";
         outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
-        expected = "Sorry, none of your previous expenses corresponds to this category.\n"
+        expected = "Sorry, none of your previous expenses have such input.\n"
                 + MESSAGE_DIVIDER + "\n";
-        new CommandCategory(expenseList.getExpenseList(), input).execute();
+        new CommandFind(expenseList.getExpenseList(), input).execute();
         actual = outContent.toString().replaceAll(System.lineSeparator(), "\n");
         assertEquals(expected.replaceAll(System.lineSeparator(), "\n"), actual);
         expenseList.clear();
-
     }
 
+    @Test
+    public void findExpense_specialCases() {
+        new CommandAdd(expenseList.getExpenseList(),
+                parser.extractAddParameters("add amt/2.50 " + "t/02-02-2012 cat/food"), currency).execute();
+        new CommandAdd(expenseList.getExpenseList(),
+                parser.extractAddParameters("add amt/2.50 " +
+                        "t/02-02-2012 cur/USD cat/food"), currency).execute();
+        new CommandAdd(expenseList.getExpenseList(),
+                parser.extractAddParameters("add amt/2.50 " +
+                        "t/02-02-2013 cur/USD cat/eat"), currency).execute();
+        new CommandAdd(expenseList.getExpenseList(),
+                parser.extractAddParameters("add amt/2.5 " +
+                        "t/02-02-2013 cur/USD cat/food"), currency).execute();
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        // With the same expenseAmount
+        String input = "2.5";
+        String expected = MESSAGE_DIVIDER_FIND + "\n"
+                + "1.SGD2.50 cat:food date:02/02/2012\n"
+                + "2.USD2.50 cat:food date:02/02/2012\n"
+                + "3.USD2.50 cat:eat date:02/02/2013\n"
+                + "4.USD2.50 cat:food date:02/02/2013\n"
+                + MESSAGE_DIVIDER + "\n";
+
+        new CommandFind(expenseList.getExpenseList(), input).execute();
+        String actual = outContent.toString().replaceAll(System.lineSeparator(), "\n");
+        assertEquals(expected.replaceAll(System.lineSeparator(), "\n"), actual);
+
+
+        input = "2.50"; // It should be no difference compare to the previous case
+        outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        expected = MESSAGE_DIVIDER_FIND + "\n"
+                + "1.SGD2.50 cat:food date:02/02/2012\n"
+                + "2.USD2.50 cat:food date:02/02/2012\n"
+                + "3.USD2.50 cat:eat date:02/02/2013\n"
+                + "4.USD2.50 cat:food date:02/02/2013\n"
+                + MESSAGE_DIVIDER + "\n";
+        new CommandFind(expenseList.getExpenseList(), input).execute();
+        actual = outContent.toString().replaceAll(System.lineSeparator(), "\n");
+        assertEquals(expected.replaceAll(System.lineSeparator(), "\n"), actual);
+
+        input = "13"; // Check whether it can get expenses with years we want
+        outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        expected = MESSAGE_DIVIDER_FIND + "\n"
+                + "1.USD2.50 cat:eat date:02/02/2013\n"
+                + "2.USD2.50 cat:food date:02/02/2013\n"
+                + MESSAGE_DIVIDER + "\n";
+        new CommandFind(expenseList.getExpenseList(), input).execute();
+        actual = outContent.toString().replaceAll(System.lineSeparator(), "\n");
+        assertEquals(expected.replaceAll(System.lineSeparator(), "\n"), actual);
+
+
+        expenseList.clear();
+    }
 
 
 
