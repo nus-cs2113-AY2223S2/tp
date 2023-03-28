f14a2d33d26e23b3561e147829567c1466ccd2ea
javienneyeo <javienneyeo@gmail.com>
2023-03-28 10:10:47 +0800

    Added exceptions to deal with incorrect user input format

--- src/main/java/com/clanki/commands/UpdateCommand.java
+++ src/main/java/com/clanki/commands/UpdateCommand.java
@@ -68,8 +68,10 @@ public class UpdateCommand extends Command {
                 index = implementUpdate(flashcards, userText);
                 System.out.println("Understood. The card has been updated to");
                 printFlashCard(flashcards.get(index));
-            } catch (IndexOutOfBoundsException e) {
+            } catch (ArrayIndexOutOfBoundsException e) {
                 System.out.println("Please enter the input in the correct format as shown in the user guide.");
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("You have selected an index out of the list. There are only " + matchingFlashcards.size() + " flashcards that match the query.");
             }
         } else {
             System.out.println("There are no flashcards with the query \"" + query + "\".");
