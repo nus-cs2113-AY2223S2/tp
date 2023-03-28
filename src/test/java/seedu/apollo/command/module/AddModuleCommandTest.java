71a1121cfa5cc3acabf0e9bb893ccbf7ba4a5136
Hong Lin Shang <e0774890@u.nus.edu>
2023-03-28 15:38:53 +0800

    Full test covergae for Exit, Help Command

--- src/test/java/seedu/apollo/command/module/AddModuleCommandTest.java
+++ src/test/java/seedu/apollo/command/module/AddModuleCommandTest.java
@@ -1,12 +1,9 @@
 package seedu.apollo.command.module;
 
 import org.junit.jupiter.api.Test;
-import seedu.apollo.calendar.Calendar;
 import seedu.apollo.exception.utils.IllegalCommandException;
 import seedu.apollo.module.ModuleList;
 import seedu.apollo.storage.Storage;
-import seedu.apollo.task.TaskList;
-import seedu.apollo.ui.Ui;
 
 import java.io.FileNotFoundException;
 
@@ -15,24 +12,19 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class AddModuleCommandTest {
 
-    TaskList taskList = new TaskList();
-    Ui ui = new Ui();
     Storage storage = new Storage("test.txt", "testModuleData.txt");
-    ModuleList moduleList = new ModuleList();
     ModuleList allModules = storage.loadModuleData();
-    Calendar calendar = new Calendar();
 
     AddModuleCommandTest() throws FileNotFoundException {
     }
 
     @Test
-    void testAddModuleCommand_invalidParams_expectsIllegalCommandException() throws
-            FileNotFoundException {
+    void testAddModuleCommand_invalidParams_expectsIllegalCommandException() {
         assertThrows(IllegalCommandException.class, () -> new AddModuleCommand("addmod modules", allModules));
     }
 
     @Test
-    void testAddModuleCommand_validParams_expectsNoException() throws FileNotFoundException{
+    void testAddModuleCommand_validParams_expectsNoException() {
         assertDoesNotThrow(() -> new AddModuleCommand("CS2113", allModules));
     }
 
