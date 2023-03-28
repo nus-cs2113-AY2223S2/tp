1ced322af28818cafe2d7e4c582dff8b737c24eb
Vignesh-30 <30vignesh@gmail.com>
2023-03-28 17:24:22 +0800

    Created file path to save Sniff Appointments

--- src/main/java/seedu/sniff/Sniff.java
+++ src/main/java/seedu/sniff/Sniff.java
@@ -2,21 +2,27 @@ package seedu.sniff;
 
 import exception.SniffException;
 import functionalities.commands.Command;
+import functionalities.storage.Storage;
 import functionalities.ui.Ui;
 import functionalities.parser.Parser;
 import functionalities.SniffTasks;
 
+import java.io.IOException;
+
 public class Sniff {
     private static SniffTasks tasks;
     private static Ui UI;
+    private static Storage storage;
 
     public Sniff() {
         UI = new Ui();
         tasks = new SniffTasks();
-        //Implement Storage later
+        storage = new Storage();
     }
 
-    public void run() {
+    public void run() throws SniffException, IOException {
+        String absolutePath = getPath();
+        Storage.openFile(absolutePath);
         UI.showWelcomeMessage();
         boolean isExit = false;
         while (!isExit) {
@@ -29,16 +35,23 @@ public class Sniff {
             } catch (SniffException e) {
                 UI.showErrorMessage();
             } finally {
+                storage.saveAppointments(absolutePath);
                 UI.showLine();
             }
         }
     }
 
+    private static String getPath() {
+        String home = System.getProperty("user.home");
+        java.nio.file.Path path = java.nio.file.Paths.get(home, "SniffAppointments.txt");
+        String absolutePath = path.toString();
+        return absolutePath;
+    }
+
     /**
      * Main entry-point for the java.sniff.Sniff application.
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SniffException, IOException {
         new Sniff().run();
     }
-
 }
