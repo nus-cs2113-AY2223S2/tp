d50fce11b8127c4f998a0670cc0663ff3614962f
thomasjlalba <e0773507@u.nus.edu>
2023-03-29 12:05:38 +0800

    Add JUnit testing for execute method for AddExpenseCommand class

--- src/test/java/chching/command/AddExpenseCommandTest.java
+++ src/test/java/chching/command/AddExpenseCommandTest.java
@@ -1,6 +1,14 @@
 package chching.command;
 
+import chching.Storage;
+import chching.Ui;
+import chching.currency.Converter;
+import chching.currency.Selector;
 import chching.record.Expense;
+import chching.record.ExpenseList;
+import chching.record.IncomeList;
+import chching.record.TargetStorage;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -20,8 +28,46 @@ public class AddExpenseCommandTest {
     static final float EXPENSE_VALUE = (float) 1.50;
     static final float ZERO_EXPENSE_VALUE = (float) 0;
     static final float NEGATIVE_EXPENSE_VALUE = (float) -1.50;
-
-
+    private Ui ui;
+    private Storage storage;
+    private Selector selector;
+    private Converter converter;
+    private TargetStorage targetStorage;
+    private IncomeList incomes;
+    private ExpenseList expenses;
+    private Expense transport;
+    
+    /**
+     * Set up the test environment.
+     * creates an empty ExpenseList expenses and empty IncomeList incomes.
+     * transport has category CATEGORY, description DESCRIPTION,
+     * date DATE, value EXPENSE_VALUE.
+     */
+    @BeforeEach
+    void setUp() {
+        ui = new Ui();
+        
+        transport = new Expense(CATEGORY, DESCRIPTION, DATE, EXPENSE_VALUE);
+        expenses = new ExpenseList();
+        incomes = new IncomeList();
+    }
+    
+    /**
+     * Junit Test when there is valid expense input
+     * to execute successfully and add to expenses
+     */
+    @Test
+    void execute_normalScenario_success() {
+        try {
+            int expensesSize = expenses.getRecordCount();
+            AddExpenseCommand command = new AddExpenseCommand(transport);
+            command.execute(incomes, expenses, ui, storage, selector, converter, targetStorage);
+            assertEquals(expensesSize + 1, expenses.size());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            fail(); // test should not reach this line
+        }
+    }
 
     /**
      * Junit Test when there is null field for the expense input
