e39b22b1fb382187c0ec6b60e2254cb2fd20a021
kristianachwan <kristianachwan@gmail.com>
2023-03-29 15:37:02 +0800

    Add javadoc method header for manager and command file

--- src/main/java/commands/staff/ViewStaffCommand.java
+++ src/main/java/commands/staff/ViewStaffCommand.java
@@ -7,14 +7,25 @@ import ui.TextUi;
 public class ViewStaffCommand extends Command {
     public static final String COMMAND_WORD = "view_staff";
 
+    /**
+     * Constructor of ViewStaffCommand
+     */
     public ViewStaffCommand() {
     }
 
+    /**
+     * Execute the command of viewing all staffs.
+     * @param ui Ui object in if there is anything to be printed.
+     */
     @Override
     public void execute(TextUi ui) {
         ui.printMessage(StaffManager.getStaffsString());
     }
 
+    /**
+     * Indicates whether the program should exit or not
+     * @return Boolean to indicate whether exist is true or not
+     */
     @Override
     public boolean isExit() {
         return false;
