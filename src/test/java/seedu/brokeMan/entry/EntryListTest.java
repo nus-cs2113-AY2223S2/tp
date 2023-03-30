b0ce5cf5637469e0322ca7b866c366a91aee841e
namsengi11 <namsengi11@gmail.com>
2023-03-30 23:44:40 +0800

    Add Entry Test
    Added multiple J Unit test classes to test EntryList, ExpenseList, and IncomeList.

--- src/test/java/seedu/brokeMan/entry/EntryListTest.java
+++ src/test/java/seedu/brokeMan/entry/EntryListTest.java
@@ -3,14 +3,14 @@ package seedu.brokeMan.entry;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import seedu.brokeMan.entry.expense.Expense;
+import seedu.brokeMan.entry.income.Income;
 
 import java.util.Arrays;
 import java.util.LinkedList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static seedu.brokeMan.entry.EntryList.deleteEntry;
-import static seedu.brokeMan.entry.EntryList.getEntryListSum;
+import static seedu.brokeMan.entry.EntryList.*;
 import static seedu.brokeMan.parser.StringToTime.convertStringToTime;
 import static seedu.brokeMan.parser.StringToCategory.convertStringToCategory;
 
@@ -19,24 +19,38 @@ public class EntryListTest {
 
     Entry entry1;
     Entry entry2;
+    Entry entry3;
+    Entry entry4;
+    Entry entry5;
+    Entry entry6;
     LinkedList<Entry> expenseList;
+    LinkedList<Entry> incomeList;
 
     @BeforeEach
     public void setUp() throws Exception {
         entry1 = new Expense(4, "lunch", convertStringToTime("2022 09 08 12 14"), convertStringToCategory("FOOD"));
         entry2 = new Expense(6, "dinner",  convertStringToTime("2023 12 12 20 01"), convertStringToCategory("FOOD"));
+        entry3 = new Expense(5, "book", convertStringToTime("2023 09 08 20 14"), convertStringToCategory("ENTERTAINMENT"));
         expenseList = new LinkedList<Entry>(Arrays.asList(entry1, entry2));
+        entry4 = new Income(5, "Stock", convertStringToTime("2023 03 29 10 11"), convertStringToCategory("INVESTMENT"));
+        entry5 = new Income(10, "TA", convertStringToTime("2023 03 10 12 15"), convertStringToCategory("SALARY"));
+        entry5 = new Income(20, "Allowance", convertStringToTime("2023 03 31 20 50"), convertStringToCategory("OTHERS"));
+        incomeList = new LinkedList<Entry>(Arrays.asList(entry4, entry5));
     }
 
-
     @Test
-    public void deleteEntry_inBoundIndex_success() {
+    public void addEntryExpense_validExpense_success() {
+        addEntry(entry3, expenseList);
+        assertEquals(expenseList, new LinkedList<>(Arrays.asList(entry1, entry2, entry3)));
+    }
+    @Test
+    public void deleteEntryExpense_inBoundIndex_success() {
         deleteEntry(2, expenseList);
         assertEquals(expenseList, new LinkedList<>(Arrays.asList(entry1)));
     }
 
     @Test
-    public void deleteEntry_outBoundIndex_exceptionThrown() {
+    public void deleteEntryExpense_outBoundIndex_exceptionThrown() {
         try {
             deleteEntry(3, expenseList);
         } catch (IndexOutOfBoundsException | IllegalArgumentException e) {
@@ -46,9 +60,33 @@ public class EntryListTest {
     }
 
     @Test
-    public void getTotalAmount_inputList_success() {
+    public void getTotalAmountExpense_inputList_success() {
         assertEquals(10, getEntryListSum(expenseList));
     }
 
+    @Test
+    public void addEntryIncome_validIncome_success() {
+        addEntry(entry6, incomeList);
+        assertEquals(incomeList, new LinkedList<>(Arrays.asList(entry4, entry5, entry6)));
+    }
 
+    @Test
+    public void deleteEntryIncome_inBoundIndex_success() {
+        deleteEntry(1, incomeList);
+        assertEquals(incomeList, new LinkedList<>(Arrays.asList(entry5)));
+    }
+
+    @Test
+    public void deleteEntryIncome_outBoundIndex_exceptionThrown() {
+        try {
+            deleteEntry(0, expenseList);
+        } catch (IndexOutOfBoundsException e) {
+            assertThrows(IndexOutOfBoundsException.class, () -> deleteEntry(4, incomeList));
+        }
+    }
+
+    @Test
+    public void getTotalAmountIncome_inputList_success() {
+        assertEquals(25, getEntryListSum(incomeList));
+    }
 }
---
+++ src/test/java/seedu/brokeMan/entry/ExpenseListTest.java
@@ -0,0 +1,35 @@
+package seedu.brokeMan.entry;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import seedu.brokeMan.entry.expense.Expense;
+import seedu.brokeMan.entry.expense.ExpenseList;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.LinkedList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExpenseListTest {
+    private static Expense Expense1;
+    private static Expense Expense2;
+
+    @BeforeAll
+    public static void setUp() {
+        Expense1 = new Expense(2000.0, "Food", LocalDateTime.of(2023, Month.APRIL, 1, 10, 0), Category.FOOD);
+        Expense2 = new Expense(500.0, "Book", LocalDateTime.of(2023, Month.APRIL, 15, 15, 30), Category.ENTERTAINMENT);
+        ExpenseList.addExpense(Expense1);
+        ExpenseList.addExpense(Expense2);
+    }
+
+    @Test
+    public void testAddExpense_validExpense_success() {
+        Expense Expense3 = new Expense(100.0, "Drink", LocalDateTime.of(2023, Month.APRIL, 25, 12, 0), Category.ENTERTAINMENT);
+        ExpenseList.addExpense(Expense3);
+        LinkedList<Expense> expectedExpenseList = new LinkedList<>();
+        expectedExpenseList.add(Expense1);
+        expectedExpenseList.add(Expense2);
+        expectedExpenseList.add(Expense3);
+        assertEquals(expectedExpenseList, ExpenseList.expenseList);
+    }
+
+}
