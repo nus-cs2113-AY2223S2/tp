1b325d63f2778970cf7615decb3dae15df1695b8
hrithie <hrithie@hotmail.com>
2023-03-30 13:20:20 +0800

    Implemented logger & added logging

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -1,18 +1,16 @@
 package seedu.duke;
 
-// import java.util.Scanner;
-//import seedu.duke.ui.Ui;
-//import seedu.duke.EventList;
-//import seedu.duke.parser.Parser;
-
-
 import seedu.duke.storage.EventListStorage;
 import seedu.duke.storage.NusModuleLoader;
 import seedu.duke.storage.JsonEventListStorage;
 import seedu.duke.storage.JsonNusModuleLoader;
 import seedu.duke.storage.Storage;
 import seedu.duke.storage.StorageManager;
-
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class Duke {
     /**
@@ -24,7 +22,10 @@ public class Duke {
     private final EventListStorage eventListStorage;
     private final NusModuleLoader nusModuleLoader;
     private final Storage storage;
+    public static final Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+
     public Duke (){
+        startLogger();
         ui = new Ui();
         eventListStorage = new JsonEventListStorage();
         nusModuleLoader = new JsonNusModuleLoader();
@@ -32,15 +33,42 @@ public class Duke {
         eventTracker = new EventList(storage.loadEvents());
         parser = new Parser();
     }
+
     public void run(){
+        LOGGER.log(Level.INFO, "NUSPlanner is starting!");
         ui.showWelcome();
         ui.getSemester();
+        LOGGER.log(Level.INFO, "User has successfully input semester they are in.");
         ui.getUserCommand(eventTracker);
+        LOGGER.log(Level.INFO, "User input is 'bye', proceeding to save edits to file (if any).");
+        LOGGER.log(Level.INFO, "Exiting NUSPlanner.");
         storage.saveToFile(eventTracker); //Will only save to file on exit
     }
 
     public static void main(String[] args) {
         Duke nusPlanner = new Duke();
         nusPlanner.run();
+    }
+
+    /**
+     * Starts logger to log information throughout the execution
+     * of NUSPlanner. The logging information written to "log.txt"
+     */
+    private static void startLogger() {
+        LOGGER.setUseParentHandlers(false);
+        LOGGER.setLevel(Level.ALL);
+        ConsoleHandler consoleH = new ConsoleHandler();
+        consoleH.setLevel(Level.SEVERE);
+        LOGGER.addHandler(consoleH);
+
+        try {
+            FileHandler fileH = new FileHandler("log.txt");
+            fileH.setLevel(Level.FINE);
+            SimpleFormatter formatter = new SimpleFormatter();
+            fileH.setFormatter(formatter);
+            LOGGER.addHandler(fileH);
+        } catch (java.io.IOException e) {
+            LOGGER.log(Level.SEVERE, "Error detected in file logger!", e);
+        }
     }
 }
