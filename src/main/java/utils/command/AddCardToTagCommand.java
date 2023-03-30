7b29742be7b00e5bf527797a3a3b37b521eb9803
Mitch Malvin <e0773911@u.nus.edu>
2023-03-30 15:34:09 +0800

    fix savedata.json

--- src/main/java/utils/command/AddCardToTagCommand.java
+++ src/main/java/utils/command/AddCardToTagCommand.java
@@ -51,6 +51,7 @@ public class AddCardToTagCommand extends Command {
 
         //add the tag uuid to the card
         tagUUID = tagToAdd.getUUID();
+        
         cardToAdd.addTag(tagUUID);
     }
 
