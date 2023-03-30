33e66587b1f4825af106705c4cef6dd2130a454a
ychui <huuyucong2000@gmail.com>
2023-03-30 10:21:04 +0800

    Update text-ui-test

--- src/main/java/seedu/badMaths/ui/Ui.java
+++ src/main/java/seedu/badMaths/ui/Ui.java
@@ -8,16 +8,16 @@ import java.util.ArrayList;
 public class Ui {
 
     private static String logo = "\n" +
-            "____            _   __  __       _   _         \n" +
-            "|  _ \\          | | |  \\/  |     | | | |        \n" +
-            "| |_) | __ _  __| | | \\  / | __ _| |_| |__  ___ \n" +
+            "____            _   __  __       _   _\n" +
+            "|  _ \\          | | |  \\/  |     | | | |\n" +
+            "| |_) | __ _  __| | | \\  / | __ _| |_| |__  ___\n" +
             "|  _ < / _` |/ _` | | |\\/| |/ _` | __| '_ \\/ __|\n" +
             "| |_) | (_| | (_| | | |  | | (_| | |_| | | \\__ \\\n" +
             "|____/ \\__,_|\\__,_| |_|  |_|\\__,_|\\__|_| |_|___/";
 
 
     public static void printWelcomeMessage() {
-        System.out.println("This is BadMaths. You can type 'Help.' to learn what I can do for you :)");
+        System.out.println(logo + "\nYou can type 'Help.' to learn what I can do for you :)");
     }
     public static void printLogo(){
         System.out.println(logo);
