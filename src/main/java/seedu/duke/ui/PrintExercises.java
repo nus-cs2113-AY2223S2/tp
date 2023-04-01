57af4cf6a49efc658305c849c759396c8da4a4e1
L-K-Chng <lipkuang02@gmail.com>
2023-04-01 19:22:56 +0800

    Added string formatting for data method.

--- src/main/java/seedu/duke/ui/PrintExercises.java
+++ src/main/java/seedu/duke/ui/PrintExercises.java
@@ -9,26 +9,30 @@ public class PrintExercises {
     private static final String CLOSE_BRACE = "]";
 
     public static void printExercise(ArrayList<ExerciseData> exercises) {
-        String getWorkoutType;
-        String getWorkoutTypeFinal;
-        String getInstructions;
-        String getInstructionsFinal;
-        for (ExerciseData exercise : exercises) {
-            // Removed open and close braces for instructions as well as workout type.
-            getWorkoutType = exercise.getWorkoutType().toString();
-            getInstructions = exercise.getInstructions().toString();
-            int start = getWorkoutType.indexOf(OPEN_BRACE);
-            int end = getWorkoutType.indexOf(CLOSE_BRACE);
-            int startInstructions = getInstructions.indexOf(OPEN_BRACE);
-            int endInstructions = getInstructions.indexOf(CLOSE_BRACE);
-            getWorkoutTypeFinal = getWorkoutType.substring(start + 1, end);
-            getInstructionsFinal = getInstructions.substring(startInstructions + 1, endInstructions);
-            System.out.println("Exercise ID: " + exercise.getId() + ". ");
-            System.out.println("Name: " + exercise.getName());
-            System.out.println("Difficulty Level: " + exercise.getLevel());
-            System.out.println("Workout Type: " + getWorkoutTypeFinal);
-            System.out.println(getInstructionsFinal + System.lineSeparator());
+        if (exercises.isEmpty()) {
+            System.out.println("You have not completed any exercises ☹");
+            System.out.println("Add on to this list by completing a workout session!");
+        } else {
+            String getWorkoutType;
+            String getWorkoutTypeFinal;
+            String getInstructions;
+            String getInstructionsFinal;
+            for (ExerciseData exercise : exercises) {
+                // Removed open and close braces for instructions as well as workout type.
+                getWorkoutType = exercise.getWorkoutType().toString();
+                getInstructions = exercise.getInstructions().toString();
+                int start = getWorkoutType.indexOf(OPEN_BRACE);
+                int end = getWorkoutType.indexOf(CLOSE_BRACE);
+                int startInstructions = getInstructions.indexOf(OPEN_BRACE);
+                int endInstructions = getInstructions.indexOf(CLOSE_BRACE);
+                getWorkoutTypeFinal = getWorkoutType.substring(start + 1, end);
+                getInstructionsFinal = getInstructions.substring(startInstructions + 1, endInstructions);
+                System.out.println("Exercise ID: " + exercise.getId() + ". ");
+                System.out.println("Name: " + exercise.getName());
+                System.out.println("Difficulty Level: " + exercise.getLevel());
+                System.out.println("Workout Type: " + getWorkoutTypeFinal);
+                System.out.println(getInstructionsFinal + System.lineSeparator());
+            }
         }
     }
-
 }
