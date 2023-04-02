4484579e5f2e19fa760dedec7ef59cf5c9b08762
TzeLoong <tzeloong12@gmail.com>
2023-04-02 13:32:05 +0800

    Fix code style violations

--- src/test/java/seedu/commands/LendExpenditureCommandTest.java
+++ src/test/java/seedu/commands/LendExpenditureCommandTest.java
@@ -1,10 +1,10 @@
 package seedu.commands;
 
-import org.junit.jupiter.api.Test;
-import seedu.expenditure.AcademicExpenditure;
+// import org.junit.jupiter.api.Test;
+// import seedu.expenditure.AcademicExpenditure;
 import seedu.expenditure.ExpenditureList;
-import java.time.LocalDate;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+// import java.time.LocalDate;
+// import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LendExpenditureCommandTest {
     ExpenditureList testExpenditures = new ExpenditureList();
