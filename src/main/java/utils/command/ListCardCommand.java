b23d3abb23558a294858e5ff8fbddab083bbb667
Mitch Malvin <e0773911@u.nus.edu>
2023-04-02 22:19:44 +0800

    ref : fix checkstyle

--- src/main/java/utils/command/ListCardCommand.java
+++ src/main/java/utils/command/ListCardCommand.java
@@ -1,14 +1,8 @@
 package utils.command;
 
-import java.util.ArrayList;
-import model.Card;
 import model.CardList;
-import model.Deck;
 import model.DeckList;
-import model.DeckUUID;
-import model.Tag;
 import model.TagList;
-import model.TagUUID;
 import utils.UserInterface;
 import utils.exceptions.InkaException;
 import utils.storage.IDataStorage;
