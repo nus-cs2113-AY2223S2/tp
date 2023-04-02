c7efa12f1c2f5ed8002624d1d125e95f31d675f1
Mitch Malvin <e0773911@u.nus.edu>
2023-04-02 21:23:38 +0800

    test: unit test for cardSelector

---
+++ src/test/java/model/CardSelectorTest.java
@@ -0,0 +1,22 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import utils.exceptions.InvalidUUIDException;
+
+public class CardSelectorTest {
+
+    private CardSelector cardSelector;
+
+    private final String INVALID_UUID_STR = "UUID needs to be in 03658854-e5d4-468f-8c41-74917e5d4515 format";
+
+    @Test
+    void CardSelectorInvalidUUID() {
+        try {
+            this.cardSelector = new CardSelector("weird uuid string");
+        } catch (InvalidUUIDException e) {
+            assertEquals(INVALID_UUID_STR, e.getUiMessage());
+        }
+    }
+}
