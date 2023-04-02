bd21e3d24fcf383734d856ebbd40b1444140ffd3
Leo Zheng Rui Darren <leo.darren@gmail.com>
2023-04-03 02:38:38 +0800

    Improve error messages for lend and borrow expenditures

--- src/main/java/seedu/ui/ErrorMessages.java
+++ src/main/java/seedu/ui/ErrorMessages.java
@@ -16,10 +16,11 @@ public enum ErrorMessages {
     ERROR_NOT_POSITIVE_VALUE_MESSAGE("Please enter a positive value!"),
     ERROR_COMMAND_NOT_RECOGNISED_MESSAGE("Command not recognised. Please try again"),
     ERROR_LACK_OF_PARAMETERS_MESSAGE("Input command does not have required parameters! Please try again"),
-    ERROR_DATE_TIME_ERROR_MESSAGE("Date error! Please enter your date in yyyy-mm-dd format!"),
+    ERROR_DATE_TIME_ERROR_MESSAGE("Date error! Please enter a single date in yyyy-mm-dd format!"),
     ERROR_INVALID_EXPENDITURE_TYPE_MESSAGE("Invalid expenditure type! Please try again"),
     ERROR_NUMBER_FORMAT_MESSAGE("Index must be an integer and within bounds! Please try again"),
-    ERROR_AMOUNT_FORMAT_MESSAGE("The amount you provided is not in the right format!"),
+    ERROR_AMOUNT_FORMAT_MESSAGE("The amount you provided is not in the right format! " +
+            "Please enter a single number value"),
     ERROR_WRONG_FORMAT_MESSAGE("Input command cannot be recognised as it is in the wrong format. Please try again"),
     ERROR_BUDGET_NOT_NUMERICAL_MESSAGE("Budget amount is not a numerical value!"),
     ERROR_INVALID_DATE_MESSAGE("Borrow date must be before the return date! Please try again"),
