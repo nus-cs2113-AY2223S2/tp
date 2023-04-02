1e9f021eb8ce714fb369df220938c650d4f71e28
azriellee <azriel20005@gmail.com>
2023-04-02 20:21:02 +0800

    IO files fix

--- src/main/java/seedu/rainyDay/RainyDay.java
+++ src/main/java/seedu/rainyDay/RainyDay.java
@@ -47,7 +47,6 @@ public class RainyDay {
     private void run() {
         setUpDate();
         runCommand();
-        ui.sayFarewellToUser(userData.getReportOwner());
     }
 
     private void setUpDate() {
