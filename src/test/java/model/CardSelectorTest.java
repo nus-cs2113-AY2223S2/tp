9d75b68cee4c90907b8f49e9b43d356911177b10
Mitch Malvin <e0773911@u.nus.edu>
2023-04-02 21:37:23 +0800

    ref: fix checkstyle issue

--- src/test/java/model/CardSelectorTest.java
+++ src/test/java/model/CardSelectorTest.java
@@ -7,12 +7,12 @@ import utils.exceptions.InvalidUUIDException;
 
 public class CardSelectorTest {
 
+    private static final String INVALID_UUID_STR = "UUID needs to be in 03658854-e5d4-468f-8c41-74917e5d4515 format";
+
     private CardSelector cardSelector;
 
-    private final String INVALID_UUID_STR = "UUID needs to be in 03658854-e5d4-468f-8c41-74917e5d4515 format";
-
     @Test
-    void CardSelectorInvalidUUID() {
+    void cardSelectorInvalidUUID() {
         try {
             this.cardSelector = new CardSelector("weird uuid string");
         } catch (InvalidUUIDException e) {
