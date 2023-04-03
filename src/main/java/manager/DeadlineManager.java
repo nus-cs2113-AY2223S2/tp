c8e585bd47507124ff6e2de07b70206242c11b73
Stella1585 <88623728+Stella1585@users.noreply.github.com>
2023-04-03 18:16:24 +0800

    no error msg when deadline list empty

--- src/main/java/manager/DeadlineManager.java
+++ src/main/java/manager/DeadlineManager.java
@@ -52,16 +52,13 @@ public class DeadlineManager {
      * @param ui manages user output.
      */
     public static void printDeadlines(TextUi ui) {
-        try {
-            if (deadlines.isEmpty()) {
-                throw new DinerDirectorException(Messages.MESSAGE_DEADLINE_EMPTY_LIST);
-            }
-            System.out.println(Messages.MESSAGE_DEADLINE_VIEW_LIST);
-            for (int i = 1; i <= deadlines.size(); i++) {
-                ui.printMessage(i + ". " + deadlines.get(i - 1).toString());
-            }
-        } catch (DinerDirectorException e) {
-            System.out.println(e);
+        if (deadlines.isEmpty()) {
+            System.out.println(Messages.MESSAGE_DEADLINE_EMPTY_LIST);
+            return;
+        }
+        System.out.println(Messages.MESSAGE_DEADLINE_VIEW_LIST);
+        for (int i = 1; i <= deadlines.size(); i++) {
+            ui.printMessage(i + ". " + deadlines.get(i - 1).toString());
         }
     }
 
