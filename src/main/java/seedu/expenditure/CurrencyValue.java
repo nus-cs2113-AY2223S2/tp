817053e3a1a12f10fafbeedb7a716c3b7dcc5873
Chick3nBoy <leonardo.ongdc@gmail.com>
2023-04-03 22:27:32 +0800

    Add currency value class

---
+++ src/main/java/seedu/expenditure/CurrencyValue.java
@@ -0,0 +1,20 @@
+package seedu.expenditure;
+
+public class CurrenyValue {
+    private static final double AUS_CONVERSION = 1.11;
+    private static final double CAD_CONVERSION = 1.01;
+    private static final double CNY_CONVERSION = 5.07;
+    private static final double DKK_CONVERSION = 5.15;
+    private static final double EUR_CONVERSION = 0.69;
+    private static final double GBP_CONVERSION = 0.61;
+    private static final double ILS_CONVERSION = 2.70;
+    private static final double JPY_CONVERSION = 99.96;
+    private static final double KRW_CONVERSION = 989.05;
+    private static final double NOK_CONVERSION = 7.78;
+    private static final double NZD_CONVERSION = 1.20;
+    private static final double SEK_CONVERSION = 7.80;
+    private static final double TWD_CONVERSION = 22.98;
+    private static final double USD_CONVERSION = 7.78;
+
+
+}
