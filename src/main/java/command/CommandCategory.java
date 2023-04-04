6f430b9785f338b815c95b0f41978ff0bc92d1f8
FanZixian <u3577161@connect.hku.hk>
2023-04-04 11:43:08 +0800

    Update case sensitivity problem for category method, also it can accept "" input by default to set the category to uncategorized

--- src/main/java/command/CommandCategory.java
+++ src/main/java/command/CommandCategory.java
@@ -30,12 +30,21 @@ public class CommandCategory extends Command {
         }
     }
 
+    /**
+     * To deal with case sensitivity problem, all categories will be stored in this Set in lower case
+     */
     public CommandCategory(ArrayList<Expense> expenseList, String category) {
         super(COMMAND_NAME);
         this.expenseList = expenseList;
-        this.category = category;
+        // If user doesn't enter any category, it will return all expenses with uncategorized by default
+        if (category.equals("")) {
+            this. category = "uncategorized";
+        } else {
+            this.category = category.toLowerCase();
+        }
+
         for (Expense e : expenseList) {
-            String categoryOfe = e.getDescription();
+            String categoryOfe = e.getDescription().toLowerCase();
             if (!categorySet.contains(categoryOfe)) {
                 categorySet.add(categoryOfe);
             }
