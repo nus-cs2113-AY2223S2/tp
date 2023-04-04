6f18cadd81547d79a2dc27638b020ec03144d7f3
Shawn Tang <shawntang83@gmail.com>
2023-04-04 14:25:20 +0800

    Add JUnit for PetList

--- src/test/java/seedu/pettracker/data/PetListTest.java
+++ src/test/java/seedu/pettracker/data/PetListTest.java
@@ -5,6 +5,8 @@ import seedu.pettracker.exceptions.DuplicatePetException;
 import seedu.pettracker.exceptions.EmptyPetNameException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class PetListTest {
 
     @Test
@@ -13,8 +15,30 @@ public class PetListTest {
         assertEquals(0, pl.getNumberOfPets());
     }
 
+    @Test
     void addPetToList() throws EmptyPetNameException, DuplicatePetException {
         PetList.addPet("Bob");
         assertEquals(1, PetList.getNumberOfPets());
+    }
+
+    @Test
+    public void addPetToList_duplicateName_exceptionThrown() {
+        try {
+            PetList.addPet("Alice");
+            PetList.addPet("Alice");
+            fail();
+        } catch (Exception e){
+            assertEquals("ERROR: Pet already exists",e.getMessage());
+        }
+    }
+
+    @Test
+    public void addPetToList_emptyName_exceptionThrown() {
+        try {
+            PetList.addPet("");
+            fail();
+        } catch (Exception e){
+            assertEquals("ERROR: Pet Name is empty",e.getMessage());
+        }
     }
 }
