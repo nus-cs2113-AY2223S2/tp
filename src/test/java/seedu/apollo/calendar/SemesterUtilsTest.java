215df48d77e2909e47e0ca6dc083275a47aac4da
gohyixuan <gohyixuan02@gmail.com>
2023-04-05 02:38:33 +0800

    Remove unused imports

--- src/test/java/seedu/apollo/calendar/SemesterUtilsTest.java
+++ src/test/java/seedu/apollo/calendar/SemesterUtilsTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SemesterUtilsTest {
