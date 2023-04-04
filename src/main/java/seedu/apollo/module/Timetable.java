e39f66837773e29aa7260212bf531e2f57fe2e99
T-Wan-Lin <tanwanlin.c2021>
2023-04-04 23:24:59 +0800

    Add week enhancement

--- src/main/java/seedu/apollo/module/Timetable.java
+++ src/main/java/seedu/apollo/module/Timetable.java
@@ -61,6 +61,65 @@ public class Timetable {
         return weeks;
     }
 
+    /**
+     * This method compresses the weeks of a lesson into a String that can be used for the UI.
+     * For example, if a lesson occurs on weeks 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, then
+     * the method will return [Weekly]. If a lesson does not occur for consecutive weeks, then
+     * it will print out all weeks that the lesson occurs on. For example, if a lesson occurs on
+     * weeks 5, 7, 9, 11, then the method will return [Weeks: 5, 7, 9, 11, 13]. Lastly if a lesson occurs on
+     * consecutive weeks but does not span the entire semester, then the method will return the first and last weeks.
+     * For example, if a lesson occurs on weeks 1, 2, 3, 4, 5, then the method will return [Weeks: 1-5].
+     * @param   timetable       The timetable of the lesson.
+     * @return  The formatted String representation of the weeks.
+     */
+    public String compressedWeeks(Timetable timetable) {
+        ArrayList<Integer> unfilteredWeeks = timetable.getWeeks();
+        ArrayList<Integer> filteredWeeks = new ArrayList<>();
+        String formattedWeeks;
+
+        int firstWeek = unfilteredWeeks.get(0);
+        int lastWeek = unfilteredWeeks.get(unfilteredWeeks.size() - 1);
+
+        //First, check if the lesson spans the whole 13-week semester
+        if (unfilteredWeeks.size() == 13) {
+            formattedWeeks = "[Weekly]";
+            return formattedWeeks;
+        } else if (lastWeek - firstWeek == unfilteredWeeks.size() - 1) {
+            //else if is continguous, then compress
+            filteredWeeks.add(firstWeek);
+            filteredWeeks.add(lastWeek);
+        } else {
+            //else print all weeks lesson occurs on
+            filteredWeeks = unfilteredWeeks;
+        }
+
+        //return String representation
+        if (filteredWeeks.size() == unfilteredWeeks.size()){
+            formattedWeeks = "["+ "Weeks: " + arrayListToString(filteredWeeks, ", ") + "]";
+        } else {
+            formattedWeeks = "["+ "Weeks: " + filteredWeeks.get(0) + "-" + filteredWeeks.get(1) + "]";
+        }
+        return formattedWeeks;
+    }
+
+    /**
+     * This method converts an ArrayList of Integers into a String.
+     * @param arrayList The ArrayList to be converted.
+     * @param delimiter The delimiter to separate the elements of the ArrayList.
+     * @return String representation of the ArrayList.
+     */
+    public String arrayListToString(ArrayList<Integer>arrayList, String delimiter){
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < arrayList.size(); i++) {
+            stringBuilder.append(arrayList.get(i));
+            if (i != arrayList.size() - 1) {
+                stringBuilder.append(delimiter);
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+
     public void setWeeks(ArrayList<Integer> weeks) {
         this.weeks = weeks;
     }
--- src/main/java/seedu/apollo/ui/Ui.java
+++ src/main/java/seedu/apollo/ui/Ui.java
@@ -82,7 +82,7 @@ public class Ui {
         printNote();
         showLine();
         printUtility();
-        //printAddModuleOptions();
+
     }
 
     private void printHelpCommandOptions(){
@@ -280,9 +280,10 @@ public class Ui {
     public void printModuleListWithLesson(Module newModule, ArrayList<Timetable> timetableList) {
         System.out.println("These are your classes for Module " + newModule.getCode() + ": \n");
         for (Timetable timetable : timetableList) {
+
             System.out.println(timetable.getLessonType() + " " + timetable.getClassNumber() + '\n' +
                     "   " + timetable.getDay() + " " + timetable.getStartTime() + " - " +
-                    timetable.getEndTime());
+                    timetable.getEndTime() + " " + timetable.compressedWeeks(timetable));
         }
 
     }
@@ -329,7 +330,7 @@ public class Ui {
         for (Timetable timetable : copyList) {
             System.out.println("Class Number: " + timetable.getClassNumber());
             System.out.println("   " + timetable.getDay() + " " + timetable.getStartTime() + " - " +
-                    timetable.getEndTime());
+                                timetable.getEndTime() + " " + timetable.compressedWeeks(timetable));
         }
     }
 
@@ -393,10 +394,14 @@ public class Ui {
         printLessonTypeMessage(lessonTypes);
         System.out.println();
         for (Timetable timetable : timetableList) {
+
             System.out.println(timetable.getLessonType() + " " + timetable.getClassNumber() + '\n' +
-                    "   " + timetable.getDay() + " " + timetable.getStartTime() + " - " + timetable.getEndTime());
+                    "   " + timetable.getDay() + " " + timetable.getStartTime() + " - "
+                    + timetable.getEndTime() + " " + timetable.compressedWeeks(timetable));
         }
     }
+
+
 
     /**
      * For {@code addmod, delmod, listmod} commands.
@@ -771,7 +776,7 @@ public class Ui {
         for (Timetable timetable : copyList) {
             System.out.println("Class Number: " + timetable.getClassNumber());
             System.out.println("   " + timetable.getDay() + " " + timetable.getStartTime() + " - " +
-                    timetable.getEndTime());
+                    timetable.getEndTime() + " " + timetable.compressedWeeks(timetable));
         }
     }
 
