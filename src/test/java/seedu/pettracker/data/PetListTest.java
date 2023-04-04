20bacac270a8360cb896f96593998dff343c7ded
Shawn Tang <shawntang83@gmail.com>
2023-04-04 14:41:09 +0800

    Add stat-related JUnit to PetList

--- src/test/java/seedu/pettracker/data/PetListTest.java
+++ src/test/java/seedu/pettracker/data/PetListTest.java
@@ -3,6 +3,9 @@ package seedu.pettracker.data;
 import org.junit.jupiter.api.Test;
 import seedu.pettracker.exceptions.DuplicatePetException;
 import seedu.pettracker.exceptions.EmptyPetNameException;
+import seedu.pettracker.exceptions.InvalidStatException;
+import seedu.pettracker.exceptions.NonPositiveIntegerException;
+import seedu.pettracker.exceptions.PetNotFoundException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
@@ -39,6 +42,46 @@ public class PetListTest {
             fail();
         } catch (Exception e){
             assertEquals("ERROR: Pet Name is empty",e.getMessage());
+        }
+    }
+
+    @Test
+    public void addStatToPet_correctStat_success() throws NonPositiveIntegerException,
+            InvalidStatException, PetNotFoundException, DuplicatePetException, EmptyPetNameException {
+        PetList.addPet("Candy");
+        PetList.addStat("Candy","Weight","10");
+    }
+
+    @Test
+    public void addStatToPet_nonPositiveInteger_exceptionThrown() {
+        try {
+            PetList.addPet("Dylan");
+            PetList.addStat("Dylan","Weight","-1");
+            fail();
+        } catch (Exception e){
+            assertEquals("ERROR: Integer provided should be above 0",e.getMessage());
+        }
+    }
+
+    @Test
+    public void addStatToPet_invalidStat_exceptionThrown() {
+        try {
+            PetList.addPet("Ellie");
+            PetList.addStat("Ellie","invalid","10");
+            fail();
+        } catch (Exception e){
+            assertEquals("ERROR: The only valid stats are type, age, or weight.",e.getMessage());
+        }
+    }
+
+    @Test
+    public void addStatToPet_petNotFound_exceptionThrown() {
+        try {
+            PetList.addPet("Freddy");
+            PetList.addStat("invalid","Weight","10");
+            fail();
+        } catch (Exception e){
+            assertEquals("ERROR: Pet not Found",e.getMessage());
         }
     }
 }
