03f536aa074b5bc2013fdb0458efe36b4489fcd7
KN-CY <kaemoncyng@gmail.com>
2023-04-06 01:03:45 +0800

    Update Storage test name

--- src/test/java/seedu/rainyDay/modules/StorageTest.java
+++ src/test/java/seedu/rainyDay/modules/StorageTest.java
@@ -68,7 +68,7 @@ public class StorageTest {
     }
 
     @Test
-    public void loadFromFile_invalidFileType_classNotFoundExceptionThrown() {
+    public void loadFromFile_invalidFileType_JsonParseExceptionThrown() {
         String invalidTypeFilePath = "test files/thisFileTypeIsInvalid.txt";
         assertThrows(JsonParseException.class, () -> Storage.loadFromFile(invalidTypeFilePath));
     }
