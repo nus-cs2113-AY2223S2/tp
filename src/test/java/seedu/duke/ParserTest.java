4718243ddf1fb5034d9e30bf085440038f4841bc
briantjs00 <briantjs00@gmail.com>
2023-04-05 20:37:46 +0800

    Add JUnit test for Parser class

---
+++ src/test/java/seedu/duke/ParserTest.java
@@ -0,0 +1,77 @@
+package seedu.duke;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.duke.budget.BudgetPlanner;
+import seedu.duke.command.ExitCommand;
+import seedu.duke.command.HelpCommand;
+import seedu.duke.command.ListCurrentCommand;
+import seedu.duke.command.ListPuCommand;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+public class ParserTest {
+    @Test
+    public void parseUserCommand_listPU_success() {
+        String userInput = "/list pu";
+        Parser parser = new Parser();
+        ArrayList<University> universities = new ArrayList<>();
+        ArrayList<Module> modules = new ArrayList<>();
+        ArrayList<Module> puModules = new ArrayList<>();
+        ArrayList<Deadline> deadlines = new ArrayList<>();
+        Storage storage = new Storage();
+        DeadlineStorage deadlineStorage = new DeadlineStorage();
+        BudgetPlanner budgetPlanner = new BudgetPlanner();
+        assertTrue(parser.parseUserCommand(userInput, universities, modules, puModules, storage, deadlineStorage,
+                budgetPlanner, deadlines) instanceof ListPuCommand);
+    }
+
+    @Test
+    public void parseUserCommand_listCurrent_success() {
+        String userInput = "/list current";
+        Parser parser = new Parser();
+        ArrayList<University> universities = new ArrayList<>();
+        ArrayList<Module> modules = new ArrayList<>();
+        ArrayList<Module> puModules = new ArrayList<>();
+        ArrayList<Deadline> deadlines = new ArrayList<>();
+        Storage storage = new Storage();
+        DeadlineStorage deadlineStorage = new DeadlineStorage();
+        BudgetPlanner budgetPlanner = new BudgetPlanner();
+        assertTrue(parser.parseUserCommand(userInput, universities, modules, puModules, storage, deadlineStorage,
+                budgetPlanner, deadlines) instanceof ListCurrentCommand);
+    }
+
+    @Test
+    public void parseUserCommand_exit_success() {
+        String userInput = "/exit";
+        Parser parser = new Parser();
+        ArrayList<University> universities = new ArrayList<>();
+        ArrayList<Module> modules = new ArrayList<>();
+        ArrayList<Module> puModules = new ArrayList<>();
+        ArrayList<Deadline> deadlines = new ArrayList<>();
+        Storage storage = new Storage();
+        DeadlineStorage deadlineStorage = new DeadlineStorage();
+        BudgetPlanner budgetPlanner = new BudgetPlanner();
+        assertTrue(parser.parseUserCommand(userInput, universities, modules, puModules, storage, deadlineStorage,
+                budgetPlanner, deadlines) instanceof ExitCommand);
+    }
+
+    @Test
+    public void parseUserCommand_help_success() {
+        String userInput = "/help";
+        Parser parser = new Parser();
+        ArrayList<University> universities = new ArrayList<>();
+        ArrayList<Module> modules = new ArrayList<>();
+        ArrayList<Module> puModules = new ArrayList<>();
+        ArrayList<Deadline> deadlines = new ArrayList<>();
+        Storage storage = new Storage();
+        DeadlineStorage deadlineStorage = new DeadlineStorage();
+        BudgetPlanner budgetPlanner = new BudgetPlanner();
+        assertTrue(parser.parseUserCommand(userInput, universities, modules, puModules, storage, deadlineStorage,
+                budgetPlanner, deadlines) instanceof HelpCommand);
+    }
+
+}
+
+
