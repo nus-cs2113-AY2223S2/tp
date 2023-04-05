e054d0e7d1bc11c994cdc219fb77f8f374cb348e
Koh_Jing_Jie_Marcus <88085589+Koh-Jing-Jie-Marcus@users.noreply.github.com>
2023-04-05 21:16:09 +0800

    Add test cases for ExamplesCommand

---
+++ src/test/java/seedu/commands/ExamplesCommandTest.java
@@ -0,0 +1,32 @@
+package seedu.commands;
+
+import org.junit.jupiter.api.Test;
+import seedu.exceptions.LifeTrackerException;
+import seedu.ui.ExampleUi;
+import seedu.ui.GeneralUi;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExamplesCommandTest {
+
+    @Test
+    void printExamples_validInput_expectException() throws LifeTrackerException {
+        ExampleUi ui = new ExampleUi();
+        ExamplesCommand examplesCommand = new ExamplesCommand("examples", " exercise");
+        assertDoesNotThrow(() -> {
+            examplesCommand.printExamples("exercise", ui);
+        });
+    }
+
+    @Test
+    void printExamples_invalidInput_expectException() throws LifeTrackerException {
+        ExampleUi ui = new ExampleUi();
+        ExamplesCommand examplesCommand = new ExamplesCommand("examples", " food");
+        LifeTrackerException thrown = assertThrows(LifeTrackerException.class, () -> {
+            examplesCommand.printExamples("food", ui);
+                });
+
+        String expectedErrorMessage = "You can only input exercise/meal for this command!";
+        assertEquals(expectedErrorMessage, thrown.getMessage());
+    }
+}
