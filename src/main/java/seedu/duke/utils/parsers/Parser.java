264e1bbf7de3fba85d8ffe103d8f64adc31cab39
firwer <firwer@gmail.com>
2023-04-05 18:46:46 +0800

    Add support for punctuations to add command

--- src/main/java/seedu/duke/utils/parsers/Parser.java
+++ src/main/java/seedu/duke/utils/parsers/Parser.java
@@ -4,7 +4,7 @@ import seedu.duke.objects.Inventory;
 import seedu.duke.utils.SessionManager;
 
 public abstract class Parser {
-    protected static final String ADD_REGEX = "n/([\\w\\s]+) upc/(\\d+) qty/(\\d+) p/(\\d+(?:\\.\\d+)?) " +
+    protected static final String ADD_REGEX = "n/([\\w\\s\\p{Punct}]+) upc/(\\d+) qty/(\\d+) p/(\\d+(?:\\.\\d+)?) " +
             "?(c/([\\w\\s]+))?";
     protected static final Integer NAME_INDEX = 1;
     protected static final Integer UPC_INDEX = 2;
--- src/test/java/seedu/duke/utils/StorageTest.java
+++ src/test/java/seedu/duke/utils/StorageTest.java
@@ -15,7 +15,8 @@ class StorageTest {
     public synchronized void testCSVReadWrite() {
         Inventory testInventory = new Inventory();
         Item testItem = new Item("testItem", "123456789012", 10, 10.0, LocalDateTime.now());
-        Item testItem2 = new Item("testItem2", "123456789013", 10, 10.0, LocalDateTime.now());
+        Item testItem2 = new Item("test Punctuation-items,here", "123456789013", 10, 10.0,
+                LocalDateTime.now());
         AddCommand addCommand = new AddCommand(testInventory, testItem);
         addCommand.run();
         addCommand = new AddCommand(testInventory, testItem2);
@@ -23,5 +24,9 @@ class StorageTest {
         Storage.writeCSV(testInventory);
         Assertions.assertTrue(Storage.readCSV(Types.SESSIONFILEPATH).getItemInventory().contains(testItem));
         Assertions.assertTrue(Storage.readCSV(Types.SESSIONFILEPATH).getItemInventory().contains(testItem2));
+        Assertions.assertEquals("testItem", Storage.readCSV(Types.SESSIONFILEPATH).getItemInventory().
+                get(0).getName());
+        Assertions.assertEquals("test Punctuation-items,here", Storage.readCSV(Types.SESSIONFILEPATH).
+                getItemInventory().get(1).getName());
     }
 }
