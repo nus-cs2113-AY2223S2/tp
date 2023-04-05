5e022f57d3eac8dfd5bda6312bc371547ca2a447
jeromeongithub <left4jerry@gmail.com>
2023-04-05 20:28:06 +0800

    Change from ArrayList to HashSet to avoid duplicate ids

--- src/main/java/seedu/todolist/logic/command/DeleteTaskCommand.java
+++ src/main/java/seedu/todolist/logic/command/DeleteTaskCommand.java
@@ -6,30 +6,33 @@ import seedu.todolist.exception.InvalidIdException;
 import seedu.todolist.task.TaskList;
 import seedu.todolist.ui.Ui;
 
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.StringJoiner;
+
+import static seedu.todolist.logic.ParserUtil.parseId;
 
 public class DeleteTaskCommand extends Command {
     public static final Flags[] EXPECTED_FLAGS = {Flags.COMMAND_DELETE};
 
     //@@author jeromeongithub
-    private ArrayList<Integer> idArrayList = new ArrayList<Integer>();
+    private HashSet<Integer> idHashSet = new HashSet<Integer>();
 
     public DeleteTaskCommand(HashMap<Flags, String> args) throws InvalidIdException {
         String idList = args.get(Flags.COMMAND_DELETE);
+        parseId(idList);
         String[] arrayOfIds = idList.split(" ");
         for (String idString : arrayOfIds) {
             int id = Integer.parseInt(idString);
             assert id >= 0 : "Invalid id contained in variable";
-            idArrayList.add(id);
+            idHashSet.add(id);
         }
     }
 
     @Override
     public void execute(TaskList taskList, Ui ui) throws InvalidIdException {
         StringJoiner stringJoiner = new StringJoiner(System.lineSeparator());
-        for (int id : idArrayList) {
+        for (int id : idHashSet) {
             String taskString = taskList.deleteTask(id);
             stringJoiner.add(taskString);
         }
