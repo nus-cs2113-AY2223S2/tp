12d942e0d0bfddd4658f2737201fa589523e8ce6
jeromeongithub <left4jerry@gmail.com>
2023-04-05 17:35:26 +0800

    Add author tag

--- src/main/java/seedu/todolist/logic/command/DeleteTaskCommand.java
+++ src/main/java/seedu/todolist/logic/command/DeleteTaskCommand.java
@@ -13,15 +13,15 @@ import java.util.StringJoiner;
 public class DeleteTaskCommand extends Command {
     public static final Flags[] EXPECTED_FLAGS = {Flags.COMMAND_DELETE};
 
-    // private int id;
+    //@@author jeromeongithub
     private ArrayList<Integer> idArrayList = new ArrayList<Integer>();
 
     public DeleteTaskCommand(HashMap<Flags, String> args) throws InvalidIdException {
         String idList = args.get(Flags.COMMAND_DELETE);
         String[] arrayOfIds = idList.split(" ");
-        for (String idString: arrayOfIds) {
+        for (String idString : arrayOfIds) {
             int id = Integer.parseInt(idString);
-            assert id >= 0: "Invalid id contained in variable";
+            assert id >= 0 : "Invalid id contained in variable";
             idArrayList.add(id);
         }
     }
