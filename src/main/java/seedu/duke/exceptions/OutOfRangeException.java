846a204c9638267f7501592bace3d28bf14a0979
ysl-28 <88529907+ysl-28@users.noreply.github.com>
2023-04-06 04:12:55 +0800

    Add javadoc comments

--- src/main/java/seedu/duke/exceptions/OutOfRangeException.java
+++ src/main/java/seedu/duke/exceptions/OutOfRangeException.java
@@ -5,8 +5,8 @@ import seedu.duke.utils.Ui;
 
 public class OutOfRangeException extends Exception{
     public void printOutOfRange() {
-        System.out.println(LINE);
+        System.out.println(Ui.LINE);
         System.out.println("The number you have entered exceeds the maximum limit of 99,999,999.");
-        System.out.println(LINE);
+        System.out.println(Ui.LINE);
     }
 }
