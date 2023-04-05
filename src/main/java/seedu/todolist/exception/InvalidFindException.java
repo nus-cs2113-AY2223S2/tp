84194934a4b1be88a06a23c73887b3395f7d1172
jeromeongithub <left4jerry@gmail.com>
2023-04-05 22:28:23 +0800

    Fix checkstyle issues

--- src/main/java/seedu/todolist/exception/InvalidFindException.java
+++ src/main/java/seedu/todolist/exception/InvalidFindException.java
@@ -2,6 +2,8 @@ package seedu.todolist.exception;
 
 import seedu.todolist.constants.Errors;
 
-public class InvalidFindException  extends ToDoListException {
-    public InvalidFindException() { super(Errors.INVALID_FIND_TAG); }
+public class InvalidFindException extends ToDoListException {
+    public InvalidFindException() {
+        super(Errors.INVALID_FIND_TAG);
+    }
 }
