48e494916434cc733e8b5258f9fda90fcce320b3
wuzhzn <dagreatbao@gmail.com>
2023-04-06 01:38:33 +0800

    Update junit test for venue class

--- src/test/java/seedu/duke/venue/VenueTest.java
+++ src/test/java/seedu/duke/venue/VenueTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class VenueTest {
     @Test
     public void testCreatingVenue() {
-        assertEquals("name1 loc1 7", new Venue(1,"name1","loc1", 7).toString());
-        assertEquals("    0", new Venue(2, " "," ", 0).toString());
+        assertEquals("1. name1 loc1 7", new Venue(1,"name1","loc1", 7).toString());
+        assertEquals("2.     0", new Venue(2, " "," ", 0).toString());
     }
 }
