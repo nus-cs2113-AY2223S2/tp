27861cdff2c8b917d20a8ef3813d282c3963434e
cleoncheng2000 <e0725796@u.nus.edu>
2023-04-07 04:45:10 +0800

    Update help message

--- src/main/java/functionalities/ui/Ui.java
+++ src/main/java/functionalities/ui/Ui.java
@@ -83,26 +83,31 @@ public class Ui {
 
     public static void showHelpMessage() {
         showLine();
-        System.out.println(" These are the following Sniff commands available:\n"
+        System.out.println("These are the following Sniff commands available:\n"
                 + "Add consultation appointment:\n"
-                + "consultation at/ANIMAL_TYPE an/ANIMAL_NAME on/OWNER_NAME cn/CONTACT_NUMBER cd/DATE ct/TIME\n"
+                + " consultation at/ANIMAL_TYPE an/ANIMAL_NAME on/OWNER_NAME cn/CONTACT_NUMBER cd/DATE ct/TIME\n"
                 + "Add vaccination appointment:\n"
-                + "vaccination at/ANIMAL_TYPE an/ANIMAL_NAME on/OWNER_NAME cn/CONTACT_NUMBER v/VACCINE_TYPE vd/DATE " +
-                        "vt/TIME\n"
+                + " vaccination at/ANIMAL_TYPE an/ANIMAL_NAME on/OWNER_NAME cn/CONTACT_NUMBER v/VACCINE_TYPE vd/DATE " +
+                "vt/TIME\n"
                 + "Add surgery appointment:\n"
-                + "surgery at/ANIMAL_TYPE an/ANIMAL_NAME on/OWNER_NAME cn/CONTACT_NUMBER sd/START_DATE " +
-                                "st/START_TIME ed/END_DATE et/END_TIME p/PRIORITY_LEVEL\n"
+                + " surgery at/ANIMAL_TYPE an/ANIMAL_NAME on/OWNER_NAME cn/CONTACT_NUMBER sd/START_DATE " +
+                "st/START_TIME ed/END_DATE et/END_TIME p/PRIORITY_LEVEL\n"
                 + "Listing all appointments:\n"
-                + "list\n"
+                + " list\n"
                 + "Removing an appointment:\n"
-                + "remove uid/UID\n"
-                + "Finding an appointment:\n"
-                + "find a/ANIMAL_TYPE or find t/APPOINTMENT_TYPE or find uid/UID\n"
-                + "Marking/Unmarking an appointment:\n"
-                + "mark uid/UID or unmark uid/UID\n"
+                + " remove uid/UID\n"
+                + "Finding an appointment by animal type, appointment type or uid:\n"
+                + " find a/ANIMAL_TYPE\n"
+                + " find t/APPOINTMENT_TYPE \n"
+                + " find uid/UID\n"
+                + "Marking or Unmarking an appointment:\n"
+                + " mark uid/UID\n"
+                + " unmark uid/UID\n"
                 + "Exiting the program:\n"
-                + "bye\n");
+                + " bye\n"
+                + "Additional notes:\n"
+                + " 1. DATES and TIMES format are in (YYYY-MM-DD) and (HH:MM) respectively\n"
+                + " 2. PRIORITY_LEVEL format is in (L, M, H)");
         showLine();
     }
-
 }
