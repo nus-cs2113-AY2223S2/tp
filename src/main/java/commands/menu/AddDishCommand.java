c9b0dcf36bf97869651752d51b843a26a116d525
Zeno-Zr <zenozr00@gmail.com>
2023-04-07 02:56:42 +0800

    Refactor addDishCommand and deleteDishCommand
    * Drop the "Command" for the above two methods which were called in
    AddDishCommand and DeleteDishCommand

--- src/main/java/commands/menu/AddDishCommand.java
+++ src/main/java/commands/menu/AddDishCommand.java
@@ -22,7 +22,7 @@ public class AddDishCommand extends Command {
 
     @Override
     public void execute(TextUi ui) {
-        DishManager.addDishCommand(this.dishName, this.dishPrice, this.ingredientsList, ui);
+        DishManager.addDish(this.dishName, this.dishPrice, this.ingredientsList, ui);
     }
 
     @Override
