94e9280f773ff36356473f106c4c8c58702c6421
Mitch Malvin <e0773911@u.nus.edu>
2023-04-06 22:58:16 +0800

    chore : cleanup comments

--- src/main/java/model/CardUUID.java
+++ src/main/java/model/CardUUID.java
@@ -3,19 +3,8 @@ package model;
 import java.util.UUID;
 
 public class CardUUID extends InkaUUID {
-//    UUID uuid;
 
     public CardUUID(UUID uuid) {
-//        this.uuid = uuid;
         super(uuid);
     }
-
-//    public boolean equals(CardUUID cardUUID) {
-//        return this.uuid.equals(cardUUID.uuid);
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return this.uuid.toString();
-//    }
 }
