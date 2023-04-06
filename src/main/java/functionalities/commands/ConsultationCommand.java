c1b99bd1a586662df3f7504fa4748fe853a29c2b
cleoncheng2000 <e0725796@u.nus.edu>
2023-04-06 16:00:56 +0800

    Update consultation command typo

--- src/main/java/functionalities/commands/ConsultationCommand.java
+++ src/main/java/functionalities/commands/ConsultationCommand.java
@@ -9,14 +9,14 @@ import functionalities.ui.Ui;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-public class ConsulationCommand extends Command {
+public class ConsultationCommand extends Command {
 
     private final Animal animal;
     private final Owner owner;
     private final LocalDate date;
     private final LocalTime time;
 
-    public ConsulationCommand(String animalType, String animalName,
+    public ConsultationCommand(String animalType, String animalName,
                               String ownerName, String contactNumber, LocalDate date,
                               LocalTime time) throws SniffException {
         this.animal = new Animal(animalType, animalName);
