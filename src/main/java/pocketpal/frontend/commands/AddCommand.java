31cfc69b03e5fb8e46243a408aca138a3f0b33ec
kaceycsn <119840829+kaceycsn@users.noreply.github.com>
2023-04-06 23:09:49 +0800

    Update coding standard
    Use convertStringToCategory() method to shorten code

--- src/main/java/pocketpal/frontend/commands/AddCommand.java
+++ src/main/java/pocketpal/frontend/commands/AddCommand.java
@@ -5,8 +5,6 @@ import pocketpal.backend.Backend;
 import pocketpal.data.entry.Category;
 import pocketpal.data.entry.Entry;
 import pocketpal.data.parsing.EntryParser;
-import pocketpal.frontend.constants.EntryConstants;
-import pocketpal.frontend.constants.MessageConstants;
 import pocketpal.frontend.exceptions.InvalidCategoryException;
 import pocketpal.communication.Request;
 import pocketpal.communication.RequestMethod;
@@ -15,8 +13,9 @@ import pocketpal.communication.ResponseStatus;
 import pocketpal.frontend.ui.UI;
 import pocketpal.frontend.util.StringUtil;
 
-import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import static pocketpal.frontend.util.CategoryUtil.convertStringToCategory;
 
 /**
  * Represents the add feature in PocketPal. Users may provide
@@ -32,52 +31,13 @@ public class AddCommand extends Command {
      * AddCommand constructor which initialises entryObj to be added
      *
      * @param description Description of the entry
-     * @param category    Category which entry belongs to
+     * @param categoryStr    Category which entry belongs to
      * @param amount      Price of entry
      * @throws InvalidCategoryException If input category is invalid
      */
-    public AddCommand(String description, double amount, String category) throws InvalidCategoryException {
-        switch (StringUtil.toTitleCase(category)) {
-        case EntryConstants.CLOTHING:
-            this.entryObj = new Entry(description, amount, Category.CLOTHING);
-            break;
-
-        case EntryConstants.ENTERTAINMENT:
-            this.entryObj = new Entry(description, amount, Category.ENTERTAINMENT);
-            break;
-
-        case EntryConstants.FOOD:
-            this.entryObj = new Entry(description, amount, Category.FOOD);
-            break;
-
-        case EntryConstants.INCOME:
-            this.entryObj = new Entry(description, amount, Category.INCOME);
-            break;
-
-        case EntryConstants.MEDICAL:
-            this.entryObj = new Entry(description, amount, Category.MEDICAL);
-            break;
-
-        case EntryConstants.OTHERS:
-            this.entryObj = new Entry(description, amount, Category.OTHERS);
-            break;
-
-        case EntryConstants.PERSONAL:
-            this.entryObj = new Entry(description, amount, Category.PERSONAL);
-            break;
-
-        case EntryConstants.TRANSPORTATION:
-            this.entryObj = new Entry(description, amount, Category.TRANSPORTATION);
-            break;
-
-        case EntryConstants.UTILITIES:
-            this.entryObj = new Entry(description, amount, Category.UTILITIES);
-            break;
-
-        default:
-            logger.log(Level.WARNING, "Input category is invalid");
-            throw new InvalidCategoryException(MessageConstants.MESSAGE_INVALID_CATEGORY);
-        }
+    public AddCommand(String description, double amount, String categoryStr) throws InvalidCategoryException {
+        Category category = convertStringToCategory(StringUtil.toTitleCase(categoryStr));
+        this.entryObj = new Entry(description, amount, category);
     }
 
     /**
