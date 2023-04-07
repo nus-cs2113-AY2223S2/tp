5340bd174b9f98cb7e991289b89b6cb76800677c
Hong Lin Shang <e0774890@u.nus.edu>
2023-04-07 22:36:02 +0800

    Fix Checkstyle

--- src/test/java/seedu/apollo/command/task/SpecificHelpCommandTest.java
+++ src/test/java/seedu/apollo/command/task/SpecificHelpCommandTest.java
@@ -33,7 +33,6 @@ import java.rmi.UnexpectedException;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class SpecificHelpCommandTest {
     TaskList taskList = new TaskList();
