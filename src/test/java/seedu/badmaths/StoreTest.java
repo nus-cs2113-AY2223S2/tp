1bc9e92d2c4d214816c60e14d07f15f8320119b7
Wilson Lee Jun Wei <leejunwei2000@gmail.com>
2023-04-07 16:49:36 +0800

    Rectify issues on StoreTest.java

--- src/test/java/seedu/badmaths/StoreTest.java
+++ src/test/java/seedu/badmaths/StoreTest.java
@@ -8,34 +8,37 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.ArrayList;
 import seedu.badmaths.note.Note;
 import seedu.badmaths.note.NotesList;
-// import java.nio.file.Path;
-// import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Files;
+import java.io.IOException;
 
 public class StoreTest {
-    /*
-    Path tempFile = Files.createTempFile("temp", ".txt");
-    String fileContents = "HIGH\tN\t0\tThis is a test note\nMEDIUM\tY\t2\tThi is another test note\n";
-    Files.writeString(tempFile, fileContents);
-    Files.delete(tempFile);
-    */
 
     @Test
-    void checkIsInvalidToDo() {
+    void checkIsInvalidToDo() throws IOException {
+        Path temporaryFile = Files.createTempFile("TempFile", ".txt");
+        String contentsFile = "HIGH\tN\t0\tThis is a test \nMEDIUM\tY\t2\tThi is another test\n";
+        Files.writeString(temporaryFile, contentsFile);
         String todo = "82738232";
         ArrayList<Note> notesArray = new ArrayList<>();
         NotesList notes = new NotesList(notesArray);
         Store storeTest = new Store(notes, todo);
         boolean test = storeTest.isInvalidTodo(todo);
         assertEquals(false, test);
+        Files.delete(temporaryFile);
     }
 
     @Test
-    void storeValidInputCommand() {
+    void storeValidInputCommand() throws IOException {
+        Path temporaryFile = Files.createTempFile("TempFile", ".txt");
+        String contentsFile = "HIGH\tN\t0\tThis is a test \nMEDIUM\tY\t2\tThi is another test\n";
+        Files.writeString(temporaryFile, contentsFile);
         ArrayList<Note> notesArray = new ArrayList<>();
         String toDo = "index";
         NotesList notes = new NotesList(notesArray);
         Store storeTest = new Store(notes, toDo);
         storeTest.storeNotes();
         assertEquals(1, notes.getSize()); // (expected, actual)
+        Files.delete(temporaryFile);
     }
 }
