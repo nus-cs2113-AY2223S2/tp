514fe9fba1fba900415e8d76965acbd906eb29d6
Li Mingyuan <e0773670@u.nus.edu>
2023-04-07 17:59:11 +0800

    Improve code formatting

--- src/main/java/seedu/moneymind/command/SearchCommand.java
+++ src/main/java/seedu/moneymind/command/SearchCommand.java
@@ -168,20 +168,20 @@ public class SearchCommand implements Command {
         for (int i = 0; i <= firstString.length(); i++) {
             int lastValue = i;
             for (int j = 0; j <= secondString.length(); j++) {
-                if (i == 0)
+                if (i == 0) {
                     costs[j] = j;
-                else {
-                    if (j > 0) {
-                        int newValue = costs[j - 1];
-                        if (firstString.charAt(i - 1) != secondString.charAt(j - 1))
-                            newValue = Math.min(Math.min(newValue, lastValue), costs[j]) + 1;
-                        costs[j - 1] = lastValue;
-                        lastValue = newValue;
+                } else if (j > 0) {
+                    int newValue = costs[j - 1];
+                    if (firstString.charAt(i - 1) != secondString.charAt(j - 1)) {
+                        newValue = Math.min(Math.min(newValue, lastValue), costs[j]) + 1;
                     }
+                    costs[j - 1] = lastValue;
+                    lastValue = newValue;
                 }
             }
-            if (i > 0)
+            if (i > 0) {
                 costs[secondString.length()] = lastValue;
+            }
         }
         return costs[secondString.length()];
     }
