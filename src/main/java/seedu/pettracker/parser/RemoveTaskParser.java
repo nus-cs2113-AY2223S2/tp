e020a18283a9551d76fb1e760d1cfc4b2fa408b9
SpawnageLoong <richardloongcj@gmail.com>
2023-04-07 22:46:07 +0800

    Remove unreachable catch statements

--- src/main/java/seedu/pettracker/parser/RemoveTaskParser.java
+++ src/main/java/seedu/pettracker/parser/RemoveTaskParser.java
@@ -6,24 +6,18 @@ import seedu.pettracker.exceptions.IllegalArgException;
 
 public class RemoveTaskParser implements ArgParser<RemoveTaskCommand>{
     final String EMPTY_ARG_MESSAGE = "This command requires arguments.";
-    final String INVALID_ARG_FORMAT_MESSAGE = "Invalid argument format. Please enter the arguments in the " +
-            "following format: TASK_NUMBER.";
     final String INVALID_TASK_NUMBER_MESSAGE = "Invalid task number. Please enter a valid task number.";
     @Override
     public RemoveTaskCommand parse(String commandArgs) throws IllegalArgException {
         if (commandArgs.isEmpty()) {
             throw new EmptyArgException(EMPTY_ARG_MESSAGE);
         }
+        int taskNum;
         try {
-            int taskNum;
-            try {
-                taskNum = Integer.parseInt(commandArgs);
-            } catch (NumberFormatException e) {
-                throw new IllegalArgException(INVALID_TASK_NUMBER_MESSAGE);
-            }
-            return new RemoveTaskCommand(taskNum);
-        } catch (ArrayIndexOutOfBoundsException e) {
-            throw new IllegalArgException(INVALID_ARG_FORMAT_MESSAGE);
+            taskNum = Integer.parseInt(commandArgs);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgException(INVALID_TASK_NUMBER_MESSAGE);
         }
+        return new RemoveTaskCommand(taskNum);
     }
 }
