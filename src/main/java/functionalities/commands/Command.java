2df7cdb2a4a63c66dd816ab748c0d963890492d7
cleoncheng2000 <e0725796@u.nus.edu>
2023-04-09 02:49:55 +0800

    Add javadoc for command class

--- src/main/java/functionalities/commands/Command.java
+++ src/main/java/functionalities/commands/Command.java
@@ -3,6 +3,10 @@ package functionalities.commands;
 import exception.SniffException;
 import functionalities.SniffTasks;
 
+/**
+ * The Command class represents a command to be executed.
+ * This class is abstract and provides methods executing the command and checking the status of the program.
+ */
 public abstract class Command {
     public boolean isExit;
 
