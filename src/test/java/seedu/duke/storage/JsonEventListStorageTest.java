be5fdb6e188e55370ba9ee30ca294cec15b67cdd
MatthewLiuZJ <matthew.liu.zhenjie@gmail.com>
2023-04-08 17:52:04 +0800

    Fix Checkstyle Errors

--- src/test/java/seedu/duke/storage/JsonEventListStorageTest.java
+++ src/test/java/seedu/duke/storage/JsonEventListStorageTest.java
@@ -1,80 +1,79 @@
- package seedu.duke.storage;
+package seedu.duke.storage;
 
- import org.junit.jupiter.api.Order;
- import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 
- import seedu.duke.EventList;
- import seedu.duke.NPExceptions;
- import seedu.duke.User;
+import seedu.duke.EventList;
+import seedu.duke.NPExceptions;
+import seedu.duke.User;
 
- import java.io.File;
- import java.io.FileNotFoundException;
+import java.io.File;
+import java.io.FileNotFoundException;
 
- import static seedu.duke.UserUtility.getUser;
+import static seedu.duke.UserUtility.getUser;
 
- class JsonEventListStorageTest {
+class JsonEventListStorageTest {
+    private static final String fileLocation = System.getProperty("user.dir") + "/save.json";
+    File saveFile = new File(fileLocation);
+    private final JsonEventListStorage jsonEventListStorage = new JsonEventListStorage();
+    @Test
+    @Order(1)
+    public void emptyLoad(){ //check that it can load when there isn't an instance of save.json in the path.
+        saveFile.delete();
+        EventList testList = new EventList(jsonEventListStorage.loadEvents());
+        jsonEventListStorage.loadEvents();
+        assert((testList.getSize()==0) && (!saveFile.exists()));
+    }
+    @Test
+    @Order(2)
+    public void saveEvents() throws NPExceptions{ //check that storage class saves a file
+        EventList testList = new EventList();
+        testList.addEvent("testing", "10:00", "2023/03/20", "11:00", "2023/03/20");
+        testList.addEvent("testing2","03:24", "2023/03/23", "08:50", "2023/03/23");
+        jsonEventListStorage.saveToFile(testList);
+        assert(saveFile.exists());
+        saveFile.delete();
+    }
 
-     private static final String fileLocation = System.getProperty("user.dir") + "/save.json";
-     File saveFile = new File(fileLocation);
-     private final JsonEventListStorage jsonEventListStorage = new JsonEventListStorage();
-     @Test
-     @Order(1)
-     public void emptyLoad(){ //check that it can load when there isn't an instance of save.json in the path.
-         saveFile.delete();
-         EventList testList = new EventList(jsonEventListStorage.loadEvents());
-         jsonEventListStorage.loadEvents();
-         assert((testList.getSize()==0) && (!saveFile.exists()));
-     }
-     @Test
-     @Order(2)
-     public void saveEvents() throws NPExceptions{ //check that storage class saves a file
-         EventList testList = new EventList();
-         testList.addEvent("testing", "10:00", "2023/03/20", "11:00", "2023/03/20");
-         testList.addEvent("testing2","03:24", "2023/03/23", "08:50", "2023/03/23");
-         jsonEventListStorage.saveToFile(testList);
-         assert(saveFile.exists());
-         saveFile.delete();
-     }
-
-     @Test
-     @Order(3)
-     public void loadEvents() throws FileNotFoundException, NPExceptions {
-         //checks that storage EventList from deserialized save.json matches original EventList data
-         EventList original = new EventList();
-         original.addEvent("testing", "10:00", "2023/03/20",
-                 "10:00", "2023/03/20");
-         original.addEvent("testing2","03:24", "2023/03/23",
-                 "08:50", "2023/03/23");
-         jsonEventListStorage.saveToFile(original);
-         EventList testListCheck = new EventList();
-         testListCheck.addEvent("testing", "10:00", "2023/03/20",
-                 "10:00", "2023/03/20");
-         testListCheck.addEvent("testing2","03:24", "2023/03/23",
-                 "08:50", "2023/03/23");
-         EventList testList = new EventList(jsonEventListStorage.loadEvents());
-         String a = testListCheck.getFullList().toString();
-         String b = testList.getFullList().toString();
-         assert(a.equals(b));
-         saveFile.delete();
-     }
-     @Test
-     @Order(4)
-     public void updateEvents() throws NPExceptions{ //Check that data can be updated and matches what was updated.
-         EventList original = new EventList();
-         User user = getUser();
-         user.setSemester(2);
-         original.addEvent("testing", "10:00", "2023/03/20",
-                 "10:00", "2023/03/20");
-         original.addEvent("testing2","03:24", "2023/03/23",
-                 "08:50", "2023/03/23");
-         jsonEventListStorage.saveToFile(original);
-         EventList edited = new EventList(jsonEventListStorage.loadEvents());
-         edited.addEvent("edit new event", "13:00", "2023/03/21", "19:00", "2023/03/21");
-         jsonEventListStorage.saveToFile(edited);
-         EventList savedEvent =  new EventList(jsonEventListStorage.loadEvents());
-         String a = edited.getFullList().toString();
-         String b = savedEvent.getFullList().toString();
-         assert(a.equals(b));
-         saveFile.delete();
-     }
- }
+    @Test
+    @Order(3)
+    public void loadEvents() throws FileNotFoundException, NPExceptions {
+        //checks that storage EventList from deserialized save.json matches original EventList data
+        EventList original = new EventList();
+        original.addEvent("testing", "10:00", "2023/03/20",
+                "10:00", "2023/03/20");
+        original.addEvent("testing2","03:24", "2023/03/23",
+                "08:50", "2023/03/23");
+        jsonEventListStorage.saveToFile(original);
+        EventList testListCheck = new EventList();
+        testListCheck.addEvent("testing", "10:00", "2023/03/20",
+                "10:00", "2023/03/20");
+        testListCheck.addEvent("testing2","03:24", "2023/03/23",
+                "08:50", "2023/03/23");
+        EventList testList = new EventList(jsonEventListStorage.loadEvents());
+        String a = testListCheck.getFullList().toString();
+        String b = testList.getFullList().toString();
+        assert(a.equals(b));
+        saveFile.delete();
+    }
+    @Test
+    @Order(4)
+    public void updateEvents() throws NPExceptions{ //Check that data can be updated and matches what was updated.
+        EventList original = new EventList();
+        User user = getUser();
+        user.setSemester(2);
+        original.addEvent("testing", "10:00", "2023/03/20",
+                "10:00", "2023/03/20");
+        original.addEvent("testing2","03:24", "2023/03/23",
+                "08:50", "2023/03/23");
+        jsonEventListStorage.saveToFile(original);
+        EventList edited = new EventList(jsonEventListStorage.loadEvents());
+        edited.addEvent("edit new event", "13:00", "2023/03/21", "19:00", "2023/03/21");
+        jsonEventListStorage.saveToFile(edited);
+        EventList savedEvent =  new EventList(jsonEventListStorage.loadEvents());
+        String a = edited.getFullList().toString();
+        String b = savedEvent.getFullList().toString();
+        assert(a.equals(b));
+        saveFile.delete();
+    }
+}
