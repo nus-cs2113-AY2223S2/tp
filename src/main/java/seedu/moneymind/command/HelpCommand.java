08d4868b0596c6300e6610f79d606c34735bf2c7
alexgoexercise <alexzhaolixiuqi@gmail.com>
2023-04-09 03:38:48 +0800

    Create a summary command for better user experience

--- src/main/java/seedu/moneymind/command/HelpCommand.java
+++ src/main/java/seedu/moneymind/command/HelpCommand.java
@@ -4,13 +4,14 @@ import seedu.moneymind.ui.Ui;
 
 import static seedu.moneymind.string.Strings.INTRODUCTION_HELP_COMMAND;
 import static seedu.moneymind.string.Strings.HELP_INSTRUCTION;
+import static seedu.moneymind.string.Strings.SUMMARY_INSTRUCTION;
 import static seedu.moneymind.string.Strings.CATEGORY_INSTRUCTION;
 import static seedu.moneymind.string.Strings.EVENT_INSTRUCTION;
 import static seedu.moneymind.string.Strings.VIEW_INSTRUCTION;
+import static seedu.moneymind.string.Strings.EDIT_INSTRUCTION;
 import static seedu.moneymind.string.Strings.DELETE_INSTRUCTION;
 import static seedu.moneymind.string.Strings.SEARCH_INSTRUCTION;
 import static seedu.moneymind.string.Strings.BYE_INSTRUCTION;
-import static seedu.moneymind.string.Strings.EDIT_INSTRUCTION;
 
 /**
  * Represents the help command.
@@ -21,6 +22,7 @@ public class HelpCommand implements Command {
     public void execute(Ui ui) {
         System.out.println(INTRODUCTION_HELP_COMMAND);
         System.out.println(HELP_INSTRUCTION);
+        System.out.println(SUMMARY_INSTRUCTION);
         System.out.println(CATEGORY_INSTRUCTION);
         System.out.println(EVENT_INSTRUCTION);
         System.out.println(VIEW_INSTRUCTION);
--- src/main/java/seedu/moneymind/string/Strings.java
+++ src/main/java/seedu/moneymind/string/Strings.java
@@ -22,41 +22,57 @@ public class Strings {
             "                           __/ |                     \n" +
             "                          |___/                      \n";
     public static final String GREETING = "Welcome to Moneymind\n" + LOGO + "How may I help you?";
+    public static final String OFFER_HELP = "Type 'help' to see the details of all the commands.";
+    public static final String OFFER_HELP_FOR_COMMAND = "Type 'summary' to see the summary of all the commands you can use.\n" +
+            "Type 'help' to see the details of all the commands.";
     public static final String ERROR = "OOPS!!! I'm sorry, but I don't know what that means :-(";
     public static final String DATE_FORMAT = "dd/MM/yyyy HH:mm";
     public static final String DATA_FILE = "EventList.txt";
-    public static final String BUDGET_LIMIT_MESSAGE = "The budget limit is 999999999$, give a smaller budget";
-    public static final String INDEX_LIMIT_MESSAGE = "The limit for index is 999999999, give a smaller index";
-    public static final String EXPENSE_LIMIT_MESSAGE = "The expense limit is 999999999$, give a smaller expense";
-    public static final String NO_CATEGORY_MESSAGE = "Category does not exist";
+    public static final String BUDGET_LIMIT_MESSAGE = "The budget limit is 999999999$, please try to give a smaller budget";
+    public static final String INDEX_LIMIT_MESSAGE = "The limit for index is 999999999, please try to give a smaller index";
+    public static final String EXPENSE_LIMIT_MESSAGE = "The expense limit is 999999999$, please try to give a smaller expense";
+    public static final String NO_CATEGORY_MESSAGE = "Sorry, the category you are looking for does not exist";
     public static final String DOT = ".";
     public static final String NO_CATEGORIES_TO_VIEW = "There are no categories to view";
-    public static final String NO_SEARCH_RESULTS = "No matching search results.";
+    public static final String NO_SEARCH_RESULTS = "Sorry, there is matching search results.";
     public static final String NULL_CATEGORY_ASSERTION = "Category name should not be null";
     public static final String NULL_CATEGORY_LIST_ASSERTION = "Category list should not be null";
     public static final String INTRODUCTION_HELP_COMMAND = "Here are the commands you can use:";
-    public static final String HELP_INSTRUCTION = "1. help - show instructions on how to use the app\n"
+    public static final String SUMMARY = "summary";
+    public static final String SHOW_SUMMARY = "Here are the commands you can use:\n";
+    public static final String SUMMARY_LIST = SHOW_SUMMARY + "1. help\n" +
+            "2. summary\n" +
+            "3. category\n" +
+            "4. event\n" +
+            "5. view\n" +
+            "6. edit\n" +
+            "7. delete\n" +
+            "8. search\n" +
+            "9. bye\n";
+    public static final String HELP_INSTRUCTION = "1. help - show detailed instructions on how to use the app\n"
             + "Format: help\n" + "Example: help\n";
-    public static final String CATEGORY_INSTRUCTION = "2. category - add a category to your list\n" +
+    public static final String SUMMARY_INSTRUCTION = "2. summary - show a summary of the commands that you can use\n"
+            + "Format: summary\n" + "Example: summary\n";
+    public static final String CATEGORY_INSTRUCTION = "3. category - add a category to your list\n" +
             "Format: category <name> [(optional) b/<budget number>]\n" + "Example: category food b/2000\n";
-    public static final String EVENT_INSTRUCTION = "3. event - add an event to a category\n" +
+    public static final String EVENT_INSTRUCTION = "4. event - add an event to a category\n" +
             "Format: event <name> e/<expense number> [(optional) t/<time>]" +
             "Example: event lunch e/10 t/01/01/2020 12:00\n" +
             "(time is optional and the format is dd/mm/yyyy hh:mm)\n";
     public static final String VIEW_INSTRUCTION =
-            "4. view - view all the events in a category or all the categories\n" +
+            "5. view - view all the events in a category or all the categories\n" +
             "You can view all the categories by entering view without any category name\n" +
             "Format: view <category name>\n" + "Example: view food\n";
-    public static final String EDIT_INSTRUCTION = "5. edit - edit the expense for an event\n" +
+    public static final String EDIT_INSTRUCTION = "6. edit - edit the expense for an event\n" +
             "edit c/<category name> e/<event index>\n" +
             "Example: edit c/food e/1\n";
-    public static final String DELETE_INSTRUCTION = "6. delete - delete an event or a category\n" +
+    public static final String DELETE_INSTRUCTION = "7. delete - delete an event or a category\n" +
             "delete c/<category name> [(optional) e/<event index>]\n" +
             "Example: delete c/food e/1\n" + "Example: delete c/food\n";
-    public static final String SEARCH_INSTRUCTION = "7. search - search for matching events and categories\n" +
+    public static final String SEARCH_INSTRUCTION = "8. search - search for matching events and categories\n" +
             "Format: search <keyword>\n" +
             "Example: search bill\n";
-    public static final String BYE_INSTRUCTION = "8. bye - exit the app\n" + "Format: bye\n" + "Example: bye\n";
+    public static final String BYE_INSTRUCTION = "9. bye - exit the app\n" + "Format: bye\n" + "Example: bye\n";
     public static final String SELECTING_CATEGORY_MESSAGE = "Please select the category you want to add the event to:";
     public static final String GO_BACK_MESSAGE = "Please try again or enter back to go back to the main program";
     public static final String EDIT_EXPENSE_LIMIT_MESSAGE = "The expense limit is 999999999$\n" + GO_BACK_MESSAGE;
@@ -106,6 +122,7 @@ public class Strings {
     public static final String HELP = "help";
     public static final String NO_DESCRIPTION_FOR_BYE = "Bye command should not have any description";
     public static final String NO_DESCRIPTION_FOR_HELP = "Help command should not have any description";
+    public static final String NO_DESCRIPTION_FOR_SUMMARY = "Summary command should not have any description";
     public static final String POSITIVE_INTEGER_FOR_EVENT_INDEX = "Please give a positive integer for event index";
     public static final String NON_NEGATIVE_INTEGER_FOR_EXPENSE = "Please give a non-negative integer for expense";
     public static final String EMPTY_DESCRIPTION_FOR_EDIT = "OOPS!!! The description of an edit cannot be empty.";
