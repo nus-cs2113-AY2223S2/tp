c55ef4aa771602773c2d70b07102c817d6ad516c
cleoncheng2000 <e0725796@u.nus.edu>
2023-04-09 02:50:13 +0800

    Add javadoc for consultation, help, surgery and vaccination commands

--- src/main/java/functionalities/commands/SurgeryCommand.java
+++ src/main/java/functionalities/commands/SurgeryCommand.java
@@ -8,6 +8,10 @@ import functionalities.SniffTasks;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+/**
+ * The SurgeryCommand class represents an add surgery command to be executed.
+ * This class is extends the abstract Command class and contains methods to executing the add surgery command.
+ */
 public class SurgeryCommand extends Command {
 
     private final Animal animal;
@@ -18,6 +22,20 @@ public class SurgeryCommand extends Command {
     private final LocalDate endDate;
     private final LocalTime endTime;
 
+    /**
+     * Constructs a new SurgeryCommand object with the specified parameters.
+     *
+     * @param animalType    the type of the animal for the surgery appointment.
+     * @param animalName    the name of the animal for the surgery appointment.
+     * @param ownerName     the name of the owner.
+     * @param contactNumber the contact number of the owner.
+     * @param startDate     the start date of the surgery appointment.
+     * @param startTime     the start time of the surgery appointment.
+     * @param endDate       the end date of the surgery appointment.
+     * @param endTime       the end time of the surgery appointment.
+     * @param priority      the priority level of the surgery appointment.
+     * @throws SniffException if any details are of the wrong format.
+     */
     public SurgeryCommand(String animalType, String animalName, String ownerName,
                           String contactNumber, LocalDate startDate, LocalTime startTime,
                           LocalDate endDate, LocalTime endTime, String priority) throws SniffException {
@@ -30,6 +48,11 @@ public class SurgeryCommand extends Command {
         this.endTime = endTime;
     }
 
+    /**
+     * Executes add surgery method located in SniffTasks class
+     *
+     * @param tasks The SniffTasks Class
+     */
     @Override
     public void executeCommand(SniffTasks tasks) throws SniffException {
         tasks.addSurgery(animal, owner, priority, startDate, startTime, endDate, endTime);
