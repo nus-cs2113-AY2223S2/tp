1fe5c4cab4c8e071153907269800016af81c8bfc
11-Navya <e0913130@u.nus.edu>
2023-04-09 00:57:00 +0800

    Fixed indentation

--- src/main/java/functionalities/commands/EditConsultationCommand.java
+++ src/main/java/functionalities/commands/EditConsultationCommand.java
@@ -9,27 +9,27 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class EditConsultationCommand extends Command {
-       public String uid;
+    public String uid;
 
-        private final Animal animal;
-        private final Owner owner;
-        private final LocalDate date;
-        private final LocalTime time;
+    private final Animal animal;
+    private final Owner owner;
+    private final LocalDate date;
+    private final LocalTime time;
 
 
-        public EditConsultationCommand(String uid,String animalType, String animalName,
-                                       String ownerName, String contactNumber, LocalDate date,
-                                       LocalTime time) throws SniffException {
-            this.uid = uid;
-            this.animal = new Animal(animalType, animalName);
-            this.owner = new Owner(ownerName, contactNumber);
-            this.date = date;
-            this.time = time;
-        }
-
-        @Override
-        public void executeCommand(SniffTasks tasks) throws SniffException {
-            //tasks.removeAppointment(uid);
-            tasks.editConsultation(uid,animal, owner, date, time);
-        }
+    public EditConsultationCommand(String uid,String animalType, String animalName,
+                                   String ownerName, String contactNumber, LocalDate date,
+                                   LocalTime time) throws SniffException {
+        this.uid = uid;
+        this.animal = new Animal(animalType, animalName);
+        this.owner = new Owner(ownerName, contactNumber);
+        this.date = date;
+        this.time = time;
     }
+
+    @Override
+    public void executeCommand(SniffTasks tasks) throws SniffException {
+        //tasks.removeAppointment(uid);
+        tasks.editConsultation(uid,animal, owner, date, time);
+    }
+}
