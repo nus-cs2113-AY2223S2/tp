d875fc5105814b47612e85dcda59b3dd7d3b36be
itszhixuan <leezhixuan001@gmail.com>
2023-04-08 14:57:32 +0800

    Replaced with 'p/' for tests

--- src/test/java/seedu/parser/ParserTest.java
+++ src/test/java/seedu/parser/ParserTest.java
@@ -30,49 +30,49 @@ public class ParserTest {
 
     @Test
     void parseAcademicCommand() {
-        String inputString = "academic d/2000-01-01 a/200.0 s/Tuition";
+        String inputString = "academic d/2000-01-01 a/200.0 p/Tuition";
         Command finalCommand = MainInputParser.parseInputs(inputString);
         assertEquals(finalCommand.getClass().getName(), "seedu.commands.AcademicExpenditureCommand");
     }
 
     @Test
     void parseAccommodationCommand() {
-        String inputString = "accommodation d/2000-01-01 a/500.0 s/Rent";
+        String inputString = "accommodation d/2000-01-01 a/500.0 p/Rent";
         Command finalCommand = MainInputParser.parseInputs(inputString);
         assertEquals(finalCommand.getClass().getName(), "seedu.commands.AccommodationExpenditureCommand");
     }
 
     @Test
     void parseEntertainmentCommand() {
-        String inputString = "entertainment d/2000-01-01 a/45.90 s/Singing";
+        String inputString = "entertainment d/2000-01-01 a/45.90 p/Singing";
         Command finalCommand = MainInputParser.parseInputs(inputString);
         assertEquals(finalCommand.getClass().getName(), "seedu.commands.EntertainmentExpenditureCommand");
     }
 
     @Test
     void parseFoodCommand() {
-        String inputString = "food d/2000-01-01 a/9.90 s/Lunch";
+        String inputString = "food d/2000-01-01 a/9.90 p/Lunch";
         Command finalCommand = MainInputParser.parseInputs(inputString);
         assertEquals(finalCommand.getClass().getName(), "seedu.commands.FoodExpenditureCommand");
     }
 
     @Test
     void parseOthersCommand() {
-        String inputString = "other d/2000-01-01 a/2.19 s/Miscellaneous";
+        String inputString = "other d/2000-01-01 a/2.19 p/Miscellaneous";
         Command finalCommand = MainInputParser.parseInputs(inputString);
         assertEquals(finalCommand.getClass().getName(), "seedu.commands.OtherExpenditureCommand");
     }
 
     @Test
     void parseTransportCommand() {
-        String inputString = "transport d/2000-01-01 a/2.00 s/MRT fair";
+        String inputString = "transport d/2000-01-01 a/2.00 p/MRT fair";
         Command finalCommand = MainInputParser.parseInputs(inputString);
         assertEquals(finalCommand.getClass().getName(), "seedu.commands.TransportExpenditureCommand");
     }
 
     @Test
     void parseTuitionCommand() {
-        String inputString = "tuition d/2000-01-01 a/30.00 s/Chinese lessons";
+        String inputString = "tuition d/2000-01-01 a/30.00 p/Chinese lessons";
         Command finalCommand = MainInputParser.parseInputs(inputString);
         assertEquals(finalCommand.getClass().getName(), "seedu.commands.TuitionExpenditureCommand");
     }
@@ -86,23 +86,23 @@ public class ParserTest {
 
     @Test
     void parseEditCommand() {
-        String inputString = "edit 3 d/2000-01-01 a/10.90 s/Lunch";
+        String inputString = "edit 3 d/2000-01-01 a/10.90 p/Lunch";
         Command finalCommand = MainInputParser.parseInputs(inputString);
         assertEquals(finalCommand.getClass().getName(), "seedu.commands.EditCommand");
     }
 
     @Test
     void parseLendCommand() {
-        // Format: category d/date, n/name, a/amount, b/deadline, s/description
-        String inputString = "lend d/2000-01-01 n/Bob a/100.0 b/2024-07-20 s/To buy flowers";
+        // Format: category d/date, n/name, a/amount, b/deadline, p/description
+        String inputString = "lend d/2000-01-01 n/Bob a/100.0 b/2024-07-20 p/To buy flowers";
         Command finalCommand = MainInputParser.parseInputs(inputString);
         assertEquals(finalCommand.getClass().getName(), "seedu.commands.LendExpenditureCommand");
     }
 
     @Test
     void parseBorrowCommand() {
-        // Format: category d/date, n/name, a/amount, b/deadline, s/description
-        String inputString = "borrow d/2000-01-01 n/Alice a/100.0 b/2024-07-20 s/For school loans";
+        // Format: category d/date, n/name, a/amount, b/deadline, p/description
+        String inputString = "borrow d/2000-01-01 n/Alice a/100.0 b/2024-07-20 p/For school loans";
         Command finalCommand = MainInputParser.parseInputs(inputString);
         assertEquals(finalCommand.getClass().getName(), "seedu.commands.BorrowExpenditureCommand");
     }
@@ -127,19 +127,19 @@ public class ParserTest {
         Command finalCommand = MainInputParser.parseInputs(inputMissingDescription);
         assertEquals(finalCommand.getClass().getName(), "seedu.commands.InvalidCommand");
 
-        String inputMissingDate = "academic a/200.0 s/Tuition";
+        String inputMissingDate = "academic a/200.0 p/Tuition";
         finalCommand = MainInputParser.parseInputs(inputMissingDate);
         assertEquals(finalCommand.getClass().getName(), "seedu.commands.InvalidCommand");
 
-        String inputMissingAmount = "academic d/2000-01-01 s/Tuition";
+        String inputMissingAmount = "academic d/2000-01-01 p/Tuition";
         finalCommand = MainInputParser.parseInputs(inputMissingAmount);
         assertEquals(finalCommand.getClass().getName(), "seedu.commands.InvalidCommand");
     }
 
     @Test
     void invalidInputDueToWrongInputFormat() {
-        String wrongDate = "academic d/01-01-2000 a/200.0 s/Tuition";
-        String wrongAmount = "academic d/2000-01-01 a/Twenty s/Tuition";
+        String wrongDate = "academic d/01-01-2000 a/200.0 p/Tuition";
+        String wrongAmount = "academic d/2000-01-01 a/Twenty p/Tuition";
         Command finalCommand = MainInputParser.parseInputs(wrongDate);
         assertEquals(finalCommand.getClass().getName(), "seedu.commands.InvalidCommand");
         finalCommand = MainInputParser.parseInputs(wrongAmount);
@@ -148,8 +148,8 @@ public class ParserTest {
 
     @Test
     void invalidInputDueToWrongPositionInput() {
-        String posOfDate = "food a/9.90 s/Lunch d/2000-01-01";
-        String posOfAmount = "food a/9.90 d/2000-01-01 s/Lunch";
+        String posOfDate = "food a/9.90 p/Lunch d/2000-01-01";
+        String posOfAmount = "food a/9.90 d/2000-01-01 p/Lunch";
         Command finalCommand = MainInputParser.parseInputs(posOfDate);
         assertEquals(finalCommand.getClass().getName(), "seedu.commands.InvalidCommand");
         finalCommand = MainInputParser.parseInputs(posOfAmount);
@@ -158,8 +158,8 @@ public class ParserTest {
 
     @Test
     void invalidInputDueToWrongBackSlashInput() {
-        String swapAWithD = "academic a/2000-01-01 d/200.0 s/Tuition";
-        String replaceDWithF = "academic d/2000-01-01 f/200.0 s/Tuition";
+        String swapAWithD = "academic a/2000-01-01 d/200.0 p/Tuition";
+        String replaceDWithF = "academic d/2000-01-01 f/200.0 p/Tuition";
         Command finalCommand = MainInputParser.parseInputs(swapAWithD);
         assertEquals(finalCommand.getClass().getName(), "seedu.commands.InvalidCommand");
         finalCommand = MainInputParser.parseInputs(replaceDWithF);
