cbd2b13862ed17d1db22375600dbf3f0def41b18
SSzeWen <seetszewen@gmail.com>
2023-04-08 15:55:08 +0800

    Add View Budget Command Junit tests

--- src/test/java/seedu/duke/command/ViewBudgetCommandTest.java
+++ src/test/java/seedu/duke/command/ViewBudgetCommandTest.java
@@ -1,22 +1,45 @@
 package seedu.duke.command;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import seedu.duke.UI;
 import seedu.duke.budget.BudgetPlanner;
 
 import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ViewBudgetCommandTest {
 
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    //@@author dfa-reused
+    //Reused from https://stackoverflow.com/questions/1119385/junit-test-for-system-out-println
+    private static final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @BeforeAll
+    public void setUpStream() {
+        System.setOut(new PrintStream(outContent));
+    }
 
     @Test
     void viewBudget_execute_success() {
+
+        outContent.reset();
         BudgetPlanner budgetPlanner = BudgetPlanner.getInstance();
         Command command = new ViewBudgetCommand(budgetPlanner);
         command.execute();
-        assertEquals("","");
+        UI ui = UI.getUiOneInstance();
+        String ls = System.lineSeparator();
+        assertEquals(ls + ui.getLine() + ls
+                        + "Total budget: " + budgetPlanner.getBudget() + ls
+                        + "Accommodation cost: " + budgetPlanner.getAccommodationTotalCost() + ls
+                        + "Airplane Ticket cost: " + budgetPlanner.getAirplaneTicketTotalCost() + ls
+                        + "Food cost: " + budgetPlanner.getFoodTotalCost() + ls
+                        + "Entertainment cost: " + budgetPlanner.getEntertainmentTotalCost() + ls
+                        + "Surplus/Deficit: " + budgetPlanner.getSurplus() + ls
+                        + ui.getLine()
+                , ls + outContent.toString().stripTrailing());
     }
 }
