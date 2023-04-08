e99729a66a0bce4e2918a8bb5ec8ec1c492bfa45
Li Mingyuan <e0773670@u.nus.edu>
2023-04-08 17:33:03 +0800

    Update search tests actual

--- src/main/java/seedu/moneymind/ui/Ui.java
+++ src/main/java/seedu/moneymind/ui/Ui.java
@@ -1,7 +1,15 @@
 package seedu.moneymind.ui;
 
-import static seedu.moneymind.string.Strings.BYE_MESSAGE;
-import static seedu.moneymind.string.Strings.HORIZONTAL_LINE;
+import seedu.moneymind.category.Category;
+import seedu.moneymind.command.CategoryCommand;
+import seedu.moneymind.event.Event;
+import static seedu.moneymind.command.SearchCommand.getCategoryOfEvent;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+import static seedu.moneymind.string.Strings.*;
+import static seedu.moneymind.string.Strings.NO_SEARCH_RESULTS;
 
 public class Ui {
     private static final String LOAD_ERROR_RISK_MESSAGE = 
@@ -37,8 +45,90 @@ public class Ui {
         System.out.println(ERROR + "\n" + e);
     }
 
-    public void list() {
-        System.out.println(LIST);
+    /**
+     * Prints search results for categories that match the query string.
+     * @param matchingCategories Set containing categories that match the query string.
+     */
+    public static void printMatchingCategories(Set<Category> matchingCategories) {
+        System.out.println("Matching Categories:");
+
+        if (matchingCategories.size() == 0) {
+            System.out.println(NO_SEARCH_RESULTS);
+        }
+
+        for (Category category : matchingCategories) {
+            int categoryIndex = CategoryCommand.categoryMap.get(category.getName());
+            System.out.println((categoryIndex + 1) + DOT + category.getName());
+        }
+    }
+
+    /**
+     * Prints search results for events that match the query string.
+     * @param matchingEvents Set containing events that match the query string.
+     */
+    public static void printMatchingEvents(Set<Event> matchingEvents) {
+        System.out.println("\nMatching Events:");
+
+        if (matchingEvents.size() == 0) {
+            System.out.println(NO_SEARCH_RESULTS);
+        }
+
+        for (Event event : matchingEvents) {
+            Category categoryOfEvent = getCategoryOfEvent(event);
+            assert categoryOfEvent != null;
+            String categoryName = categoryOfEvent.getName();
+            System.out.println(event.getDescription() + " (Category: " + categoryName + ")");
+        }
+    }
+
+    /**
+     * Prints search results for categories that are similar to the query string.
+     * @param similarCategoriesList Array list containing the similar categories, sorted by similarity distance.
+     */
+    public static void printSimilarCategories(ArrayList<Category> similarCategoriesList) {
+        System.out.println("\nSimilar Categories:");
+
+        int similarCategoryCount = 3;
+        if (similarCategoriesList.size() < 3) {
+            similarCategoryCount = similarCategoriesList.size();
+        }
+
+        if (similarCategoryCount == 0) {
+            System.out.println(NO_SEARCH_RESULTS);
+            return;
+        }
+
+        for (int i = 0; i < similarCategoryCount; i++) {
+            Category category = similarCategoriesList.get(i);
+            int categoryIndex = CategoryCommand.categoryMap.get(category.getName());
+            System.out.println((categoryIndex + 1) + DOT + category.getName());
+        }
+    }
+
+    /**
+     * Prints search results for events that are similar to the query string.
+     * @param similarEventsList Array list containing the similar events, sorted by similarity distance.
+     */
+    public static void printSimilarEvents(ArrayList<Event> similarEventsList) {
+        System.out.println("\nSimilar Events:");
+
+        int similarEventsCount = 3;
+        if (similarEventsList.size() < 3) {
+            similarEventsCount = similarEventsList.size();
+        }
+
+        if (similarEventsCount == 0) {
+            System.out.println(NO_SEARCH_RESULTS);
+            return;
+        }
+
+        for (int i = 0; i < similarEventsCount; i++) {
+            Event event = similarEventsList.get(i);
+            Category categoryOfEvent = getCategoryOfEvent(event);
+            assert categoryOfEvent != null;
+            String categoryName = categoryOfEvent.getName();
+            System.out.println(event.getDescription() + " (Category: " + categoryName + ")");
+        }
     }
 
 }
--- src/test/java/seedu/moneymind/commands/SearchCommandTest.java
+++ src/test/java/seedu/moneymind/commands/SearchCommandTest.java
@@ -1,8 +1,17 @@
 package seedu.moneymind.commands;
 
 import org.junit.jupiter.api.Test;
+import seedu.moneymind.category.Category;
+import seedu.moneymind.command.SearchCommand;
+import seedu.moneymind.event.Event;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SearchCommandTest extends CommandTest {
 
@@ -11,24 +20,206 @@ public class SearchCommandTest extends CommandTest {
     }
 
     @Test
-    void searchCommand_withQuery_expectCorrectOutput() {
+    void searchCommand_assignItemsBySimilarity_matchNoCategory() {
+        String query = "test";
+
+        SearchCommand command = new SearchCommand(query);
+
+        Set<Category> matchingCategories = new LinkedHashSet<>();
+        Set<Event> matchingEvents = new LinkedHashSet<>();
+        HashMap<Category, Integer> similarCategories = new HashMap<>();
+        HashMap<Event, Integer> similarEvents = new HashMap<>();
+
         setup();
-        String terminalOutput = executeInput("search hello").toString();
-        String expected = "Matching Categories:" + System.lineSeparator() +
-                "No matching search results." + System.lineSeparator() +
-                System.lineSeparator() +
-                "Matching Events:" + System.lineSeparator() +
-                "No matching search results." + System.lineSeparator() +
-                System.lineSeparator() +
-                "Similar Categories:" + System.lineSeparator() +
-                "2.book" + System.lineSeparator() +
-                "1.food" + System.lineSeparator() +
-                System.lineSeparator() +
-                "Similar Events:" + System.lineSeparator() +
-                "Lord of the Rings (Category: book)" + System.lineSeparator() +
-                "Harry Potter (Category: book)" + System.lineSeparator() +
-                "salad (Category: food)" + System.lineSeparator();
-        assertEquals(expected, terminalOutput);
+
+        command.assignItemsBySimilarity(matchingCategories, matchingEvents, similarCategories, similarEvents, query);
+
+        assertEquals(0, matchingCategories.size());
+        assertEquals(0, matchingEvents.size());
+        assertEquals(2, similarCategories.size());
+        assertEquals(4, similarEvents.size());
+
+        clear();
+    }
+
+    @Test
+    void searchCommand_assignItemsBySimilarity_matchOneCategory() {
+        String query = "book";
+
+        SearchCommand command = new SearchCommand(query);
+
+        Set<Category> matchingCategories = new LinkedHashSet<>();
+        Set<Event> matchingEvents = new LinkedHashSet<>();
+        HashMap<Category, Integer> similarCategories = new HashMap<>();
+        HashMap<Event, Integer> similarEvents = new HashMap<>();
+
+        setup();
+
+        command.assignItemsBySimilarity(matchingCategories, matchingEvents, similarCategories, similarEvents, query);
+
+        assertEquals(1, matchingCategories.size());
+        assertEquals(0, matchingEvents.size());
+        assertEquals(1, similarCategories.size());
+        assertEquals(4, similarEvents.size());
+
+        clear();
+    }
+
+    @Test
+    void searchCommand_assignItemsBySimilarity_matchMultipleCategory() {
+        String query = "oo";
+
+        SearchCommand command = new SearchCommand(query);
+
+        Set<Category> matchingCategories = new LinkedHashSet<>();
+        Set<Event> matchingEvents = new LinkedHashSet<>();
+        HashMap<Category, Integer> similarCategories = new HashMap<>();
+        HashMap<Event, Integer> similarEvents = new HashMap<>();
+
+        setup();
+
+        command.assignItemsBySimilarity(matchingCategories, matchingEvents, similarCategories, similarEvents, query);
+
+        assertEquals(2, matchingCategories.size());
+        assertEquals(0, matchingEvents.size());
+        assertEquals(0, similarCategories.size());
+        assertEquals(4, similarEvents.size());
+
+        clear();
+    }
+
+    @Test
+    void searchCommand_assignItemsBySimilarity_matchNoEvent() {
+        String query = "test";
+
+        SearchCommand command = new SearchCommand(query);
+
+        Set<Category> matchingCategories = new LinkedHashSet<>();
+        Set<Event> matchingEvents = new LinkedHashSet<>();
+        HashMap<Category, Integer> similarCategories = new HashMap<>();
+        HashMap<Event, Integer> similarEvents = new HashMap<>();
+
+        setup();
+
+        command.assignItemsBySimilarity(matchingCategories, matchingEvents, similarCategories, similarEvents, query);
+
+        assertEquals(0, matchingCategories.size());
+        assertEquals(0, matchingEvents.size());
+        assertEquals(2, similarCategories.size());
+        assertEquals(4, similarEvents.size());
+
+        clear();
+    }
+
+    @Test
+    void searchCommand_assignItemsBySimilarity_matchOneEvent() {
+        String query = "lord";
+
+        SearchCommand command = new SearchCommand(query);
+
+        Set<Category> matchingCategories = new LinkedHashSet<>();
+        Set<Event> matchingEvents = new LinkedHashSet<>();
+        HashMap<Category, Integer> similarCategories = new HashMap<>();
+        HashMap<Event, Integer> similarEvents = new HashMap<>();
+
+        setup();
+
+        command.assignItemsBySimilarity(matchingCategories, matchingEvents, similarCategories, similarEvents, query);
+
+        assertEquals(0, matchingCategories.size());
+        assertEquals(1, matchingEvents.size());
+        assertEquals(2, similarCategories.size());
+        assertEquals(3, similarEvents.size());
+
+        clear();
+    }
+
+    @Test
+    void searchCommand_assignItemsBySimilarity_matchMultipleEvent() {
+        String query = "e";
+
+        SearchCommand command = new SearchCommand(query);
+
+        Set<Category> matchingCategories = new LinkedHashSet<>();
+        Set<Event> matchingEvents = new LinkedHashSet<>();
+        HashMap<Category, Integer> similarCategories = new HashMap<>();
+        HashMap<Event, Integer> similarEvents = new HashMap<>();
+
+        setup();
+
+        command.assignItemsBySimilarity(matchingCategories, matchingEvents, similarCategories, similarEvents, query);
+
+        assertEquals(0, matchingCategories.size());
+        assertEquals(2, matchingEvents.size());
+        assertEquals(2, similarCategories.size());
+        assertEquals(2, similarEvents.size());
+
+        clear();
+    }
+
+    @Test
+    void searchCommand_sortCategoryBySimilarity_expectSorted() {
+        String query = "test";
+
+        SearchCommand command = new SearchCommand(query);
+
+        Set<Category> matchingCategories = new LinkedHashSet<>();
+        Set<Event> matchingEvents = new LinkedHashSet<>();
+        HashMap<Category, Integer> similarCategories = new HashMap<>();
+        HashMap<Event, Integer> similarEvents = new HashMap<>();
+        ArrayList<Category> similarCategoriesList = new ArrayList<>();
+
+        setup();
+
+        command.assignItemsBySimilarity(matchingCategories, matchingEvents, similarCategories, similarEvents, query);
+        similarCategoriesList.addAll(similarCategories.keySet());
+        command.sortCategoryBySimilarity(similarCategoriesList, similarCategories);
+
+        boolean sorted = true;
+        for (int i = 0; i < similarCategoriesList.size()-1; i++) {
+            int currentCategoryValue = similarCategories.get(similarCategoriesList.get(i));
+            int nextCategoryValue = similarCategories.get(similarCategoriesList.get(i+1));
+            if (currentCategoryValue > nextCategoryValue) {
+                sorted = false;
+            }
+        }
+
+        assertEquals(2, similarCategoriesList.size());
+        assertTrue(sorted);
+
+        clear();
+    }
+
+    @Test
+    void searchCommand_sortEventBySimilarity_expectSorted() {
+        String query = "test";
+
+        SearchCommand command = new SearchCommand(query);
+
+        Set<Category> matchingCategories = new LinkedHashSet<>();
+        Set<Event> matchingEvents = new LinkedHashSet<>();
+        HashMap<Category, Integer> similarCategories = new HashMap<>();
+        HashMap<Event, Integer> similarEvents = new HashMap<>();
+        ArrayList<Event> similarEventsList = new ArrayList<>();
+
+        setup();
+
+        command.assignItemsBySimilarity(matchingCategories, matchingEvents, similarCategories, similarEvents, query);
+        similarEventsList.addAll(similarEvents.keySet());
+        command.sortEventBySimilarity(similarEventsList, similarEvents);
+
+        boolean sorted = true;
+        for (int i = 0; i < similarEventsList.size()-1; i++) {
+            int currentEventValue = similarEvents.get(similarEventsList.get(i));
+            int nextEventValue = similarEvents.get(similarEventsList.get(i+1));
+            if (currentEventValue > nextEventValue) {
+                sorted = false;
+            }
+        }
+
+        assertEquals(4, similarEventsList.size());
+        assertTrue(sorted);
+
         clear();
     }
 
