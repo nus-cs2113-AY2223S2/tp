7bde58b0d45371721112645a1d0887355a7fedb4
SSzeWen <seetszewen@gmail.com>
2023-04-08 17:28:14 +0800

    Add documentation for Storage Class

--- src/main/java/seedu/duke/Storage.java
+++ src/main/java/seedu/duke/Storage.java
@@ -14,6 +14,10 @@ public class Storage implements DatabaseInterface {
     private static final String SAVED_MODULES_FILE_PATH = "data/saved_modules.txt";
     private ArrayList<Module> modules;
 
+    /**
+     * Constructor of the Storage class that initialises the Storage database.
+     * Prints a failure message when there is an IOException when trying to initialise.
+     */
     private Storage() {
         this.modules = new ArrayList<>();
         try {
@@ -30,6 +34,13 @@ public class Storage implements DatabaseInterface {
         return instance;
     }
 
+    /**
+     * Initialises the Storage database for module saving.
+     * It creates a new directory and file being data/saved_modules.txt when there is no file found.
+     * If there is already an existing file, it loads the data into the ArrayList of modules.
+     *
+     * @throws IOException when the file path does not exist but commands are being given to load from the file path.
+     */
     public void initialiseDatabase() throws IOException {
         File savedModulesFile = new File(SAVED_MODULES_FILE_PATH);
         if (!savedModulesFile.exists()) {
@@ -50,6 +61,13 @@ public class Storage implements DatabaseInterface {
         }
     }
 
+    /**
+     * Returns a boolean value, either true or false on whether the saved_modules.txt file is corrupted.
+     * Corrupted occurs when there might be tampering of the txt file in an incorrect way.
+     *
+     * @return true if saved_modules.txt file is corrupted.
+     * @return false if saved_modules.txt file is not corrupted.
+     */
     @Override
     public boolean checkDatabaseCorrupted() {
         ArrayList<Module> allModules = DataReader.getDataReaderOneInstance().getModules();
@@ -69,6 +87,15 @@ public class Storage implements DatabaseInterface {
         return isCorrupted;
     }
 
+    /**
+     * Returns a boolean value, either true or false on whether the module is a valid one cross compared to the list of
+     * valid modules.
+     *
+     * @param moduleToCheck is the module to be checked whether valid or not.
+     * @param allModules    is the arraylist of modules that is being cross-referenced from.
+     * @return true if the moduleToCheck is a valid one, meaning it exists in allModules.
+     * @return false if the moduleToCheck is an invalid one, meaning it does not exist in allModules.
+     */
     private boolean checkIsValidModule(Module moduleToCheck, ArrayList<Module> allModules) {
         for (Module module : allModules) {
             if (module.toString().equals(moduleToCheck.toString())) {
@@ -78,6 +105,12 @@ public class Storage implements DatabaseInterface {
         return false;
     }
 
+    /**
+     * Reads module information from the txt file line by line and adding it into the module list
+     *
+     * @param modulesFilePath is the moduleFilePath to read data from.
+     * @param modules         is the ArrayList of modules to add the txt file module data into.
+     */
     private void readModData(String modulesFilePath, ArrayList<Module> modules) {
         try (BufferedReader br = new BufferedReader(new FileReader(modulesFilePath))) {
             String line;
@@ -107,6 +140,16 @@ public class Storage implements DatabaseInterface {
         }
     }
 
+    /**
+     * Returns a boolean value, either true or false depending on whether the module was added successfully to the
+     * list of modules.
+     * Checks if the module to be added was already previously added.
+     *
+     * @param moduleToAdd is the target module to be added into the list.
+     * @return true if the module was added successfully.
+     * @return false if the module was not added successfully, in cases like it was already added before or when the
+     * moduleToAdd is of null value.
+     */
     public boolean addModuleToModuleList(Module moduleToAdd) {
         assert (moduleToAdd != null) : "error line 111";
         if (moduleToAdd == null) {
@@ -127,16 +170,28 @@ public class Storage implements DatabaseInterface {
         return true;
     }
 
+    /**
+     * Returns a boolean value, either true or false depending on whether the module already exists in the module list.
+     *
+     * @param moduleToAdd is the module to be checked if already exists
+     * @return true if module already exists in the module list.
+     * @return false if module does not exist in the module list.
+     */
     public boolean doesModuleExist(Module moduleToAdd) {
         for (Module module : modules) {
-            if ((moduleToAdd.getUnivId() == module.getUnivId()) &&
-                    moduleToAdd.toString().equalsIgnoreCase(module.toString())) {
+            if (moduleToAdd.toString().equalsIgnoreCase(module.toString())) {
                 return true;
             }
         }
         return false;
     }
 
+    /**
+     * Saves Module to the saved_modules.txt file by appending it to the back of the txt file.
+     *
+     * @param saveModuleString is the String format to be saved for the target module.
+     * @throws IOException when the file path does not exist but commands are being given to load from the file path.
+     */
     private void saveModuleToStorage(String saveModuleString) throws IOException {
         FileWriter fw = new FileWriter(SAVED_MODULES_FILE_PATH, true);
         fw.write(writeTaskPreparation(saveModuleString));
@@ -144,6 +199,7 @@ public class Storage implements DatabaseInterface {
     }
 
     /**
+     * Returns true or false depending on whether the module was deleted successfully.
      * Deletes the module corresponding to the uni specified by user. Module will the removed from user's
      * saved list of modules. Uses Index relative to specific PU list. Sorts ArrayList modules after deleting
      * module to ensure that modules are still sorted according to printing length
