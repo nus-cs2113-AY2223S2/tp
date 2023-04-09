0c1409ea7832ada1b7648b3fa1c0fb04b3601f53
Gujimugger <wangsilangCHANGE@gmail.com>
2023-04-10 01:54:15 +0800

    mc

--- src/main/java/utils/parser/KeywordParser.java
+++ src/main/java/utils/parser/KeywordParser.java
@@ -5,6 +5,7 @@ import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+
 import model.CardSelector;
 import model.TagSelector;
 import org.apache.commons.cli.AlreadySelectedException;
@@ -231,7 +232,7 @@ public abstract class KeywordParser {
      * @return Formatted help string
      */
     protected String formatHelpMessage(String keyword, String[] actionList, String[] headerList,
-            Options[] optionsList) {
+                                       Options[] optionsList) {
         assert optionsList.length == headerList.length;
         assert optionsList.length == actionList.length;
 
--- src/main/java/utils/parser/TagKeywordParser.java
+++ src/main/java/utils/parser/TagKeywordParser.java
@@ -1,7 +1,11 @@
 package utils.parser;
 
 import java.util.List;
+<<<<<<< HEAD
 import model.TagSelector;
+=======
+
+>>>>>>> 8dbba37 (Autoindent for checkstyle)
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
@@ -27,6 +31,7 @@ public class TagKeywordParser extends KeywordParser {
     protected Command handleAction(String action, List<String> tokens)
             throws ParseException, InkaException {
         switch (action) {
+<<<<<<< HEAD
         case DECK_ACTION:
             return handleDeck(tokens);
         case DELETE_ACTION:
@@ -39,6 +44,20 @@ public class TagKeywordParser extends KeywordParser {
             return handleList(tokens);
         default:
             throw new UnrecognizedCommandException();
+=======
+            case DECK_ACTION:
+                return handleDeck(tokens);
+            case DELETE_ACTION:
+                return handleDelete(tokens);
+            case EDIT_ACTION:
+                return handleEdit(tokens);
+            case HELP_ACTION:
+                return handleHelp();
+            case LIST_ACTION:
+                return handleList(tokens);
+            default:
+                throw new UnrecognizedCommandException();
+>>>>>>> 8dbba37 (Autoindent for checkstyle)
         }
     }
 
