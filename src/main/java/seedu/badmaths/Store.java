67b35bb10204e5c31f6ef001084863904cb8f155
Wilson Lee Jun Wei <leejunwei2000@gmail.com>
2023-04-09 15:43:23 +0800

    Implement Logging for Store.java

--- src/main/java/seedu/badmaths/Store.java
+++ src/main/java/seedu/badmaths/Store.java
@@ -5,8 +5,16 @@ import seedu.badmaths.note.NotesList;
 import seedu.badmaths.storage.NotesFileWriter;
 import seedu.badmaths.ui.Ui;
 
+import java.util.logging.Logger;
+import java.util.logging.LogManager;
+import java.util.logging.Level;
+import java.io.IOException;
+import java.io.File;
+
 public class Store {
 
+    protected static String logFilePath = "Store";
+    private static Logger log = Logger.getLogger("Store");
     private static final String filePath = "data/notes.txt";
     public NotesList notes;
     protected String toDo;
@@ -16,12 +24,27 @@ public class Store {
         this.notes = notes;
     }
 
+    public static void implementLogger() {
+        LogManager.getLogManager().reset();
+        log.setLevel(Level.INFO);
+        try {
+            if (!new File(logFilePath).exists()) {
+                new File(logFilePath).createNewFile();
+            }
+            throw new IOException();
+        } catch (IOException ioException) {
+            log.log(Level.SEVERE, "File logger is not working", ioException);
+        }
+    }
+
     public static boolean isInvalidTodo(String todo) {
         return todo.equals("Invalid todo");
     }
 
-    public void storeNotes() { // this is a method
+    public void storeNotes() {
+        implementLogger();
         try {
+            log.log(Level.INFO, "Storing in inputs by users");
             if (isInvalidTodo(toDo)) {
                 throw new IllegalTodoException();
             }
@@ -33,6 +56,7 @@ public class Store {
             NotesFileWriter.saveFile(filePath, notes.getAll());
         } catch (IllegalTodoException exception) {
             Ui.printIncorrectFormatEntered();
+            log.severe("Incorrect Format Entered");
         }
     }
 }
