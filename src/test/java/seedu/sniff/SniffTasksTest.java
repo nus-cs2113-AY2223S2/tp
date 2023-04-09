7bf76e0311cbc9fde3a518666550f274be0106e0
dendendenden04 <dennisokq@gmail.com>
2023-04-10 01:02:49 +0800

    Remove unused imports for SniffTasksTest

--- src/test/java/seedu/sniff/SniffTasksTest.java
+++ src/test/java/seedu/sniff/SniffTasksTest.java
@@ -1,6 +1,5 @@
 package seedu.sniff;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import exception.DuplicateAppointmentException;
