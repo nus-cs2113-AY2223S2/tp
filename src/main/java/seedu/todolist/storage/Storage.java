0008726c56b7831d7f3ea1f2f6e064a790947e26
jeromeongithub <left4jerry@gmail.com>
2023-04-09 22:35:52 +0800

    fix checkstyle issue

--- src/main/java/seedu/todolist/storage/Storage.java
+++ src/main/java/seedu/todolist/storage/Storage.java
@@ -78,7 +78,7 @@ public class Storage {
      * Loads the task list from the local save file.
      *
      * @return The loaded task list if it is not empty. Otherwise, a newly created task list is returned.
-     * @throws FileNotFoundException If no saved file was found.
+     * @throws FileNotFoundException   If no saved file was found.
      * @throws FailedLoadDataException If the task list could not be loaded because the save file is invalid.
      */
     public TaskList loadData() throws FileNotFoundException, FailedLoadDataException {
@@ -104,15 +104,16 @@ public class Storage {
     }
 
     //@@author clement559
+
     /**
      * Loads the configuration settings from the local config file.
      *
-     * @throws FileNotFoundException If the config file cannot be found.
+     * @throws FileNotFoundException     If the config file cannot be found.
      * @throws FailedLoadConfigException If the config filed cannot be loaded.
      */
     public Config loadConfig() throws FileNotFoundException, FailedLoadConfigException {
         try {
-                JsonReader reader = new JsonReader(new FileReader(configFile));
+            JsonReader reader = new JsonReader(new FileReader(configFile));
             Config config = gson.fromJson(reader, Config.class);
             if (config == null) {
                 return new Config();
