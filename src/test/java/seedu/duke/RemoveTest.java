79ee1b49c04babb7c8ab1e1d59640e12cc3b2e14
ArtemiszenN <artemiszenn@gmail.com>
2023-04-09 21:56:00 +0800

    Add a new test to try and find the elusive remove bug

--- src/test/java/seedu/duke/RemoveTest.java
+++ src/test/java/seedu/duke/RemoveTest.java
@@ -6,6 +6,7 @@ import seedu.duke.objects.Item;
 import seedu.duke.objects.Inventory;
 import seedu.duke.commands.AddCommand;
 import seedu.duke.commands.RemoveCommand;
+import seedu.duke.utils.SessionManager;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -56,5 +57,34 @@ class RemoveTest {
         Command removeCommand3 = new RemoveCommand(inventory,"123");
         removeCommand3.run();
         assertEquals(0, inventory.getItemInventory().size());
+    }
+
+    @Test
+    void removeByUpcMultipleTimes(){
+        Inventory inventory = new Inventory();
+        SessionManager.setAutoSave(false);
+        for(int i=0;i<1000;i++){
+            Command addCommand = new AddCommand(inventory, new Item("item1", "01",15,10.0));
+            addCommand.run();
+            addCommand = new AddCommand(inventory, new Item("item2", "02",15,10.0));
+            addCommand.run();
+            addCommand = new AddCommand(inventory, new Item("item3", "03",15,10.0));
+            addCommand.run();
+            addCommand = new AddCommand(inventory, new Item("item4", "04",15,10.0));
+            addCommand.run();
+            addCommand = new AddCommand(inventory, new Item("item5", "05",15,10.0));
+            addCommand.run();
+            Command removeCommand = new RemoveCommand(inventory, "01");
+            removeCommand.run();
+            removeCommand = new RemoveCommand(inventory, "02");
+            removeCommand.run();
+            removeCommand = new RemoveCommand(inventory, "03");
+            removeCommand.run();
+            removeCommand = new RemoveCommand(inventory, "04");
+            removeCommand.run();
+            removeCommand = new RemoveCommand(inventory, "05");
+            removeCommand.run();
+            assertEquals(inventory.getItemInventory().size(),0);
+        }
     }
 }
