4000c4357c43eb94e95594b947fe3b8cf78c5479
wuzhzn <dagreatbao@gmail.com>
2023-04-09 09:55:43 +0800

    Fix bug when user changes the text file

--- src/main/java/seedu/duke/storage/EventDetailsStorage.java
+++ src/main/java/seedu/duke/storage/EventDetailsStorage.java
@@ -2,6 +2,7 @@ package seedu.duke.storage;
 
 import seedu.duke.event.Event;
 import seedu.duke.exception.InvalidIndexException;
+import seedu.duke.exception.NoSavedInformationException;
 import seedu.duke.ui.Ui;
 import seedu.duke.venue.VenueList;
 
@@ -15,7 +16,7 @@ import java.util.Scanner;
 /**
  * Storage that handles the data from the event details text file
  */
-public class EventDetailsStorage extends Storage{
+public class EventDetailsStorage extends Storage {
 
     private static final String filePath = "data/eventDetails.txt";
 
@@ -40,6 +41,12 @@ public class EventDetailsStorage extends Storage{
             event.updateEventName("Default Event");
             System.out.println("Choose an event name using update event name <EVENT_NAME>");
             updateFile(event);
+        } catch (NoSavedInformationException e) {
+            ui.showLine();
+            System.out.println("There is no saved event name found in event details text file.");
+            System.out.println("Setting event name...");
+            event.updateEventName("Default Event");
+            updateFile(event);
         }
     }
 
@@ -50,14 +57,18 @@ public class EventDetailsStorage extends Storage{
      * @throws NoSuchElementException if error occurs due to no next line found
      * @throws InvalidIndexException if error occurred due to invalid index
      * @throws NumberFormatException if error occurred due to the not reading a number when expected
+     * @throws NoSavedInformationException if error occured due the no saved information on text file
      */
     public static void load(Event event, VenueList venueList) throws FileNotFoundException,
-            NoSuchElementException, InvalidIndexException, NumberFormatException {
+            NoSuchElementException, InvalidIndexException, NumberFormatException, NoSavedInformationException {
         File file = new File(filePath);
         Scanner s = new Scanner(file);
         String line = s.nextLine();
         String[] parsedLine = line.split("\\|");
         System.out.println("Retrieving your event data.....");
+        if (parsedLine.length == 0 || parsedLine[0].equals("")){
+            throw new NoSavedInformationException();
+        }
         event.updateEventName(parsedLine[0]);
         if (parsedLine.length == 1){
             throw new InvalidIndexException();
