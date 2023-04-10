b30685840d2caa2f84d44e613b0f052dd738404b
hyperbola-bear <hung.hin.wang@gmail.com>
2023-04-10 15:46:10 +0800

    category find

--- src/main/java/chching/command/FindCommand.java
+++ src/main/java/chching/command/FindCommand.java
@@ -83,8 +83,8 @@ public class FindCommand extends Command {
             for (int i = 0; i < expenses.size(); i++) {
                 Expense expense = expenses.get(i);
                 if ((description == null 
-                    || expense.getDescription().toLowerCase().contains(description.toLowerCase()))
-                    && (category == null || expense.getCategory().equals(category)) 
+                    || expense.getDescription().toLowerCase().trim().contains(description.toLowerCase()))
+                    && (category == null || expense.getCategory().toLowerCase().trim().contains(category)) 
                     && (date == null || expense.getDate().equals(date))) {
                     expensesMatched.addExpense(expense);
                 }
