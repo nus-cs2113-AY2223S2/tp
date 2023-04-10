c4272f5fc31dadc1555f9dda14f546e51f2f9659
azriellee <azriel20005@gmail.com>
2023-04-10 14:33:19 +0800

    Update DG parser diagram

--- src/main/java/seedu/rainyDay/parser/ParseDelete.java
+++ src/main/java/seedu/rainyDay/parser/ParseDelete.java
@@ -12,7 +12,7 @@ import java.util.logging.Logger;
 public class ParseDelete extends Parser {
     private static final Logger logger = Logger.getLogger(ParseDelete.class.getName());
 
-    public Command parseDeleteStatement(String userInput) throws RainyDayException {
+    public Command deleteStatement(String userInput) throws RainyDayException {
         String[] tokens = userInput.split("\\s+");
         if (tokens.length != 2) {
             logger.warning("invalid delete index from user");
