df3ebaa71a9b01b39ec3407c09fea983b75f90e4
Bui Phuong Nam <e0851451@u.nus.edu>
2023-04-10 14:05:34 +0700

    Checkstyle part 2

--- src/test/java/data/AccountTest.java
+++ src/test/java/data/AccountTest.java
@@ -8,8 +8,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.File;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
